e8d7286865451d80aa190b3642aa3b666d4f452a
Kristen Schau <47155823+krschau@users.noreply.github.com>
2023-03-09 12:43:05 -0500

    List real widgets in Add Widget dialog (#16)

--- tools/Dashboard/DevHome.Dashboard/Views/AddWidgetDialog.xaml.cs
+++ tools/Dashboard/DevHome.Dashboard/Views/AddWidgetDialog.xaml.cs
@@ -1,18 +1,66 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+using System;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.Windows.Widgets.Hosts;
 
 namespace DevHome.Dashboard.Views;
 public sealed partial class AddWidgetDialog : ContentDialog
 {
-    public AddWidgetDialog()
+    private readonly WidgetCatalog _widgetCatalog;
+
+    public AddWidgetDialog(WidgetCatalog catalog)
     {
         this.InitializeComponent();
+
+        _widgetCatalog = catalog;
+
         configurationContentFrame.Content = new WidgetConfigurationContent();
+
+        FillAvailableWidgets();
     }
 
-    private void WidgetConfigurationNavigationView_SelectionChanged(
+    private void FillAvailableWidgets()
+    {
+        AddWidgetNavigationView.MenuItems.Clear();
+
+        // Fill NavigationView Menu with Widget Providers, and group widgets under each provider
+        foreach (var provider in _widgetCatalog.GetProviderDefinitions())
+        {
+            if (IsIncludedWidgetProvider(provider))
+            {
+                var navItem = new NavigationViewItem
+                {
+                    IsExpanded = true,
+                    Tag = provider,
+                    Content = provider.DisplayName,
+                };
+
+                foreach (var widget in _widgetCatalog.GetWidgetDefinitions())
+                {
+                    if (widget.ProviderDefinition.Id.Equals(provider.Id, StringComparison.Ordinal))
+                    {
+                        var subItem = new NavigationViewItem
+                        {
+                            Tag = widget,
+                            Content = widget.DisplayTitle,
+                        };
+                        navItem.MenuItems.Add(subItem);
+                    }
+                }
+
+                AddWidgetNavigationView.MenuItems.Add(navItem);
+            }
+        }
+    }
+
+    private bool IsIncludedWidgetProvider(WidgetProviderDefinition provider)
+    {
+        return provider.Id.StartsWith("Microsoft.Windows.DevHome", StringComparison.CurrentCulture);
+    }
+
+    private void AddWidgetNavigationView_SelectionChanged(
         NavigationView sender,
         NavigationViewSelectionChangedEventArgs args)
     {
--- tools/Dashboard/DevHome.Dashboard/Views/DashboardView.xaml.cs
+++ tools/Dashboard/DevHome.Dashboard/Views/DashboardView.xaml.cs
@@ -5,28 +5,32 @@ using System;
 using DevHome.Common;
 using DevHome.Dashboard.ViewModels;
 using DevHome.Dashboard.Views;
-using Microsoft.UI.Xaml.Controls;
+using Microsoft.Windows.Widgets.Hosts;
 
 namespace DevHome.Dashboard;
 
-/// <summary>
-/// An empty page that can be used on its own or navigated to within a Frame.
-/// </summary>
 public partial class DashboardView : ToolPage
 {
     public override string ShortName => "Dashboard";
 
     public DashboardViewModel ViewModel { get; }
 
+    private readonly WidgetHost _widgetHost;
+    private readonly WidgetCatalog _widgetCatalog;
+
     public DashboardView()
     {
         ViewModel = new DashboardViewModel();
         this.InitializeComponent();
+
+        // GUID is your personal Host GUID that widget platform will use to identify you
+        _widgetHost = WidgetHost.Register(new WidgetHostContext("BAA93438-9B07-4554-AD09-7ACCD7D4F031"));
+        _widgetCatalog = WidgetCatalog.GetDefault();
     }
 
     private async void AddWidgetButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
     {
-        var dialog = new AddWidgetDialog
+        var dialog = new AddWidgetDialog(_widgetCatalog)
         {
             // XamlRoot must be set in the case of a ContentDialog running in a Desktop app
             XamlRoot = this.XamlRoot,
