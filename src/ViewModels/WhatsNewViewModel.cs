96c96a3ba817fd7fed699e9d0732e0128138741b
Ujjwal Chadha <66971578+ujjwalchadha@users.noreply.github.com>
2023-03-10 13:09:02 -0800

    Update UI to use theme

--- src/ViewModels/WhatsNewViewModel.cs
+++ src/ViewModels/WhatsNewViewModel.cs
@@ -14,52 +14,19 @@ namespace DevHome.ViewModels;
 
 public class WhatsNewViewModel : ObservableRecipient
 {
-    public ICommand ItemClickCommand
-    {
-        get;
-    }
-
     public ObservableCollection<WhatsNewCard> Source { get; } = new ObservableCollection<WhatsNewCard>();
-
-    public WhatsNewViewModel()
-    {
-        //ItemClickCommand = new RelayCommand<SampleOrder>(OnItemClick);
-
-        //stringResource.GetLocalized("")
-
-        //var xamlReader = XamlReader.LoadWithInitialTemplateValidation(File.ReadAllText("DataResources\\WhatsNewPageData.xaml"));
-        //var x = 1;
-    }
 
     public void AddCard(WhatsNewCard card)
     {
         Source.Add(card);
     }
 
-    public async void OnNavigatedTo(object parameter)
+    public void OnNavigatedTo(object parameter)
     {
         Source.Clear();
-
-        // TODO: Replace with real data.
-        //var data = await _sampleDataService.GetContentGridDataAsync();
-        //foreach (var item in data)
-        //{
-        //    Source.Add(item);
-        //}
-
-        
     }
 
     public void OnNavigatedFrom()
     {
     }
-
-    //private void OnItemClick(SampleOrder? clickedItem)
-    //{
-    //    if (clickedItem != null)
-    //    {
-    //        _navigationService.SetListDataItemForNextConnectedAnimation(clickedItem);
-    //        _navigationService.NavigateTo(typeof(WhatsNewDetailViewModel).FullName!, clickedItem.OrderID);
-    //    }
-    //}
 }
