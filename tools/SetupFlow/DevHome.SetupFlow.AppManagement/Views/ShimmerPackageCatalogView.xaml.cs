9685c3cb513e4f0a992cd80fd134748f2708dd41
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2023-03-13 21:19:44 -0700

    Added shimmer view model

---
+++ tools/SetupFlow/DevHome.SetupFlow.AppManagement/Views/ShimmerPackageCatalogView.xaml.cs
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace DevHome.SetupFlow.AppManagement.Views;
+
+[INotifyPropertyChanged]
+public sealed partial class ShimmerPackageCatalogView : UserControl
+{
+    [ObservableProperty]
+    private IEnumerable<int> _shimmerPackages;
+
+    public int PackageCount
+    {
+        get => (int)GetValue(PackageCountProperty);
+        set => SetValue(PackageCountProperty, value);
+    }
+
+    public ShimmerPackageCatalogView()
+    {
+        this.InitializeComponent();
+    }
+
+    private void UpdateShimmerPackages()
+    {
+        ShimmerPackages = Enumerable.Range(1, PackageCount);
+    }
+
+    public static readonly DependencyProperty PackageCountProperty = DependencyProperty.Register(nameof(PackageCount), typeof(int), typeof(ShimmerPackageCatalogView), new PropertyMetadata(0, (c, _) => ((ShimmerPackageCatalogView)c).UpdateShimmerPackages()));
+}
