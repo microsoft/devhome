4feba722ea19285d03af3fa931cb40768355694a
Eric Johnson <ejohn@microsoft.com>
2023-03-16 20:17:19 -0700

    storing bad capability code

--- pluginsdk/Microsoft.Windows.DevHome.SDK.Lib/PluginInstanceManager.cs
+++ pluginsdk/Microsoft.Windows.DevHome.SDK.Lib/PluginInstanceManager.cs
@@ -2,14 +2,12 @@
 // Licensed under the MIT license.
 
 using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using Windows.ApplicationModel.Background;
 using Windows.Foundation;
 using Windows.Security.Authorization.AppCapabilityAccess;
 using Windows.Storage;
-using Windows.Win32;
-using Windows.Win32.Foundation;
-using Windows.Win32.System.Rpc;
 using WinRT;
 
 namespace Microsoft.Windows.DevHome.SDK;
@@ -25,10 +23,6 @@ internal class PluginInstanceManager<T> : IClassFactory
     private const int CLASS_E_NOAGGREGATION = unchecked((int)0x80040110);
 
     private const int E_ACCESSDENIED = unchecked((int)0x80070005);
-
-    private const RPC_STATUS RPC_S_NO_CALL_ACTIVE = (RPC_STATUS)1725;
-
-    private const RPC_STATUS RPC_S_OK = (RPC_STATUS)0;
 
     private static readonly Guid IID_IUnknown = Guid.Parse("00000000-0000-0000-C000-000000000046");
 
@@ -80,51 +74,8 @@ internal class PluginInstanceManager<T> : IClassFactory
 
     private bool ComCallerHasPluginHostCapability()
     {
-        bool succeeded = false;
-        uint callerProcessId = 0;
-        (succeeded, callerProcessId) = GetCallerProcessId();
-        if (succeeded)
-        {
-            // Get the caller process id and use it to check if the caller has permissions to access the feature.
-            AppCapabilityAccessStatus status = AppCapabilityAccessStatus.DeniedBySystem;
-
-            AppCapability capability = AppCapability.CreateWithProcessIdForUser(null, "devicePortalProvider", callerProcessId);
-            status = capability.CheckAccess();
-
-            return (status == AppCapabilityAccessStatus.Allowed);
-        }
 
         return false;
-    }
-
-    private static ulong HandleToULong(HANDLE h)
-    {
-        return (ulong)(IntPtr)h;
-    }
-
-    unsafe private (bool, uint) GetCallerProcessId()
-    {
-        RPC_STATUS rpcStatus = 0;
-        RPC_CALL_ATTRIBUTES_V2_W callAttributes;
-        callAttributes.Version = 2;
-        callAttributes.Flags = 0x10;
-
-        rpcStatus = PInvoke.RpcServerInqCallAttributes(null, (void*)&callAttributes);
-        
-        if (rpcStatus == RPC_S_NO_CALL_ACTIVE ||
-            (rpcStatus == RPC_S_OK && HandleToULong(callAttributes.ClientPID) == PInvoke.GetCurrentProcessId()))
-        {
-            return (true, PInvoke.GetCurrentProcessId());
-        }
-        else if (rpcStatus == RPC_S_OK)
-        {
-            // out-of-proc case.
-            return (true, (uint)HandleToULong(callAttributes.ClientPID));
-        }
-        else
-        {
-            return (false, 0);
-        }
     }
 }
 
