244262a75f954f171098dd168b3c983554ca8d45
Kristen Schau <47155823+krschau@users.noreply.github.com>
2023-03-22 13:15:41 -0700

    Update icon

--- src/Views/ShellPage.xaml.cs
+++ src/Views/ShellPage.xaml.cs
@@ -37,7 +37,6 @@ public sealed partial class ShellPage : Page
 
         UpdateNavigationMenuItems();
 
-        // TODO: Set the title bar icon by updating /Assets/WindowIcon.ico.
         // A custom title bar is required for full window theme and Mica support.
         // https://docs.microsoft.com/windows/apps/develop/title-bar?tabs=winui3#full-customization
         App.MainWindow.ExtendsContentIntoTitleBar = true;
