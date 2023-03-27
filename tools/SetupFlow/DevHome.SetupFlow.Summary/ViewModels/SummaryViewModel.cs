30be382a8eeb275e6aa753a773f0d319b73e7d3d
Darren Hoehna <dahoehna@microsoft.com>
2023-03-27 12:34:40 -0700

    First commit

--- tools/SetupFlow/DevHome.SetupFlow.Summary/ViewModels/SummaryViewModel.cs
+++ tools/SetupFlow/DevHome.SetupFlow.Summary/ViewModels/SummaryViewModel.cs
@@ -1,15 +1,20 @@
 ﻿// Copyright (c) Microsoft Corporation and Contributors
 // Licensed under the MIT license.
 
+using CommunityToolkit.Mvvm.ComponentModel;
 using DevHome.SetupFlow.Common.Services;
 using DevHome.SetupFlow.Common.ViewModels;
 using DevHome.Telemetry;
+using Microsoft.UI.Xaml;
 
 namespace DevHome.SetupFlow.Summary.ViewModels;
 
 public partial class SummaryViewModel : SetupPageViewModelBase
 {
     private readonly ILogger _logger;
+
+    [ObservableProperty]
+    private Visibility _showRestartNeeded;
 
     public SummaryViewModel(
         ISetupFlowStringResource stringResource,
