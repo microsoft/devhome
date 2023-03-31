b57ef623df0ca506859f60a8b5ae8b35b21c6332
Darren Hoehna <dhoehna@yahoo.com>
2023-03-31 13:26:09 -0700

    Summary page UI + other tweaks (#132)
    * First commit
    
    * Summary page v0.2
    
    * Summary page v1
    
    * WIP
    
    * Added final strings
    
    * Adding keys
    
    * Forgot to save this file.
    
    * Reverting changes to view
    
    * Changing version back
    
    * Removingtest data
    
    * Using resource
    
    * Removing unused strings.  Removing devdrive error because one already exists.
    
    * Making the restart box collapsed.
    
    * Adressing comments
    
    * Fixing build errors
    
    ---------
    
    Co-authored-by: Darren Hoehna <dahoehna@microsoft.com>

--- tools/SetupFlow/DevHome.SetupFlow.Summary/ViewModels/SummaryViewModel.cs
+++ tools/SetupFlow/DevHome.SetupFlow.Summary/ViewModels/SummaryViewModel.cs
@@ -1,10 +1,15 @@
 ﻿// Copyright (c) Microsoft Corporation and Contributors
 // Licensed under the MIT license.
 
+using System;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using DevHome.SetupFlow.Common.Services;
 using DevHome.SetupFlow.Common.ViewModels;
 using DevHome.Telemetry;
+using Microsoft.UI.Xaml;
 
 namespace DevHome.SetupFlow.Summary.ViewModels;
 
@@ -12,6 +17,27 @@ public partial class SummaryViewModel : SetupPageViewModelBase
 {
     private readonly ILogger _logger;
     private readonly SetupFlowOrchestrator _orchestrator;
+
+    [ObservableProperty]
+    private Visibility _showRestartNeeded;
+
+    [ObservableProperty]
+    private ObservableCollection<string> _repositoriesCloned;
+
+    [ObservableProperty]
+    private ObservableCollection<string> _appsDownloaded;
+
+    [RelayCommand]
+    public void OpenDashboard()
+    {
+        throw new NotImplementedException();
+    }
+
+    [RelayCommand]
+    public void RemoveRestartGrid()
+    {
+        _showRestartNeeded = Visibility.Collapsed;
+    }
 
     public SummaryViewModel(
         ISetupFlowStringResource stringResource,
@@ -24,6 +50,10 @@ public partial class SummaryViewModel : SetupPageViewModelBase
 
         IsNavigationBarVisible = false;
         IsStepPage = false;
+
+        _showRestartNeeded = Visibility.Collapsed;
+        _repositoriesCloned = new ();
+        _appsDownloaded = new ();
     }
 
     protected async override Task OnFirstNavigateToAsync()
