862cb7860f241238044d7689e7347a898020d1b1
Flor Elisa Chacon Ochoa <florch@microsoft.com>
2023-04-04 12:55:34 -0700

    Merge projects

--- tools/SetupFlow/DevHome.SetupFlow/Extensions/ServiceExtensions.cs
+++ tools/SetupFlow/DevHome.SetupFlow/Extensions/ServiceExtensions.cs
@@ -1,43 +1,16 @@
 ﻿// Copyright (c) Microsoft Corporation and Contributors
 // Licensed under the MIT license.
 
-using DevHome.SetupFlow.AppManagement.Extensions;
-using DevHome.SetupFlow.Common.Services;
-using DevHome.SetupFlow.ConfigurationFile.Extensions;
-using DevHome.SetupFlow.DevDrive.Extensions;
-using DevHome.SetupFlow.Loading.Extensions;
-using DevHome.SetupFlow.MainPage.Extensions;
-using DevHome.SetupFlow.RepoConfig.Extensions;
-using DevHome.SetupFlow.Review.Extensions;
-using DevHome.SetupFlow.Summary.Extensions;
-using DevHome.SetupFlow.ViewModels;
+using DevHome.SetupFlow.Summary.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-namespace DevHome.SetupFlow.Extensions;
+namespace DevHome.SetupFlow.Summary.Extensions;
 public static class ServiceExtensions
 {
-    public static IServiceCollection AddSetupFlow(this IServiceCollection services, HostBuilderContext context)
+    public static IServiceCollection AddSummary(this IServiceCollection services)
     {
-        // Project services
-        services.AddAppManagement();
-        services.AddConfigurationFile();
-        services.AddDevDrive();
-        services.AddLoading();
-        services.AddMainPage();
-        services.AddRepoConfig();
-        services.AddReview();
-        services.AddSummary();
-
-        // View-models
-        services.AddTransient<SetupFlowViewModel>();
-
-        // Services
-        services.AddSingleton<ISetupFlowStringResource, SetupFlowStringResource>();
-        services.AddSingleton<SetupFlowOrchestrator>();
-
-        // Configurations
-        services.Configure<SetupFlowOptions>(context.Configuration.GetSection(nameof(SetupFlowOptions)));
+        // View models
+        services.AddTransient<SummaryViewModel>();
 
         return services;
     }
