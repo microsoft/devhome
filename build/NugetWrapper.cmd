59cb0563eb0d0857160cf42ca11e2c1b276dba70
Manodasan Wignarajah <mawign@microsoft.com>
2023-04-04 11:32:44 -0700

    Update nuget version

--- build/NugetWrapper.cmd
+++ build/NugetWrapper.cmd
@@ -5,12 +5,12 @@ if "%VisualStudioVersion%" == "" set VisualStudioVersion=15.0
 
 if defined NUGETEXETOOLPATH goto :AzurePipelines
 
-if not exist %TEMP%\nuget.5.8.0-preview.2.exe (
+if not exist %TEMP%\nuget.6.4.0.exe (
     echo Nuget.exe not found in the temp dir, downloading.
-    powershell -Command "& { Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/v5.8.0-preview.2/nuget.exe -outfile $env:TEMP\nuget.5.8.0-preview.2.exe }"
+    powershell -Command "& { Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/v6.4.0/nuget.exe -outfile $env:TEMP\nuget.6.4.0.exe }"
 )
 
-%TEMP%\nuget.5.8.0-preview.2.exe %*
+%TEMP%\nuget.6.4.0.exe %*
 
 exit /B %ERRORLEVEL%
 
