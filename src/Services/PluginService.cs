5bedec5a16a86d4984de968f3c55e8a7e16972ea
Eric Johnson <ejohn@microsoft.com>
2023-04-05 14:40:09 -0700

    Adding extension install/uninstall/update event listeners

--- src/Services/PluginService.cs
+++ src/Services/PluginService.cs
@@ -1,85 +1,120 @@
 ﻿// Copyright (c) Microsoft Corporation and Contributors
 // Licensed under the MIT license.
 
-using System.Collections;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
-using CommunityToolkit.Mvvm.ComponentModel;
-using DevHome.Common;
 using DevHome.Common.Contracts;
 using DevHome.Common.Extensions;
 using DevHome.Common.Services;
-using DevHome.Contracts.Services;
-using DevHome.Helpers;
 using DevHome.Models;
-using DevHome.SetupFlow.RepoConfig;
-using DevHome.ViewModels;
-using DevHome.Views;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
 using Microsoft.Windows.DevHome.SDK;
-using Newtonsoft.Json;
-using SampleTool;
+using Windows.ApplicationModel;
 using Windows.ApplicationModel.AppExtensions;
-using Windows.ApplicationModel.Background;
-using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.Storage;
-using WinRT;
 
 namespace DevHome.Services;
 
 public class PluginService : IPluginService
 {
+    private static readonly PackageCatalog _catalog = PackageCatalog.OpenForCurrentUser();
+    private static readonly object _lock = new ();
+
 #pragma warning disable IDE0044 // Add readonly modifier
-    private static List<IPluginWrapper> installedPlugins = new ();
+    private static List<IPluginWrapper> _installedPlugins = new ();
+    private static List<IPluginWrapper> _enabledPlugins = new ();
 #pragma warning restore IDE0044 // Add readonly modifier
+
+    public PluginService()
+    {
+        _catalog.PackageInstalling += Catalog_PackageInstalling;
+        _catalog.PackageUninstalling += Catalog_PackageUninstalling;
+        _catalog.PackageUpdating += Catalog_PackageUpdating;
+    }
+
+    private void Catalog_PackageInstalling(PackageCatalog sender, PackageInstallingEventArgs args)
+    {
+        OnPackageChange(args.Package);
+    }
+
+    private void Catalog_PackageUninstalling(PackageCatalog sender, PackageUninstallingEventArgs args)
+    {
+        OnPackageChange(args.Package);
+    }
+
+    private void Catalog_PackageUpdating(PackageCatalog sender, PackageUpdatingEventArgs args)
+    {
+        OnPackageChange(args.TargetPackage);
+    }
+
+    private async void OnPackageChange(Package package)
+    {
+        var isDevHomeExtension = await IsValidDevHomeExtension(package);
+        if (isDevHomeExtension)
+        {
+            lock (_lock)
+            {
+                _installedPlugins.Clear();
+                _enabledPlugins.Clear();
+            }
+        }
+    }
+
+    private async Task<bool> IsValidDevHomeExtension(Package package)
+    {
+        var extensions = await AppExtensionCatalog.Open("com.microsoft.devhome").FindAllAsync();
+        foreach (var extension in extensions)
+        {
+            if (package.Id.FullName == extension.Package.Id.FullName)
+            {
+                var (devHomeProvider, classId) = await GetDevHomeExtensionPropertiesAsync(extension);
+                return devHomeProvider != null && classId != null;
+            }
+        }
+
+        return false;
+    }
+
+    private async Task<(IPropertySet?, string?)> GetDevHomeExtensionPropertiesAsync(AppExtension extension)
+    {
+        var properties = await extension.GetExtensionPropertiesAsync();
+
+        var devHomeProvider = GetSubPropertySet(properties, "DevHomeProvider");
+        if (devHomeProvider is null)
+        {
+            return (null, null);
+        }
+
+        var activation = GetSubPropertySet(devHomeProvider, "Activation");
+        if (activation is null)
+        {
+            return (devHomeProvider, null);
+        }
+
+        var comActivation = GetSubPropertySet(activation, "CreateInstance");
+        if (comActivation is null)
+        {
+            return (devHomeProvider, null);
+        }
+
+        var classId = GetProperty(comActivation, "@ClassId");
+        if (classId is null)
+        {
+            return (devHomeProvider, null);
+        }
+
+        return (devHomeProvider, classId);
+    }
 
     public async Task<IEnumerable<IPluginWrapper>> GetInstalledPluginsAsync(bool includeDisabledPlugins = false)
     {
-        if (installedPlugins.Count == 0)
+        if (_installedPlugins.Count == 0)
         {
             var extensions = await AppExtensionCatalog.Open("com.microsoft.devhome").FindAllAsync();
             foreach (var extension in extensions)
             {
-                var properties = await extension.GetExtensionPropertiesAsync();
-
-                var devHomeProvider = GetSubPropertySet(properties, "DevHomeProvider");
-                if (devHomeProvider is null)
+                var (devHomeProvider, classId) = await GetDevHomeExtensionPropertiesAsync(extension);
+                if (devHomeProvider == null || classId == null)
                 {
                     continue;
-                }
-
-                var activation = GetSubPropertySet(devHomeProvider, "Activation");
-                if (activation is null)
-                {
-                    continue;
-                }
-
-                var comActivation = GetSubPropertySet(activation, "CreateInstance");
-                if (comActivation is null)
-                {
-                    continue;
-                }
-
-                var classId = GetProperty(comActivation, "@ClassId");
-                if (classId is null)
-                {
-                    continue;
-                }
-
-                if (!includeDisabledPlugins)
-                {
-                    var isDisabled = Task.Run(() =>
-                    {
-                        var localSettingsService = Application.Current.GetService<ILocalSettingsService>();
-                        return localSettingsService.ReadSettingAsync<bool>(classId + "-ExtensionDisabled");
-                    }).Result;
-                    if (isDisabled)
-                    {
-                        continue;
-                    }
                 }
 
                 var name = extension.DisplayName;
@@ -102,11 +137,21 @@ public class PluginService : IPluginService
                     }
                 }
 
-                installedPlugins.Add(pluginWrapper);
+                var isPluginDisabled = Task.Run(() =>
+                {
+                    var localSettingsService = Application.Current.GetService<ILocalSettingsService>();
+                    return localSettingsService.ReadSettingAsync<bool>(classId + "-ExtensionDisabled");
+                }).Result;
+
+                _installedPlugins.Add(pluginWrapper);
+                if (!isPluginDisabled)
+                {
+                    _enabledPlugins.Add(pluginWrapper);
+                }
             }
         }
 
-        return installedPlugins;
+        return includeDisabledPlugins ? _installedPlugins : _enabledPlugins;
     }
 
     public async Task<IEnumerable<IPluginWrapper>> StartAllPluginsAsync()
