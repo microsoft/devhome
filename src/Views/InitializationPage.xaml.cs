9de25207dc2d1a37c0f68fe9d9e2ccfbf309730a
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2023-04-07 16:57:50 -0700

    Remove using causing a crash (#172)
    Co-authored-by: Eric Johnson <ericjohnson327@gmail.com>

--- src/Views/InitializationPage.xaml.cs
+++ src/Views/InitializationPage.xaml.cs
@@ -1,19 +1,9 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
-using System.Management.Automation;
-using DevHome.Common.Contracts;
-using DevHome.Common.Extensions;
-using DevHome.Contracts.Services;
-using DevHome.SetupFlow.Common.WindowsPackageManager;
-using DevHome.SetupFlow.Models;
-using DevHome.SetupFlow.Services;
-using DevHome.Telemetry;
 using DevHome.ViewModels;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Windows.ApplicationModel.Store.Preview.InstallControl;
 
 namespace DevHome.Views;
 
