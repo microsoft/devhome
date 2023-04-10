6d0e3784448d253ebdbbcf50dc9c0e881fa5136f
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2023-04-10 00:00:10 -0700

    Use orchestrator event

--- tools/SetupFlow/DevHome.SetupFlow/Behaviors/SetupFlowNavigationBehavior.cs
+++ tools/SetupFlow/DevHome.SetupFlow/Behaviors/SetupFlowNavigationBehavior.cs
@@ -2,7 +2,9 @@
 // Licensed under the MIT license.
 
 using System;
+using DevHome.Common.Extensions;
 using DevHome.SetupFlow.Controls;
+using DevHome.SetupFlow.Services;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.Xaml.Interactivity;
@@ -10,111 +12,127 @@ using Microsoft.Xaml.Interactivity;
 namespace DevHome.SetupFlow.Behaviors;
 
 /// <summary>
-/// Behavior class for customizing the setup flow navigation content
+/// Behavior class for customizing the setup flow navigation
 /// </summary>
 public class SetupFlowNavigationBehavior : Behavior<SetupFlowNavigation>
 {
+    /// <summary>
+    /// Singleton instance of this behavior class implemented by the setup flow
+    /// root page <see cref="SetupFlowNavigation"/> element.
+    /// </summary>
     private static SetupFlowNavigationBehavior _instance;
+
+    /// <summary>
+    /// Gets or sets the default template for the cancel control
+    /// </summary>
+    public object DefaultCancelTemplate { get; set; }
+
+    /// <summary>
+    /// Getter for the attached property <see cref="CancelTemplateProperty"/>
+    /// </summary>
+    /// <param name="control">Target user control</param>
+    /// <returns>Cancel object</returns>
+    public static object GetCancelTemplate(UserControl control) => control.GetValue(CancelTemplateProperty);
+
+    /// <summary>
+    /// Setter for the attached property <see cref="CancelTemplateProperty"/>
+    /// </summary>
+    /// <param name="control">Target control</param>
+    /// <param name="template">Cancel object</param>
+    public static void SetCancelTemplate(UserControl control, object template) => control.SetValue(CancelTemplateProperty, template);
+
+    /// <summary>
+    /// Gets or sets the default template for the previous control
+    /// </summary>
+    public object DefaultPreviousTemplate { get; set; }
+
+    /// <summary>
+    /// Getter for the attached property <see cref="PreviousTemplateProperty"/>
+    /// </summary>
+    /// <param name="control">Target user control</param>
+    /// <returns>Previous object</returns>
+    public static object GetPreviousTemplate(UserControl control) => control.GetValue(PreviousTemplateProperty);
+
+    /// <summary>
+    /// Setter for the attached property <see cref="PreviousTemplateProperty"/>
+    /// </summary>
+    /// <param name="control">Target control</param>
+    /// <param name="template">Previous object</param>
+    public static void SetPreviousTemplate(UserControl control, object template) => control.SetValue(PreviousTemplateProperty, template);
+
+    /// <summary>
+    /// Gets or sets the default template for the next control
+    /// </summary>
+    public object DefaultNextTemplate { get; set; }
+
+    /// <summary>
+    /// Getter for the attached property <see cref="NextTemplateProperty"/>
+    /// </summary>
+    /// <param name="control">Target user control</param>
+    /// <returns>Next object</returns>
+    public static object GetNextTemplate(UserControl control) => control.GetValue(NextTemplateProperty);
+
+    /// <summary>
+    /// Setter for the attached property <see cref="NextTemplateProperty"/>
+    /// </summary>
+    /// <param name="control">Target control</param>
+    /// <param name="template">Next object</param>
+    public static void SetNextTemplate(UserControl control, object template) => control.SetValue(NextTemplateProperty, template);
+
+    /// <summary>
+    /// Gets or sets the default template for the content control
+    /// </summary>
+    public object DefaultContentTemplate { get; set; }
+
+    /// <summary>
+    /// Getter for the attached property <see cref="ContentTemplateProperty"/>
+    /// </summary>
+    /// <param name="control">Target user control</param>
+    /// <returns>Content object</returns>
+    public static object GetContentTemplate(UserControl control) => control.GetValue(ContentTemplateProperty);
+
+    /// <summary>
+    /// Setter for the attached property <see cref="ContentTemplateProperty"/>
+    /// </summary>
+    /// <param name="control">Target control</param>
+    /// <param name="template">Content object</param>
+    public static void SetContentTemplate(UserControl control, object template) => control.SetValue(ContentTemplateProperty, template);
+
+    // List of all DependencyProperty
+    public static readonly DependencyProperty ContentTemplateProperty = DependencyProperty.RegisterAttached("ContentTemplate", typeof(object), typeof(SetupFlowNavigationBehavior), new PropertyMetadata(null, (_, e) => UpdateContentTemplate(e.NewValue)));
+    public static readonly DependencyProperty CancelTemplateProperty = DependencyProperty.RegisterAttached("CancelTemplate", typeof(object), typeof(SetupFlowNavigationBehavior), new PropertyMetadata(null, (_, e) => UpdateCancelTemplate(e.NewValue)));
+    public static readonly DependencyProperty PreviousTemplateProperty = DependencyProperty.RegisterAttached("PreviousTemplate", typeof(object), typeof(SetupFlowNavigationBehavior), new PropertyMetadata(null, (_, e) => UpdatePreviousTemplate(e.NewValue)));
+    public static readonly DependencyProperty NextTemplateProperty = DependencyProperty.RegisterAttached("NextTemplate", typeof(object), typeof(SetupFlowNavigationBehavior), new PropertyMetadata(null, (_, e) => UpdateNextTemplate(e.NewValue)));
 
     protected override void OnAttached()
     {
         base.OnAttached();
-        _instance = this;
 
-        // Initialize to default template and values
-        UpdateContentTemplate(_instance.DefaultContentTemplate);
-        UpdateCancelTemplate(_instance.DefaultCancelTemplate);
-        UpdatePreviousTemplate(_instance.DefaultPreviousTemplate);
-        UpdateNextTemplate(_instance.DefaultNextTemplate);
+        var orchestrator = Application.Current.GetService<SetupFlowOrchestrator>();
+        orchestrator.PageChanging += OnPageChanging;
+
+        _instance = this;
+        ResetToDefault();
     }
 
     protected override void OnDetaching()
     {
         base.OnDetaching();
+
+        var orchestrator = Application.Current.GetService<SetupFlowOrchestrator>();
+        orchestrator.PageChanging -= OnPageChanging;
+
         _instance = null;
     }
 
-    private static void SetTemporaryValue(UserControl control, DependencyProperty property, object value, Action defaultValueAction)
-    {
-        control.SetValue(property, value);
-        control.Unloaded += (_, _) => defaultValueAction();
-    }
+    private void OnPageChanging(object sender, EventArgs args) => ResetToDefault();
 
-    /***
-     * Cancel
-     */
-
-    public object DefaultCancelTemplate { get; set; }
-
-    public static object GetCancelTemplate(UserControl nav) => nav.GetValue(CancelTemplateProperty);
-
-    public static void SetCancelTemplate(UserControl nav, object template) =>
-        SetTemporaryValue(nav, CancelTemplateProperty, template, () => UpdateCancelTemplate(_instance?.DefaultCancelTemplate));
-
-    public static void UpdateCancelTemplate(object templateObject)
-    {
-        if (_instance != null)
-        {
-            _instance.AssociatedObject.CancelTemplate = templateObject;
-        }
-    }
-
-    public static readonly DependencyProperty CancelTemplateProperty = DependencyProperty.RegisterAttached("CancelTemplate", typeof(object), typeof(SetupFlowNavigationBehavior), new PropertyMetadata(null, (_, e) => UpdateCancelTemplate(e.NewValue)));
-
-    /***
-     * Previous
-     */
-
-    public object DefaultPreviousTemplate { get; set; }
-
-    public static object GetPreviousTemplate(UserControl nav) => nav.GetValue(PreviousTemplateProperty);
-
-    public static void SetPreviousTemplate(UserControl nav, object template) =>
-        SetTemporaryValue(nav, PreviousTemplateProperty, template, () => UpdatePreviousTemplate(_instance?.DefaultPreviousTemplate));
-
-    public static void UpdatePreviousTemplate(object templateObject)
-    {
-        if (_instance != null)
-        {
-            _instance.AssociatedObject.PreviousTemplate = templateObject;
-        }
-    }
-
-    public static readonly DependencyProperty PreviousTemplateProperty = DependencyProperty.RegisterAttached("PreviousTemplate", typeof(object), typeof(SetupFlowNavigationBehavior), new PropertyMetadata(null, (_, e) => UpdatePreviousTemplate(e.NewValue)));
-
-    /***
-     * Next
-     */
-
-    public object DefaultNextTemplate { get; set; }
-
-    public static object GetNextTemplate(UserControl nav) => nav.GetValue(NextTemplateProperty);
-
-    public static void SetNextTemplate(UserControl nav, object template) =>
-        SetTemporaryValue(nav, NextTemplateProperty, template, () => UpdateNextTemplate(_instance?.DefaultNextTemplate));
-
-    public static void UpdateNextTemplate(object templateObject)
-    {
-        if (_instance != null)
-        {
-            _instance.AssociatedObject.NextTemplate = templateObject;
-        }
-    }
-
-    public static readonly DependencyProperty NextTemplateProperty = DependencyProperty.RegisterAttached("NextTemplate", typeof(object), typeof(SetupFlowNavigationBehavior), new PropertyMetadata(null, (_, e) => UpdateNextTemplate(e.NewValue)));
-
-    /***
-     * Content
-     */
-
-    public object DefaultContentTemplate { get; set; }
-
-    public static object GetContentTemplate(UserControl nav) => nav.GetValue(ContentTemplateProperty);
-
-    public static void SetContentTemplate(UserControl nav, object template) =>
-        SetTemporaryValue(nav, ContentTemplateProperty, template, () => UpdateContentTemplate(_instance?.DefaultContentTemplate));
-
-    public static void UpdateContentTemplate(object templateObject)
+    /// <summary>
+    /// Sets the template of the associated <see cref="SetupFlowNavigation.ContentTemplate"/>
+    /// in the setup flow navigation
+    /// </summary>
+    /// <param name="templateObject">Template value</param>
+    private static void UpdateContentTemplate(object templateObject)
     {
         if (_instance != null)
         {
@@ -122,5 +140,54 @@ public class SetupFlowNavigationBehavior : Behavior<SetupFlowNavigation>
         }
     }
 
-    public static readonly DependencyProperty ContentTemplateProperty = DependencyProperty.RegisterAttached("ContentTemplate", typeof(object), typeof(SetupFlowNavigationBehavior), new PropertyMetadata(null, (_, e) => UpdateContentTemplate(e.NewValue)));
+    /// <summary>
+    /// Sets the template of the associated <see cref="SetupFlowNavigation.PreviousTemplate"/>
+    /// in the setup flow navigation
+    /// </summary>
+    /// <param name="templateObject">Template value</param>
+    private static void UpdatePreviousTemplate(object templateObject)
+    {
+        if (_instance != null)
+        {
+            _instance.AssociatedObject.PreviousTemplate = templateObject;
+        }
+    }
+
+    /// <summary>
+    /// Sets the template of the associated <see cref="SetupFlowNavigation.CancelTemplate"/>
+    /// in the setup flow navigation
+    /// </summary>
+    /// <param name="templateObject">Template value</param>
+    private static void UpdateCancelTemplate(object templateObject)
+    {
+        if (_instance != null)
+        {
+            _instance.AssociatedObject.CancelTemplate = templateObject;
+        }
+    }
+
+    /// <summary>
+    /// Sets the template of the associated <see cref="SetupFlowNavigation.NextTemplate"/>
+    /// in the setup flow navigation
+    /// </summary>
+    /// <param name="templateObject">Template value</param>
+    private static void UpdateNextTemplate(object templateObject)
+    {
+        if (_instance != null)
+        {
+            _instance.AssociatedObject.NextTemplate = templateObject;
+        }
+    }
+
+    /// <summary>
+    /// Resets all the navigation component templates to their default values
+    /// </summary>
+    private void ResetToDefault()
+    {
+        // Initialize to default template and values
+        UpdateContentTemplate(_instance.DefaultContentTemplate);
+        UpdateCancelTemplate(_instance.DefaultCancelTemplate);
+        UpdatePreviousTemplate(_instance.DefaultPreviousTemplate);
+        UpdateNextTemplate(_instance.DefaultNextTemplate);
+    }
 }
