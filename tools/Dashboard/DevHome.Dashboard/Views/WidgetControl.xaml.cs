62aafdb2602a4f1246116c786bdb24237b7f0f01
Kristen Schau <47155823+krschau@users.noreply.github.com>
2023-04-11 14:07:36 -0400

    Show scroll bars inside scrollable widgets (#194)

--- tools/Dashboard/DevHome.Dashboard/Views/WidgetControl.xaml.cs
+++ tools/Dashboard/DevHome.Dashboard/Views/WidgetControl.xaml.cs
@@ -18,6 +18,8 @@ public sealed partial class WidgetControl : UserControl
     public WidgetControl()
     {
         this.InitializeComponent();
+
+        WidgetScrollViewer.RegisterPropertyChangedCallback(ScrollViewer.ComputedVerticalScrollBarVisibilityProperty, OnWidgetScrollBarVisibilityChanged);
     }
 
     public WidgetViewModel WidgetSource
@@ -28,6 +30,21 @@ public sealed partial class WidgetControl : UserControl
 
     public static readonly DependencyProperty WidgetSourceProperty = DependencyProperty.Register(
         nameof(WidgetSource), typeof(WidgetViewModel), typeof(WidgetControl), new PropertyMetadata(null));
+
+    private void OnWidgetScrollBarVisibilityChanged(DependencyObject sender, DependencyProperty dp)
+    {
+        var padding = new Thickness(0, 0, 0, 0);
+
+        if (sender as ScrollViewer is ScrollViewer sv)
+        {
+            if (sv.ComputedVerticalScrollBarVisibility == Visibility.Visible)
+            {
+                padding.Right = 13;
+            }
+        }
+
+        WidgetScrollViewer.Padding = padding;
+    }
 
     private void OpenWidgetMenu(object sender, RoutedEventArgs e)
     {
