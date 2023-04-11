d91a5242eb7e6d9a38846f0fde92fb3013e3cd31
Flor Elisa Chacon Ochoa <florch@microsoft.com>
2023-04-11 11:58:37 -0700

    missing using

--- tools/SetupFlow/DevHome.SetupFlow/ViewModels/RepoConfigViewModel.cs
+++ tools/SetupFlow/DevHome.SetupFlow/ViewModels/RepoConfigViewModel.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
 using DevHome.Common.Services;
 using DevHome.SetupFlow.Helpers;
