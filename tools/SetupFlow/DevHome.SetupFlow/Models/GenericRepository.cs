f6259277928e848ee851a2f3903878f7e182b728
Darren Hoehna <dahoehna@microsoft.com>
2023-04-13 13:04:32 -0700

    Fallback to git url if no providers can parse the URL

---
+++ tools/SetupFlow/DevHome.SetupFlow/Models/GenericRepository.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors
+// Licensed under the MIT license.
+
+using System;
+using System.Threading.Tasks;
+using DevHome.Dashboard.Helpers;
+using LibGit2Sharp;
+using Microsoft.Windows.DevHome.SDK;
+using Windows.Foundation;
+using Windows.Win32.Storage.FileSystem;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+
+namespace DevHome.SetupFlow.Models;
+internal class GenericRepository : Microsoft.Windows.DevHome.SDK.IRepository
+{
+    private readonly string _displayName;
+
+    public string DisplayName => _displayName;
+
+    public bool IsPrivate => false;
+
+    public DateTimeOffset LastUpdated => DateTime.UtcNow;
+
+    public string OwningAccountName => "Unknown";
+
+    private readonly Uri _cloneUri;
+
+    public GenericRepository(Uri cloneUri)
+    {
+        _displayName = cloneUri.Segments[cloneUri.Segments.Length - 1].ToString().Replace("/", string.Empty);
+        _cloneUri = cloneUri;
+    }
+
+    public IAsyncAction CloneRepositoryAsync(string cloneDestination, IDeveloperId developerId)
+    {
+        return Task.Run(() =>
+        {
+            if (!string.IsNullOrEmpty(cloneDestination))
+            {
+                var cloneOptions = new CloneOptions
+                {
+                    Checkout = true,
+                };
+
+                try
+                {
+                    LibGit2Sharp.Repository.Clone(_cloneUri.OriginalString, cloneDestination, cloneOptions);
+                }
+                catch (RecurseSubmodulesException recurseException)
+                {
+                    Log.Logger()?.ReportError("GenericRepository", "Could not clone all sub modules", recurseException);
+                    throw;
+                }
+                catch (UserCancelledException userCancelledException)
+                {
+                    Log.Logger()?.ReportError("GenericRepository", "The user stoped the clone operation", userCancelledException);
+                    throw;
+                }
+                catch (NameConflictException nameConflictException)
+                {
+                    Log.Logger()?.ReportError("GenericRepository", nameConflictException);
+                    throw;
+                }
+                catch (Exception e)
+                {
+                    Log.Logger()?.ReportError("GenericRepository", "Could not clone the repository", e);
+                    throw;
+                }
+            }
+        }).AsAsyncAction();
+    }
+
+    public IAsyncAction CloneRepositoryAsync(string cloneDestination) => throw new NotImplementedException();
+}
