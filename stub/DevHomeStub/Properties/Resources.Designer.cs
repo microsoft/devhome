db4ea42c1b2eaeb44a6042dd32297a8daaa9f412
Chris Wall (WIN SDE) <chriwall@microsoft.com>
2023-04-19 11:04:33 -0600

    Remove Dependency that caused the build break

--- stub/DevHomeStub/Properties/Resources.Designer.cs
+++ stub/DevHomeStub/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace DevHome.Stub.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap DevHomeIcon_scale_400 {
-            get {
-                object obj = ResourceManager.GetObject("DevHomeIcon_scale_400", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
