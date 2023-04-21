a70f1ee94e99a0262f6234a449574e4e6b221dfb
Kristen Schau <47155823+krschau@users.noreply.github.com>
2023-04-21 17:00:27 -0400

    Create custom Adaptive Card renderer for LabelGroups (#264)

---
+++ common/Renderers/LabelGroup.cs
@@ -0,0 +1,148 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors
+// Licensed under the MIT license.
+
+using System.Collections.Generic;
+using AdaptiveCards.ObjectModel.WinUI3;
+using AdaptiveCards.Rendering.WinUI3;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Windows.Data.Json;
+
+namespace DevHome.Common.Renderers;
+public class LabelGroup : IAdaptiveCardElement
+{
+    public LabelGroup()
+    {
+        Labels = new List<(string, string)>();
+    }
+
+    public List<(string, string)> Labels { get; set; }
+
+    public bool RoundedCorners { get; set; } = true;
+
+    public static readonly string CustomTypeString = "LabelGroup";
+
+    public JsonObject ToJson()
+    {
+        return new JsonObject();
+    }
+
+    public JsonObject? AdditionalProperties { get; set; }
+
+    public ElementType ElementType => ElementType.Custom;
+
+    public string ElementTypeString => CustomTypeString;
+
+    public IAdaptiveCardElement? FallbackContent { get; set; }
+
+    public FallbackType FallbackType { get; set; }
+
+    public HeightType Height { get; set; } = HeightType.Auto;
+
+    public string? Id { get; set; } = CustomTypeString + "Id";
+
+    public bool IsVisible { get; set; } = true;
+
+    public bool Separator { get; set; }
+
+    public Spacing Spacing { get; set; } = Spacing.Small;
+
+    IList<AdaptiveRequirement>? IAdaptiveCardElement.Requirements { get; }
+}
+
+public class LabelGroupParser : IAdaptiveElementParser
+{
+    public IAdaptiveCardElement FromJson(
+        JsonObject inputJson,
+        AdaptiveElementParserRegistration elementParsers,
+        AdaptiveActionParserRegistration actionParsers,
+        IList<AdaptiveWarning> warnings)
+    {
+        var labelGroup = new LabelGroup();
+
+        // Parse the labels and their colors.
+        var labels = inputJson.GetNamedArray("labels", null);
+        if (labels is not null)
+        {
+            foreach (var label in labels)
+            {
+                var obj = label.GetObject();
+                if (obj != null)
+                {
+                    var text = obj.GetNamedString("text", string.Empty).ToString();
+                    var color = obj.GetNamedString("color", string.Empty).ToString();
+                    labelGroup.Labels.Add((text, color));
+                }
+            }
+        }
+
+        // Parse whether the displayed labels should have rounded corners. If not specified, default to true.
+        var roundedCorners = inputJson.GetNamedBoolean("roundedCorners", true);
+        labelGroup.RoundedCorners = roundedCorners;
+
+        return labelGroup;
+    }
+}
+
+public class LabelGroupRenderer : IAdaptiveElementRenderer
+{
+    public UIElement Render(IAdaptiveCardElement element, AdaptiveRenderContext context, AdaptiveRenderArgs renderArgs)
+    {
+        var stackPanel = new StackPanel
+        {
+            Name = LabelGroup.CustomTypeString,
+            Orientation = Orientation.Horizontal,
+        };
+
+        if (element is LabelGroup labelGroup)
+        {
+            var labels = labelGroup.Labels;
+
+            // Each label is presented in a grid with background color and text.
+            foreach (var label in labels)
+            {
+                var grid = new Grid
+                {
+                    Background = GetBrushFromColor(label.Item2, 0.4),
+                    Padding = new Thickness(7, 2, 7, 2),
+                    Margin = new Thickness(0, 0, 10, 0),
+                };
+                if (labelGroup.RoundedCorners)
+                {
+                    grid.CornerRadius = new CornerRadius(7);
+                }
+
+                var tb = new TextBlock
+                {
+                    Text = label.Item1,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    FontSize = 12,
+                };
+                grid.Children.Add(tb);
+                stackPanel.Children.Add(grid);
+            }
+        }
+
+        return stackPanel;
+    }
+
+    /// <summary>
+    /// Create a SolidColorBrush from the given RGB color and opacity. If a color is not supplied, return a transparent brush.
+    /// </summary>
+    /// <param name="colorString">A string in 8-character RGB format.</param>
+    /// <param name="opacity">A percentage given as a double between 0 and 1.</param>
+    private Microsoft.UI.Xaml.Media.SolidColorBrush GetBrushFromColor(string colorString, double opacity)
+    {
+        if (!string.IsNullOrEmpty(colorString))
+        {
+            var a = (byte)System.Convert.ToUInt32(255 * opacity);
+            var r = (byte)System.Convert.ToUInt32(colorString[..2], 16);
+            var g = (byte)System.Convert.ToUInt32(colorString.Substring(2, 2), 16);
+            var b = (byte)System.Convert.ToUInt32(colorString.Substring(4, 2), 16);
+
+            return new Microsoft.UI.Xaml.Media.SolidColorBrush(Windows.UI.Color.FromArgb(a, r, g, b));
+        }
+
+        return new Microsoft.UI.Xaml.Media.SolidColorBrush(Microsoft.UI.Colors.Transparent);
+    }
+}
--- tools/Dashboard/DevHome.Dashboard/Views/DashboardView.xaml.cs
+++ tools/Dashboard/DevHome.Dashboard/Views/DashboardView.xaml.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AdaptiveCards.Rendering.WinUI3;
 using DevHome.Common;
+using DevHome.Common.Renderers;
 using DevHome.Dashboard.Helpers;
 using DevHome.Dashboard.ViewModels;
 using Microsoft.UI.Xaml;
@@ -110,7 +111,7 @@ public partial class DashboardView : ToolPage
         // so create a new renderer for those situations. We can't just temporarily edit the existing
         // renderer, because a pinned widget might get re-rendered the wrong way while the dialog is open.
         var configRenderer = new AdaptiveCardRenderer();
-        await SetHostConfigOnWidgetRenderer(configRenderer);
+        await ConfigureWidgetRenderer(configRenderer);
         configRenderer.HostConfig.ContainerStyles.Default.BackgroundColor = Microsoft.UI.Colors.Transparent;
         return configRenderer;
     }
@@ -118,11 +119,15 @@ public partial class DashboardView : ToolPage
     private async void OnActualThemeChanged(FrameworkElement sender, object args)
     {
         // The app uses a different host config to render widgets (adaptive cards) in light and dark themes.
-        await SetHostConfigOnWidgetRenderer(_renderer);
+        await ConfigureWidgetRenderer(_renderer);
     }
 
-    private async Task SetHostConfigOnWidgetRenderer(AdaptiveCardRenderer renderer)
+    private async Task ConfigureWidgetRenderer(AdaptiveCardRenderer renderer)
     {
+        // Add custom Adaptive Card renderer.
+        renderer.ElementRenderers.Set(LabelGroup.CustomTypeString, new LabelGroupRenderer());
+
+        // Add host config for current theme.
         var hostConfigContents = string.Empty;
         var hostConfigFileName = (ActualTheme == ElementTheme.Light) ? "HostConfigLight.json" : "HostConfigDark.json";
         try
@@ -289,7 +294,7 @@ public partial class DashboardView : ToolPage
     private async void RestorePinnedWidgets(object sender, RoutedEventArgs e)
     {
         // TODO: Ideally there would be some sort of visual loading indicator while the renderer gets set up.
-        await SetHostConfigOnWidgetRenderer(_renderer);
+        await ConfigureWidgetRenderer(_renderer);
 
         Log.Logger()?.ReportInfo("DashboardView", "Get widgets for current host");
         var pinnedWidgets = _widgetHost.GetWidgets();
