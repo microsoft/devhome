368e97aa8adb8e99235af073ae5a1c129ea150ab
Eric Johnson <ejohn@microsoft.com>
2023-04-26 17:09:19 -0700

    Cleanup merged code

--- settings/DevHome.Settings/ViewModels/AboutViewModel.cs
+++ settings/DevHome.Settings/ViewModels/AboutViewModel.cs
@@ -1,16 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation and Contributors
 // Licensed under the MIT license.
 
-using System;
-using System.Reflection;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.WinUI;
 using DevHome.Common.Extensions;
-using DevHome.Common.Helpers;
 using DevHome.Common.Services;
-using DevHome.Contracts.Services;
 using Microsoft.UI.Xaml;
-using Windows.ApplicationModel;
 
 namespace DevHome.Settings.ViewModels;
 
