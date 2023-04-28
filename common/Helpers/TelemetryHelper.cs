d6afc554fae66869244a963343e5e0d79c281a5d
sshilov7 <51001703+sshilov7@users.noreply.github.com>
2023-04-27 21:28:34 -0700

    Add Measure events for DevDriveCreated and Settings launched. (#287)
    * Add Measure events for DevDriveCreated and Settings launched.
    
    * Address review comments.

--- common/Helpers/TelemetryHelper.cs
+++ common/Helpers/TelemetryHelper.cs
@@ -4,6 +4,7 @@
 using System.Collections.Generic;
 using DevHome.Common.TelemetryEvents;
 using DevHome.Telemetry;
+using Microsoft.Diagnostics.Telemetry;
 using Microsoft.Windows.DevHome.SDK;
 
 namespace DevHome.Common.Telemetry;
@@ -49,4 +50,38 @@ public static class TelemetryHelper
 
     public static void LogAppInstallFailed(string packageId, string sourceId) =>
         LogError("AppInstall_InstallFailed", LogLevel.Critical, new AppInstallEvent(packageId, sourceId));
+
+    //// Dev Drive events
+
+    /// <summary>
+    /// Send Measure telemetry even indicating that a new Dev Drive creation operation was triggered.
+    /// </summary>
+    /// <param name="operationDuration">duration of the operation</param>
+    /// <param name="hr"> HRESULT of the operation</param>
+    /// <param name="sizeInBytes">Size of Dev Drive n bytes</param>
+    /// <param name="mediaType">Volume media type</param>
+    public static void LogCreateDevDriveTriggered(long operationDuration, int hr, ulong sizeInBytes, uint mediaType)
+    {
+        LoggerFactory.Get<ILogger>().Log("CreateDevDriveTriggered", LogLevel.Measure, new
+        {
+            PartA_PrivTags = PrivTags.ProductAndServiceUsage,
+            duration = operationDuration,
+            errorCode = $"0x{hr:X}",
+            volumeSizeInBytes = sizeInBytes,
+            diskMediaType = mediaType,
+        });
+    }
+
+    /// <summary>
+    /// Send Measure telemetry event indicating that user went to Disks and Volumes page in Settings
+    /// </summary>
+    /// <param name="eventSource">Event source (currently DevDriveView or MainPageView)</param>
+    public static void LogLaunchDisksAndVolumesSettingsPageTriggered(string eventSource)
+    {
+        LoggerFactory.Get<ILogger>().Log("LaunchDisksAndVolumesSettingsPageTriggered", LogLevel.Measure, new
+        {
+            PartA_PrivTags = PrivTags.ProductAndServiceUsage,
+            source = eventSource,
+        });
+    }
 }
