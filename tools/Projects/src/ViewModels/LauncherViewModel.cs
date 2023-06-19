6cfef0b6ee4d0b58752c144a003356870dbf484a
Alexander Sklar <asklar@microsoft.com>
2023-06-19 16:50:19 -0700

    custom environment variables, via ICreatingProcess

--- tools/Projects/src/ViewModels/LauncherViewModel.cs
+++ tools/Projects/src/ViewModels/LauncherViewModel.cs
@@ -2,11 +2,15 @@
 // Licensed under the MIT license.
 
 using System;
-using System.Diagnostics;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Newtonsoft.Json;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.Shell;
+using Windows.Win32.UI.WindowsAndMessaging;
 using static Windows.Win32.PInvoke;
 
 namespace DevHome.Projects.ViewModels;
@@ -19,22 +23,79 @@ public class LauncherViewModel : ObservableObject
 
     public string IconPath { get; set; }
 
+    public Dictionary<string, string> Environment { get; set; } = new ();
+
     [JsonIgnore]
     public WeakReference<ProjectViewModel> ProjectViewModel { get; set; }
+
+    // this class implements ICreatingProcess and is used to create a process with a custom environment
+    // it also implements IServiceProvider to allow the process to query for ICreatingProcess, and it is exposed through COM interop.
+    [ComVisible(true)]
+    [Guid("D9B1F7B3-3B5F-4B5D-8B5A-8F4F969C4D9E")]
+    internal class CreatingProcess : ICreatingProcess, Windows.Win32.System.Com.IServiceProvider
+    {
+        public Dictionary<string, string> Environment { get; init; } = new ();
+
+        public void OnCreating(ICreateProcessInputs pcpi)
+        {
+            foreach (var (key, value) in Environment)
+            {
+                pcpi.SetEnvironmentVariable(key, value);
+            }
+        }
+
+        public unsafe void QueryService(Guid* guidService, Guid* riid, void** ppvObject)
+        {
+            if (*guidService == typeof(ICreatingProcess).GUID)
+            {
+                *ppvObject = (void*)Marshal.GetComInterfaceForObject(this, typeof(ICreatingProcess));
+            }
+            else if (*guidService == typeof(Windows.Win32.System.Com.IServiceProvider).GUID)
+            {
+                *ppvObject = (void*)Marshal.GetComInterfaceForObject(this, typeof(Windows.Win32.System.Com.IServiceProvider));
+            }
+            else
+            {
+                *ppvObject = null;
+                throw new NotImplementedException();
+            }
+        }
+    }
 
     internal void Launch()
     {
         var cwd = ProjectViewModel?.TryGetTarget(out var p) == true ? p.ExpandedFilePath : null;
         var argv = GetArgv();
 
-        var psi = new ProcessStartInfo
+        unsafe
         {
-            FileName = argv[0],
-            Arguments = string.Join(" ", argv.Skip(1)),
-            WorkingDirectory = cwd,
-            UseShellExecute = true,
-        };
-        Process.Start(psi);
+            var open = stackalloc char[]
+            {
+                'o',
+                'p',
+                'e',
+                'n',
+                '\0',
+            };
+            var @params = string.Join(' ', argv.Skip(1));
+            fixed (char* file = argv[0], paramsPtr = @params, cwdPtr = cwd)
+            {
+                var cp = new CreatingProcess { Environment = Environment };
+                var execInfo = new SHELLEXECUTEINFOW
+                {
+                    cbSize = (uint)Marshal.SizeOf<SHELLEXECUTEINFOW>(),
+                    lpVerb = new PCWSTR(open),
+                    lpFile = file,
+                    lpParameters = paramsPtr,
+                    lpDirectory = cwdPtr,
+                    nShow = (int)SHOW_WINDOW_CMD.SW_SHOWNORMAL,
+                    fMask = SEE_MASK_FLAG_HINST_IS_SITE,
+                    hInstApp = new HINSTANCE(Marshal.GetIUnknownForObject(cp)),
+                };
+
+                PInvoke.ShellExecuteEx(ref execInfo);
+            }
+        }
     }
 
     public string[] GetArgv()
