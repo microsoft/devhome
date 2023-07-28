bf61c3db3e53f812cb1039f9eff08ef3d84e8f07
Kristen Schau <47155823+krschau@users.noreply.github.com>
2023-07-28 13:30:52 -0700

    Make Close buttons consistent (#1329)

---
+++ common/Views/CloseButton.xaml.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation and Contributors
+// Licensed under the MIT license.
+
+using Microsoft.UI.Xaml.Controls;
+
+namespace DevHome.Common.Views;
+public sealed partial class CloseButton : Button
+{
+    public CloseButton()
+    {
+        this.InitializeComponent();
+    }
+}
