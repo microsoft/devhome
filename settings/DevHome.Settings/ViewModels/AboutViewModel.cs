ebe59dbcbc4218c994d1b02a1d0998e680221503
Kristen Schau <47155823+krschau@users.noreply.github.com>
2023-07-31 16:08:24 -0700

    Add Copy button

--- settings/DevHome.Settings/ViewModels/AboutViewModel.cs
+++ settings/DevHome.Settings/ViewModels/AboutViewModel.cs
@@ -2,9 +2,13 @@
 // Licensed under the MIT license.
 
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using DevHome.Common.Extensions;
 using DevHome.Common.Services;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Windows.ApplicationModel.Resources;
+using Windows.ApplicationModel.DataTransfer;
 
 namespace DevHome.Settings.ViewModels;
 
@@ -24,5 +28,20 @@ public partial class AboutViewModel : ObservableObject
         var version = appInfoService.GetAppVersion();
 
         return $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+    }
+
+    [RelayCommand]
+    public void CopyAboutClick()
+    {
+        var resourceLoader = new ResourceLoader(ResourceLoader.GetDefaultResourceFilePath(), "DevHome.Settings/Resources");
+        var versionString = resourceLoader.GetString("Settings_About_Version/Text");
+        var fullString = versionString + "\t" + GetVersionDescription();
+
+        var dataPackage = new DataPackage
+        {
+            RequestedOperation = DataPackageOperation.Copy,
+        };
+        dataPackage.SetText(fullString);
+        Clipboard.SetContent(dataPackage);
     }
 }
