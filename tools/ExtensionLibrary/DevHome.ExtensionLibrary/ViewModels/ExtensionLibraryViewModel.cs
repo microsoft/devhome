c06c2e05240af261c3f48168ef7efa214903eef5
Kristen Schau <47155823+krschau@users.noreply.github.com>
2023-08-28 06:53:35 -0700

    Add blank page for Extensions (#1445)

---
+++ tools/ExtensionLibrary/DevHome.ExtensionLibrary/ViewModels/ExtensionLibraryViewModel.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors
+// Licensed under the MIT license.
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DevHome.ExtensionLibrary.ViewModels;
+
+public partial class ExtensionLibraryViewModel : ObservableObject
+{
+    public ExtensionLibraryViewModel()
+    {
+    }
+}
