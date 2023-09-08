35530c5933587ef38d4c03097de2349b62f8c7fc
Eric Johnson <ericjohnson327@gmail.com>
2023-09-08 15:28:39 -0700

    New SDK changes - implementation

--- pluginsdk/Microsoft.Windows.DevHome.SDK/RepositoriesResult.h
+++ pluginsdk/Microsoft.Windows.DevHome.SDK/RepositoriesResult.h
@@ -11,6 +11,10 @@ namespace winrt::Microsoft::Windows::DevHome::SDK::implementation
         RepositoriesResult(winrt::hresult const& e, hstring const& diagnosticText);
         winrt::Windows::Foundation::Collections::IIterable<winrt::Microsoft::Windows::DevHome::SDK::IRepository> Repositories();
         winrt::Microsoft::Windows::DevHome::SDK::ProviderOperationResult Result();
+
+    private:
+        std::shared_ptr<winrt::Microsoft::Windows::DevHome::SDK::ProviderOperationResult> _Result;
+        std::shared_ptr<winrt::Windows::Foundation::Collections::IIterable<winrt::Microsoft::Windows::DevHome::SDK::IRepository>> _Repositories;
     };
 }
 namespace winrt::Microsoft::Windows::DevHome::SDK::factory_implementation
