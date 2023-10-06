c37fc63ae0ae45a7f3abdf8522eed7c417e2ba94
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2023-10-06 13:06:31 -0700

    Added PackageDetails

---
+++ tools/SetupFlow/DevHome.SetupFlow/Controls/PackageDetailsSettingsCard.xaml.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation and Contributors
+// Licensed under the MIT license.
+
+using CommunityToolkit.WinUI.Controls;
+
+namespace DevHome.SetupFlow.Controls;
+public sealed partial class PackageDetailsSettingsCard : SettingsCard
+{
+    public PackageDetailsSettingsCard()
+    {
+        this.InitializeComponent();
+    }
+}
