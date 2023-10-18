3ceed2634d1e2ac82038c19110e995be9efb7846
David Bennett <dbennett-msft@outlook.com>
2023-10-18 14:09:40 -0700

    Add DevHomeClosed Telemetry Event

---
+++ src/TelemetryEvents/DevHomeClosedEvent.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors
+// Licensed under the MIT license.
+
+using System.Diagnostics.Tracing;
+using DevHome.Logging;
+using DevHome.Telemetry;
+using Microsoft.Diagnostics.Telemetry;
+using Microsoft.Diagnostics.Telemetry.Internal;
+
+namespace DevHome.TelemetryEvents;
+
+[EventData]
+public class DevHomeClosedEvent : EventBase
+{
+    public double ElapsedTime
+    {
+        get;
+    }
+
+    public override PartA_PrivTags PartA_PrivTags => PrivTags.ProductAndServiceUsage;
+
+    public DevHomeClosedEvent(DateTime startTime)
+    {
+        ElapsedTime = (DateTime.UtcNow - startTime).TotalMilliseconds;
+        GlobalLog.Logger?.ReportDebug($"DevHome Closed Event, ElapsedTime: {ElapsedTime}ms");
+    }
+
+    public override void ReplaceSensitiveStrings(Func<string, string> replaceSensitiveStrings)
+    {
+        // No sensitive strings to replace.
+    }
+}
