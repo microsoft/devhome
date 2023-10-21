690dfb278578c3a0e973c1a873ae598ce30cdd6e
Alexander Sklar <asklar@microsoft.com>
2023-10-21 03:31:00 -0700

    .

--- tools/Projects/src/Views/ProjectView.xaml.cs
+++ tools/Projects/src/Views/ProjectView.xaml.cs
@@ -2,25 +2,9 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.WindowsRuntime;
-using CommunityToolkit.Mvvm.ComponentModel;
 using DevHome.Projects.ViewModels;
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Newtonsoft.Json;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 namespace DevHome.Projects.Views;
 public sealed partial class ProjectView : UserControl
