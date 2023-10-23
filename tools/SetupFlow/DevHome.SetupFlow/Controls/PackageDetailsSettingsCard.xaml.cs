e7089345f7638ec0b3a5efdcecdae3609f453dc5
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2023-10-23 15:49:09 -0700

    Addressing comments

--- tools/SetupFlow/DevHome.SetupFlow/Controls/PackageDetailsSettingsCard.xaml.cs
+++ tools/SetupFlow/DevHome.SetupFlow/Controls/PackageDetailsSettingsCard.xaml.cs
@@ -4,6 +4,7 @@
 using CommunityToolkit.WinUI.Controls;
 
 namespace DevHome.SetupFlow.Controls;
+
 public sealed partial class PackageDetailsSettingsCard : SettingsCard
 {
     public PackageDetailsSettingsCard()
