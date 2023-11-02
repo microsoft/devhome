29f75e5ce1a20a72832f7ab20b8b9d9a5c6a5bc8
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-11-02 17:28:38 +0530

    added enum for system update action

--- CoreWidgetProvider/Widgets/Enums/WidgetAction.cs
+++ CoreWidgetProvider/Widgets/Enums/WidgetAction.cs
@@ -43,4 +43,9 @@ public enum WidgetAction
     /// Kill process #3.
     /// </summary>
     CpuKill3,
+
+    /// <summary>
+    /// Action to Update page.
+    /// </summary>
+    CheckForUpdates,
 }
