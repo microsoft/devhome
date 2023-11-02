d2c18d4e1f4c2d00006143420d74f080a3ccb95f
Kristen Schau <47155823+krschau@users.noreply.github.com>
2023-11-02 15:22:02 -0400

    change customization in dev home

--- tools/Dashboard/DevHome.Dashboard/Views/CustomizeWidgetDialog.xaml.cs
+++ tools/Dashboard/DevHome.Dashboard/Views/CustomizeWidgetDialog.xaml.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 using System;
-using AdaptiveCards.Rendering.WinUI3;
 using DevHome.Common.Extensions;
 using DevHome.Dashboard.Helpers;
 using DevHome.Dashboard.Services;
@@ -11,35 +10,34 @@ using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.Windows.Widgets.Hosts;
-using WinUIEx;
 
 namespace DevHome.Dashboard.Views;
 
 public sealed partial class CustomizeWidgetDialog : ContentDialog
 {
-    public Widget EditedWidget { get; set; }
-
     public WidgetViewModel ViewModel { get; set; }
 
     private readonly IWidgetHostingService _hostingService;
 
+    private readonly Widget _widget;
     private readonly WidgetDefinition _widgetDefinition;
     private static DispatcherQueue _dispatcher;
 
-    public CustomizeWidgetDialog(DispatcherQueue dispatcher, WidgetDefinition widgetDefinition)
+    public CustomizeWidgetDialog(Widget widget, DispatcherQueue dispatcher, WidgetDefinition widgetDefinition)
     {
-        ViewModel = new WidgetViewModel(null, Microsoft.Windows.Widgets.WidgetSize.Large, widgetDefinition, dispatcher);
-        ViewModel.IsInEditMode = true;
+        var size = WidgetHelpers.GetLargestCapabilitySize(widgetDefinition.GetWidgetCapabilities());
 
+        ViewModel = new WidgetViewModel(null, size, _widgetDefinition, dispatcher)
+        {
+            IsInEditMode = true,
+        };
         _hostingService = Application.Current.GetService<IWidgetHostingService>();
 
         this.InitializeComponent();
 
+        _widget = widget;
         _widgetDefinition = widgetDefinition;
         _dispatcher = dispatcher;
-
-        // Get the application root window so we know when it has closed.
-        Application.Current.GetService<WindowEx>().Closed += OnMainWindowClosed;
 
         _hostingService.GetWidgetCatalog()!.WidgetDefinitionDeleted += WidgetCatalog_WidgetDefinitionDeleted;
 
@@ -48,52 +46,28 @@ public sealed partial class CustomizeWidgetDialog : ContentDialog
 
     private async void InitializeWidgetCustomization(object sender, RoutedEventArgs e)
     {
-        var size = WidgetHelpers.GetLargestCapabilitySize(_widgetDefinition.GetWidgetCapabilities());
-
-        // Create the widget for configuration. We will need to delete it if the dialog is closed without updating.
-        var widget = await _hostingService.GetWidgetHost()?.CreateWidgetAsync(_widgetDefinition.Id, size);
-        if (widget != null)
-        {
-            Log.Logger()?.ReportInfo("CustomizeWidgetDialog", $"Created Widget {widget.Id}");
-            ViewModel.Widget = widget;
-        }
-        else
-        {
-            Log.Logger()?.ReportError("CustomizeWidgetDialog", $"Error creating Widget {_widgetDefinition.Id}");
-        }
+        ViewModel.Widget = _widget;
+        await ViewModel.Widget.NotifyCustomizationRequestedAsync();
     }
 
     private void UpdateWidgetButton_Click(object sender, RoutedEventArgs e)
     {
         Log.Logger()?.ReportDebug("CustomizeWidgetDialog", $"Exiting dialog, updated widget");
-        EditedWidget = ViewModel.Widget;
         HideDialog();
     }
 
-    private async void CancelButton_Click(object sender, RoutedEventArgs e)
+    private void CancelButton_Click(object sender, RoutedEventArgs e)
     {
         Log.Logger()?.ReportDebug("CustomizeWidgetDialog", $"Exiting dialog, cancel button clicked");
-        var widgetIdToDelete = ViewModel.Widget.Id;
-        await ViewModel.Widget.DeleteAsync();
-        Log.Logger()?.ReportInfo("CustomizeWidgetDialog", $"Deleted Widget {widgetIdToDelete}");
-
-        EditedWidget = null;
         HideDialog();
     }
 
     private void HideDialog()
     {
-        Application.Current.GetService<WindowEx>().Closed -= OnMainWindowClosed;
         _hostingService.GetWidgetCatalog()!.WidgetDefinitionDeleted -= WidgetCatalog_WidgetDefinitionDeleted;
         ViewModel.IsInEditMode = false;
 
         this.Hide();
-    }
-
-    private async void OnMainWindowClosed(object sender, WindowEventArgs args)
-    {
-        Log.Logger()?.ReportInfo("CustomizeWidgetDialog", $"Window Closed, delete partially created widget");
-        await ViewModel.Widget.DeleteAsync();
     }
 
     private void WidgetCatalog_WidgetDefinitionDeleted(WidgetCatalog sender, WidgetDefinitionDeletedEventArgs args)
@@ -103,7 +77,6 @@ public sealed partial class CustomizeWidgetDialog : ContentDialog
         if (_widgetDefinition.Id.Equals(deletedDefinitionId, StringComparison.Ordinal))
         {
             Log.Logger()?.ReportDebug("CustomizeWidgetDialog", $"Exiting dialog, widget definition was deleted");
-            EditedWidget = null;
             _hostingService.GetWidgetCatalog()!.WidgetDefinitionDeleted -= WidgetCatalog_WidgetDefinitionDeleted;
             _dispatcher.TryEnqueue(() =>
             {
--- tools/Dashboard/DevHome.Dashboard/Views/DashboardView.xaml.cs
+++ tools/Dashboard/DevHome.Dashboard/Views/DashboardView.xaml.cs
@@ -280,7 +280,7 @@ public partial class DashboardView : ToolPage
         // delete the partially created widget.
         dialog.Closed += async (sender, args) =>
         {
-            if (dialog.AddedWidget == null && dialog.ViewModel.Widget != null)
+            if (dialog.AddedWidget == null && dialog.ViewModel != null && dialog.ViewModel.Widget != null)
             {
                 await dialog.ViewModel.Widget.DeleteAsync();
             }
@@ -473,51 +473,28 @@ public partial class DashboardView : ToolPage
         }
     }
 
-    // We can't truly edit a widget once it has been pinned. Instead, simulate editing by
-    // removing the old widget and creating a new one.
     private async Task EditWidget(WidgetViewModel widgetViewModel)
     {
-        // Get info about the widget we're "editing".
-        var index = PinnedWidgets.IndexOf(widgetViewModel);
+        // Get info about the widget we're editing.
         var originalSize = widgetViewModel.WidgetSize;
         var widgetDef = ViewModel.WidgetHostingService.GetWidgetCatalog()!.GetWidgetDefinition(widgetViewModel.Widget.DefinitionId);
 
-        var dialog = new CustomizeWidgetDialog(_dispatcher, widgetDef)
+        widgetViewModel.StopListeningForWidgetUpdated();
+        var dialog = new CustomizeWidgetDialog(widgetViewModel.Widget, _dispatcher, widgetDef)
         {
             // XamlRoot must be set in the case of a ContentDialog running in a Desktop app.
             XamlRoot = this.XamlRoot,
             RequestedTheme = this.ActualTheme,
         };
-
-        // If the dialog was closed in a way we don't already handle (for example, pressing Esc),
-        // delete the partially created widget.
-        dialog.Closed += async (sender, args) =>
-        {
-            if (dialog.EditedWidget == null && dialog.ViewModel.Widget != null)
-            {
-                await dialog.ViewModel.Widget.DeleteAsync();
-            }
-        };
         _ = await dialog.ShowAsync();
 
-        var newWidget = dialog.EditedWidget;
-
-        if (newWidget != null)
-        {
-            // Remove and delete the old widget.
-            var state = await widgetViewModel.Widget.GetCustomStateAsync();
-            PinnedWidgets.RemoveAt(index);
-            await widgetViewModel.Widget.DeleteAsync();
-
-            // Put the old widget's state on the new widget.
-            await newWidget.SetCustomStateAsync(state);
-
-            // Set the original size on the new widget and add it to the list.
-            await newWidget.SetSizeAsync(originalSize);
-            await InsertWidgetInPinnedWidgetsAsync(newWidget, originalSize, index);
-        }
-
         widgetViewModel.IsInEditMode = false;
+        widgetViewModel.StartListeningForWidgetUpdated();
+
+        // Put back the original size.
+        await widgetViewModel.Widget.SetSizeAsync(originalSize);
+
+        await widgetViewModel.RenderAsync();
     }
 
     private void WidgetGridView_DragItemsStarting(object sender, DragItemsStartingEventArgs e)
