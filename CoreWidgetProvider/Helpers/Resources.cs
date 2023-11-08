895ab4f0efc701a44e1e45dd3dbf0c2bc5b9055e
Kristen Schau <47155823+krschau@users.noreply.github.com>
2023-11-08 14:52:49 -0500

    localize save and cancel

--- CoreWidgetProvider/Helpers/Resources.cs
+++ CoreWidgetProvider/Helpers/Resources.cs
@@ -87,6 +87,8 @@ public static class Resources
             "CPUUsage_Widget_Template/CPU_Speed",
             "CPUUsage_Widget_Template/Processes",
             "CPUUsage_Widget_Template/End_Process",
+            "Widget_Template_Button/Save",
+            "Widget_Template_Button/Cancel",
         };
     }
 }
