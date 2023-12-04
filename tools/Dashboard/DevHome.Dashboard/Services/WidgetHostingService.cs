72d4c4659563ee986f72ed03ae1022b572dca939
Kristen Schau <47155823+krschau@users.noreply.github.com>
2023-12-04 15:16:29 -0500

    Move WidgetService calls off-thread

--- tools/Dashboard/DevHome.Dashboard/Services/WidgetHostingService.cs
+++ tools/Dashboard/DevHome.Dashboard/Services/WidgetHostingService.cs
@@ -4,49 +4,61 @@
 using System;
 using DevHome.Dashboard.Helpers;
 using Microsoft.Windows.Widgets.Hosts;
+using WinUIEx;
 
 namespace DevHome.Dashboard.Services;
 
 public class WidgetHostingService : IWidgetHostingService
 {
+    private readonly WindowEx _windowEx;
+
     private WidgetHost _widgetHost;
     private WidgetCatalog _widgetCatalog;
 
-    public WidgetHostingService()
+    public WidgetHostingService(WindowEx windowEx)
     {
+        _windowEx = windowEx;
+        RegisterWidgetHost();
+        GetDefaultCatalog();
+    }
+
+    private void RegisterWidgetHost()
+    {
+        try
+        {
+            _windowEx.DispatcherQueue.TryEnqueue(() =>
+            {
+                _widgetHost = WidgetHost.Register(new WidgetHostContext("BAA93438-9B07-4554-AD09-7ACCD7D4F031"));
+            });
+        }
+        catch (Exception ex)
+        {
+            Log.Logger()?.ReportError("WidgetHostingService", "Exception in WidgetHost.Register:", ex);
+        }
     }
 
     public WidgetHost GetWidgetHost()
     {
-        if (_widgetHost == null)
-        {
-            try
-            {
-                _widgetHost = WidgetHost.Register(new WidgetHostContext("BAA93438-9B07-4554-AD09-7ACCD7D4F031"));
-            }
-            catch (Exception ex)
-            {
-                Log.Logger()?.ReportError("WidgetHostingService", "Exception in WidgetHost.Register:", ex);
-            }
-        }
-
         return _widgetHost;
+    }
+
+    private void GetDefaultCatalog()
+    {
+        try
+        {
+            _windowEx.DispatcherQueue.TryEnqueue(() =>
+            {
+                _widgetCatalog = WidgetCatalog.GetDefault();
+            });
+        }
+        catch (Exception ex)
+        {
+            Log.Logger()?.ReportError("WidgetHostingService", "Exception in WidgetCatalog.GetDefault:", ex);
+        }
     }
 
     public WidgetCatalog GetWidgetCatalog()
     {
-        if (_widgetCatalog == null)
-        {
-            try
-            {
-                _widgetCatalog = WidgetCatalog.GetDefault();
-            }
-            catch (Exception ex)
-            {
-                Log.Logger()?.ReportError("WidgetHostingService", "Exception in WidgetCatalog.GetDefault:", ex);
-            }
-        }
-
         return _widgetCatalog;
     }
 }
