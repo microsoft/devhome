89b194095bd1872e5e6384eabae171a2bcfe0be4
Huzaifa Danish <modanish@microsoft.com>
2023-12-08 12:35:39 -0800

    Added new empty page project

---
+++ DevHome.Environments/Views/LandingPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors
+// Licensed under the MIT license.
+
+using DevHome.Common;
+using DevHome.Environments.ViewModels;
+using Microsoft.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace DevHome.Environments.Views;
+
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class LandingPage : ToolPage
+{
+    public LandingPage()
+    {
+        this.InitializeComponent();
+    }
+
+    public override string ShortName => "Environments";
+}
