0b30ed989c863bd34814bd59c47a96fbfb288e70
Huzaifa Danish <Huzaifa.Danish@microsoft.com>
2023-12-08 14:22:52 -0800

    Added new project files

---
+++ tools/Environments/DevHome.Environments/Views/LandingPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors
+// Licensed under the MIT license.
+
+using DevHome.Common;
+
+namespace DevHome.Environments.Views;
+
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class LandingPage : ToolPage
+{
+    public LandingPage()
+    {
+        this.InitializeComponent();
+    }
+
+    public override string ShortName => "Environments";
+}
