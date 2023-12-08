b0e76ded2aed050e526b3f14d3c62fab7b188943
Darren Hoehna <dahoehna@microsoft.com>
2023-12-08 11:38:15 -0800

    Seperating out events.

---
+++ common/TelemetryEvents/SetupFlow/RepoTool/RepoInfoModificationEvent.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using DevHome.Telemetry;
+using Microsoft.Diagnostics.Telemetry;
+using Microsoft.Diagnostics.Telemetry.Internal;
+
+namespace DevHome.Common.TelemetryEvents.SetupFlow.RepoTool;
+public class RepoInfoModificationEvent : EventBase
+{
+    public override PartA_PrivTags PartA_PrivTags => PrivTags.ProductAndServiceUsage;
+
+    public string ModificationType
+    {
+        get;
+    }
+
+    public RepoInfoModificationEvent(string modificationType)
+    {
+        ModificationType = modificationType;
+    }
+
+    public override void ReplaceSensitiveStrings(Func<string, string> replaceSensitiveStrings)
+    {
+        // No sensitive strings to replace.
+    }
+}
