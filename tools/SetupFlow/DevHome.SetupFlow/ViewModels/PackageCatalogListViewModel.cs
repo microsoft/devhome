49ae9049521edc92a01ec20c8f00e119cff79265
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2024-01-03 14:00:17 -0800

    Fix bindings

--- tools/SetupFlow/DevHome.SetupFlow/ViewModels/PackageCatalogListViewModel.cs
+++ tools/SetupFlow/DevHome.SetupFlow/ViewModels/PackageCatalogListViewModel.cs
@@ -70,7 +70,6 @@ public partial class PackageCatalogListViewModel : ObservableObject
                 foreach (var catalog in dataSourceCatalogs)
                 {
                     var catalogVM = await Task.Run(() => _packageCatalogViewModelFactory(catalog));
-                    catalogVM.CanAddAllPackages = true;
                     PackageCatalogs.Add(catalogVM);
                 }
 
