2b340fff9fe05a723740a4e42e68971038b6fb28
Eric Johnson <ericjohnson327@gmail.com>
2024-01-11 12:12:25 -0800

    Update package references (#1941)

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation and Contributors
 // Licensed under the MIT license.
 
-using System.Web.Services.Description;
 using DevHome.Activation;
 using DevHome.Common.Contracts;
 using DevHome.Common.Contracts.Services;
