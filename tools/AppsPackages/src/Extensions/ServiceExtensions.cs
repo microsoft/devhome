5329dd38f761363ce694ca71b0e0288b2f86a040
James Alickolli <32421608+licon4812@users.noreply.github.com>
2024-02-07 01:43:01 +1100

    Creation of tool Apps & Packages with very basic WIP UI (#2195)

---
+++ tools/AppsPackages/src/Extensions/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors
+// Licensed under the MIT license.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevHome.AppsPackages.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DevHome.Settings.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddAppsAndPackages(this IServiceCollection services, HostBuilderContext context)
+    {
+        // View models
+        services.AddTransient<AppsPackagesViewModel>();
+        return services;
+    }
+}
