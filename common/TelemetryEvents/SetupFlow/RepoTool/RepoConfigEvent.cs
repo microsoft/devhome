5ff18724c66557af2da9da326ac93c9ad282dd67
Darren Hoehna <dhoehna@yahoo.com>
2024-02-12 14:33:41 -0800

    Separating out events. (#2023)
    * Seperating out events.
    
    * Fixing build errors
    
    ---------
    
    Co-authored-by: Darren Hoehna <dahoehna@microsoft.com>

---
+++ common/TelemetryEvents/SetupFlow/RepoTool/RepoConfigEvent.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors
+// Licensed under the MIT license.
+
+using System;
+using DevHome.Telemetry;
+using Microsoft.Diagnostics.Telemetry;
+using Microsoft.Diagnostics.Telemetry.Internal;
+
+namespace DevHome.Common.TelemetryEvents.SetupFlow.RepoTool;
+
+public class RepoConfigEvent : EventBase
+{
+    public override PartA_PrivTags PartA_PrivTags => PrivTags.ProductAndServiceUsage;
+
+    public string Action
+    {
+        get;
+    }
+
+    public RepoConfigEvent(string action)
+    {
+        Action = action;
+    }
+
+    public override void ReplaceSensitiveStrings(Func<string, string> replaceSensitiveStrings)
+    {
+        // No sensitive strings to replace.
+    }
+}
