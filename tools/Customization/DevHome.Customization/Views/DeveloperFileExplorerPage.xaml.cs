087936c1a9db4420021beb6fa915b66f6ebccc70
Nick Eubanks <nieubank@microsoft.com>
2024-03-18 12:23:35 -0400

    cleanup unused usings

--- tools/Customization/DevHome.Customization/Views/DeveloperFileExplorerPage.xaml.cs
+++ tools/Customization/DevHome.Customization/Views/DeveloperFileExplorerPage.xaml.cs
@@ -1,11 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Collections.ObjectModel;
 using DevHome.Common.Extensions;
-using DevHome.Common.Services;
 using DevHome.Customization.ViewModels;
-using DevHome.Settings.Models;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
--- tools/Customization/DevHome.Customization/Views/MainPage.cs
+++ tools/Customization/DevHome.Customization/Views/MainPage.cs
@@ -1,14 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Collections.ObjectModel;
 using DevHome.Common;
 using DevHome.Common.Extensions;
-using DevHome.Common.Services;
 using DevHome.Customization.ViewModels;
-using DevHome.Settings.Models;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
 
 namespace DevHome.Customization.Views;
 
