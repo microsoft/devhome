4fe92fa0fdb2f0c3bea3c4b906241e85c21012e4
Darren Hoehna <dhoehna@yahoo.com>
2024-03-21 10:02:00 -0700

    Merge searching into main. (#2443)
    * Adding searching capability to the repo tool (#2245)
    
    * Re-adding lost changes
    
    * WIP
    
    * Minor tweaks
    
    * Cleaning up code.
    
    * Fixing code issues with new()
    
    * Reverting SDK version.
    
    * Reverting idl change because it is another PR.
    
    * Reverting package.appxmanifest change
    
    * Adding search term placeholder text
    
    * Removing a method.  Using GetValuesFor
    
    * Removing a space
    
    * Moving QI to where it is used
    
    ---------
    
    Co-authored-by: Darren Hoehna <dahoehna@microsoft.com>
    
    * Comment changes. (#2310)
    
    * Re-adding lost changes
    
    * WIP
    
    * Minor tweaks
    
    * Cleaning up code.
    
    * Fixing code issues with new()
    
    * Reverting SDK version.
    
    * Reverting idl change because it is another PR.
    
    * Reverting package.appxmanifest change
    
    * Adding search term placeholder text
    
    * Removing a method.  Using GetValuesFor
    
    * Removing a space
    
    * Moving QI to where it is used
    
    * Comment changes
    
    ---------
    
    Co-authored-by: Darren Hoehna <dahoehna@microsoft.com>
    
    * FIxing some errors.  Making the UI closer to the mocks> (#2326)
    
    Co-authored-by: Darren Hoehna <dahoehna@microsoft.com>
    
    * addRepo improvements (#2298)
    
    * USing tags
    
    * Can clone repos again.
    
    * Changing visibility to bool.
    
    * More changes
    
    * Account combobox is reset.
    
    * Adding some comments.
    
    * Combobox placeholder
    
    * Final touches
    
    * Fixing a comment.
    
    * Fixing SDk version
    
    * Ignoring a test
    
    * Update tools/SetupFlow/DevHome.SetupFlow/ViewModels/AddRepoViewModel.cs
    
    Co-authored-by: Kristen Schau <47155823+krschau@users.noreply.github.com>
    
    * Update tools/SetupFlow/DevHome.SetupFlow/ViewModels/AddRepoViewModel.cs
    
    Co-authored-by: Kristen Schau <47155823+krschau@users.noreply.github.com>
    
    * Update tools/SetupFlow/DevHome.SetupFlow/Views/AddRepoDialog.xaml
    
    Co-authored-by: Kristen Schau <47155823+krschau@users.noreply.github.com>
    
    * Changes
    
    * Removing prefixes for uid
    
    * Moving more things into the ViewModel.
    
    * Getting loginUI and provider name not null.
    
    * Ignoring anotehr test because making a new frame throws an exception.
    
    * Adding -async
    
    ---------
    
    Co-authored-by: Darren Hoehna <dahoehna@microsoft.com>
    Co-authored-by: Kristen Schau <47155823+krschau@users.noreply.github.com>
    
    * Enabling Search inputs with IRepositoryProvider (#2244)
    
    * Adding IRepositoryProvider2
    
    * Adding a space between the >'s
    
    * Modifying based on feedback.
    
    * Moving around arguments
    
    * Changes based on semi-API review
    
    * Adding implementation
    
    * HOlding onto the options
    
    * Copying a com array
    
    * Changing a name
    
    * Back to a working build.
    
    * WIP
    
    * eeeyyy, it works.  Now need to add some smaller changes
    
    * Cleaning up code
    
    * Works and looks good
    
    * Adding comments
    
    * Fixing a typo
    
    * Adding locks.  Getting search to work with one provider
    
    * Changing back to IIterable
    
    * Changing variable names
    
    * Adding a comment.
    
    * Adding another QI.  Removing duplicate code.
    
    * Addressing comments.
    
    * Updating version
    
    * Removing plural
    
    ---------
    
    Co-authored-by: Darren Hoehna <dahoehna@microsoft.com>
    
    * Updating version
    
    * Changing version
    
    ---------
    
    Co-authored-by: Darren Hoehna <dahoehna@microsoft.com>
    Co-authored-by: Kristen Schau <47155823+krschau@users.noreply.github.com>

--- tools/SetupFlow/DevHome.SetupFlow/Views/AddRepoDialog.xaml.cs
+++ tools/SetupFlow/DevHome.SetupFlow/Views/AddRepoDialog.xaml.cs
@@ -32,6 +32,8 @@ public partial class AddRepoDialog : ContentDialog
 
     private readonly List<CloningInformation> _previouslySelectedRepos = new();
 
+    private readonly Dictionary<string, List<string>> _searchFieldsAndValues;
+
     /// <summary>
     /// Gets or sets the view model to handle selecting and de-selecting repositories.
     /// </summary>
@@ -67,6 +69,7 @@ public partial class AddRepoDialog : ContentDialog
         AddViaUrlSegmentedItem.IsSelected = true;
         SwitchViewsSegmentedView.SelectedIndex = 1;
         _host = host;
+        _searchFieldsAndValues = new();
     }
 
     /// <summary>
@@ -139,7 +142,7 @@ public partial class AddRepoDialog : ContentDialog
     public void SelectRepositories(IEnumerable<RepoViewListItem> reposToSelect)
     {
         AddRepoViewModel.IsCallingSelectRange = true;
-        var onlyRepoNames = AddRepoViewModel.Repositories.Select(x => x.RepoName).ToList();
+        var onlyRepoNames = AddRepoViewModel.RepositoriesToDisplay.Select(x => x.RepoName).ToList();
         foreach (var repoToSelect in reposToSelect)
         {
             var index = onlyRepoNames.IndexOf(repoToSelect.RepoName);
@@ -259,6 +262,24 @@ public partial class AddRepoDialog : ContentDialog
                 deferral.Complete();
             }
         }
+        else if (AddRepoViewModel.CurrentPage == PageKind.SearchFields)
+        {
+            args.Cancel = true;
+            Dictionary<string, string> searchInput = new();
+            foreach (var searchBox in ShowingSearchTermsGrid.Children)
+            {
+                if (searchBox is AutoSuggestBox suggestBox)
+                {
+                    searchInput.Add(suggestBox.Header as string, suggestBox.Text);
+                }
+            }
+
+            // switching to the repo page causes repos to be queried.
+            var deferral = args.GetDeferral();
+            await AddRepoViewModel.ChangeToRepoPageAsync();
+            AddRepoViewModel.SearchForRepos(searchInput);
+            deferral.Complete();
+        }
     }
 
     /// <summary>
@@ -333,5 +354,47 @@ public partial class AddRepoDialog : ContentDialog
         AddRepoViewModel.FolderPickerViewModel.CloneLocationAlias = AddRepoViewModel.EditDevDriveViewModel.GetDriveDisplayName(DevDriveDisplayNameKind.FormattedDriveLabelKind);
         AddRepoViewModel.FolderPickerViewModel.InDevDriveScenario = true;
         AddRepoViewModel.EditDevDriveViewModel.IsDevDriveCheckboxChecked = true;
+    }
+
+    private void FilterSuggestions(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+    {
+        sender.ItemsSource = _searchFieldsAndValues[sender.Header.ToString()].Where(x => x.Contains(sender.Text));
+        return;
+    }
+
+    private async void SwitchToSearchPage(object sender, RoutedEventArgs e)
+    {
+        AddRepoViewModel.ChangeToSelectSearchTermsPage();
+        _searchFieldsAndValues.Clear();
+        ShowingSearchTermsGrid.Children.Clear();
+        GatheringSearchValuesGrid.Visibility = Visibility.Visible;
+        ShowingSearchTermsGrid.Visibility = Visibility.Collapsed;
+
+        var loginId = (string)AddRepoViewModel.SelectedAccount;
+        var searchTerms = AddRepoViewModel.GetSearchTerms();
+        ShowingSearchTermsGrid.RowSpacing = 10;
+
+        // Set up the UI for searching.
+        var searchTermRow = 0;
+        for (var termIndex = 0; termIndex < searchTerms.Count; termIndex++)
+        {
+            var localTermIndex = termIndex;
+            ShowingSearchTermsGrid.RowDefinitions.Add(new RowDefinition());
+
+            var searchFieldName = string.Empty;
+            var searchFieldSuggestions = await Task.Run(() => AddRepoViewModel.GetSuggestionsFor(loginId, new(), searchTerms[localTermIndex]));
+
+            _searchFieldsAndValues.Add(searchTerms[localTermIndex], searchFieldSuggestions);
+            var suggestBox = new AutoSuggestBox();
+            suggestBox.Header = searchTerms[localTermIndex];
+            suggestBox.ItemsSource = searchFieldSuggestions;
+            suggestBox.Text = searchFieldName;
+            suggestBox.TextChanged += FilterSuggestions;
+            ShowingSearchTermsGrid.Children.Add(suggestBox);
+            Grid.SetRow(suggestBox, searchTermRow++);
+        }
+
+        GatheringSearchValuesGrid.Visibility = Visibility.Collapsed;
+        ShowingSearchTermsGrid.Visibility = Visibility.Visible;
     }
 }
