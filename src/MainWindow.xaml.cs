ce75b5609c301674ffaaedab4f048b85dccd3a8b
Matt Hyman <mathy@microsoft.com>
2024-03-21 10:43:26 -0700

    Experiments.

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,9 +1,13 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using DevHome.Common.Extensions;
+using DevHome.Common.Helpers;
 using DevHome.Common.Services;
 using DevHome.Helpers;
+using DevHome.Logging;
 using DevHome.Telemetry;
 using DevHome.TelemetryEvents;
 using Microsoft.UI.Xaml;
@@ -28,5 +32,18 @@ public sealed partial class MainWindow : WindowEx
     {
         Application.Current.GetService<IExtensionService>().SignalStopExtensionsAsync();
         TelemetryFactory.Get<ITelemetry>().Log("DevHome_MainWindow_Closed_Event", LogLevel.Critical, new DevHomeClosedEvent(mainWindowCreated));
+    }
+
+    internal const int AsfwAny = -1;
+
+    [DllImport("user32.dll")]
+    private static extern bool AllowSetForegroundWindow(int id);
+
+    private void MainWindow_Activated(object sender, WindowActivatedEventArgs args)
+    {
+        if (!AllowSetForegroundWindow(AsfwAny))
+        {
+            Log.Logger()?.ReportInfo($"Failed to Allow Set Foreground Window");
+        }
     }
 }
