fbddcfeaf389ddb3c0e3891f7db8f3bc6230cc00
bbonaby-MSFT <bbonaby@microsoft.com>
2024-03-23 01:17:46 -0700

    add custom renders

---
+++ common/AdaptiveCardInputValues/ItemsViewInputValue.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Globalization;
+using AdaptiveCards.ObjectModel.WinUI3;
+using AdaptiveCards.Rendering.WinUI3;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace DevHome.Common.AdaptiveCardInputValues;
+
+/// <summary>
+/// Represents the current value of an ItemsView input element.
+/// </summary>
+public class ItemsViewInputValue : IAdaptiveInputValue
+{
+    private readonly ItemsView _itemsView;
+
+    public ItemsViewInputValue(AdaptiveChoiceSetInput input, ItemsView itemsView)
+    {
+        InputElement = input;
+        _itemsView = itemsView;
+    }
+
+    /// <summary>
+    /// Gets the current value of the input element. This is the index of the current item.
+    /// </summary>
+    public string CurrentValue => _itemsView.CurrentItemIndex.ToString(CultureInfo.InvariantCulture);
+
+    // Error message unused at this time because Validation is not needed for this class
+    public UIElement? ErrorMessage { get; set; }
+
+    public IAdaptiveInputElement InputElement { get; set; }
+
+    public void SetFocus()
+    {
+        _itemsView.Focus(FocusState.Keyboard);
+    }
+
+    // Validation is not needed for this class
+    public bool Validate()
+    {
+        return true;
+    }
+}
