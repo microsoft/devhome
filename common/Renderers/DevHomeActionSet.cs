1d5918e4bf548f64eb076aa1a01eb06b060a9a98
bbonaby-MSFT <bbonaby@microsoft.com>
2024-03-23 01:17:46 -0700

    add custom renders

---
+++ common/Renderers/DevHomeActionSet.cs
@@ -0,0 +1,134 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using AdaptiveCards.ObjectModel.WinUI3;
+using AdaptiveCards.Rendering.WinUI3;
+using DevHome.Common.Contracts;
+using Microsoft.UI.Xaml;
+
+namespace DevHome.Common.Renderers;
+
+/// <summary>
+/// Represents whether the action is visible or hidden in Dev Home's UI.
+/// </summary>
+public enum TopLevelCardActionSetVisibility
+{
+    Visible,
+    Hidden,
+}
+
+/// <summary>
+/// Represents the button at the top level of the card that will invoke the action.
+/// </summary>
+public enum ActionButtonToInvoke
+{
+    Primary,
+    Secondary,
+}
+
+/// <summary>
+/// Allows Dev Home to attach an action to a button in the Dev Home UI.
+/// Dev Home can use this invoke an action from an adaptive card when
+/// a button within Dev Home is clicked. It allows Dev Home to link a primary
+/// and secondary button to two actions within a top level action set in the adaptive card.
+/// </summary>
+/// <remarks>
+/// It is expected that the adaptive card will have a top level action set with two actions.
+/// for Dev Home to link to. The primary button in Dev Home will be linked to the first action
+/// and the secondary button will be linked to the second action.
+/// </remarks>
+public class DevHomeActionSet : IDevHomeActionRender
+{
+    /// <summary>
+    /// Gets the visibility of the action in Dev Home's UI.
+    /// </summary>
+    private readonly TopLevelCardActionSetVisibility _actionVisibility;
+
+    /// <summary>
+    /// Gets the adaptive card object that will invoke an action within the action set.
+    /// </summary>
+    public AdaptiveActionInvoker? ActionButtonInvoker { get; private set; }
+
+    private IAdaptiveActionElement? _primaryButtonAdaptiveActionElement;
+
+    private IAdaptiveActionElement? _secondaryButtonAdaptiveActionElement;
+
+    public DevHomeActionSet(TopLevelCardActionSetVisibility cardActionVisibility)
+    {
+        _actionVisibility = cardActionVisibility;
+    }
+
+    public UIElement? Render(IAdaptiveCardElement element, AdaptiveRenderContext context, AdaptiveRenderArgs renderArgs)
+    {
+        if (element is AdaptiveActionSet actionSet)
+        {
+            // We'll only support two submit/execute actions in the action set.
+            // one for the primary action and one for the secondary action.
+            if (actionSet.Actions.Count > 2 || actionSet.Actions.Count == 0)
+            {
+                throw new ArgumentException("The ActionSet must have a minimum of 1 action and maximum of 2 actions.");
+            }
+
+            ActionButtonInvoker = context.ActionInvoker;
+
+            for (var i = 0; i < actionSet.Actions.Count; i++)
+            {
+                var action = actionSet.Actions[i];
+                if (action is AdaptiveExecuteAction executeAction)
+                {
+                    LinkSubmitActionToCard(i, executeAction, context, renderArgs);
+                }
+                else if (action is AdaptiveSubmitAction submitAction)
+                {
+                    LinkSubmitActionToCard(i, submitAction, context, renderArgs);
+                }
+            }
+
+            if (_actionVisibility == TopLevelCardActionSetVisibility.Hidden)
+            {
+                // the page in Dev Home does not want to show the action set in the card.
+                // but needed to link a button in Dev Home to the action. So we return null.
+                // so we don't render the action set. Invoking the button from Dev Home will
+                // then trigger the action in the adaptive card
+                return null;
+            }
+        }
+
+        var renderer = new AdaptiveActionSetRenderer();
+        return renderer.Render(element, context, renderArgs);
+    }
+
+    /// <summary>
+    /// Invokes an adaptive card action from another control like a button in Dev Home.
+    /// </summary>
+    /// <param name="buttonToInvoke">The primary or seconday enum value that will be used to choose which one Dev Home will invoke</param>
+    public void InitiateAction(ActionButtonToInvoke buttonToInvoke)
+    {
+        IAdaptiveActionElement? adaptiveActionElement = _primaryButtonAdaptiveActionElement;
+
+        if (buttonToInvoke == ActionButtonToInvoke.Primary)
+        {
+            adaptiveActionElement = _secondaryButtonAdaptiveActionElement;
+        }
+
+        ActionButtonInvoker?.SendActionEvent(adaptiveActionElement);
+    }
+
+    /// <summary>
+    /// Links the submit/execute actiono to the adaptive card itself.
+    /// </summary>
+    private void LinkSubmitActionToCard(int index, IAdaptiveActionElement action, AdaptiveRenderContext context, AdaptiveRenderArgs renderArgs)
+    {
+        if (index == 0)
+        {
+            context.LinkSubmitActionToCard(action, renderArgs);
+            _primaryButtonAdaptiveActionElement = action;
+        }
+        else
+        {
+            context.LinkSubmitActionToCard(action, renderArgs);
+            _secondaryButtonAdaptiveActionElement = action;
+        }
+    }
+}
---
+++ common/Renderers/ItemsViewChoiceSet.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using AdaptiveCards.ObjectModel.WinUI3;
+using AdaptiveCards.Rendering.WinUI3;
+using DevHome.Common.AdaptiveCardInputValues;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace DevHome.Common.Renderers;
+
+/// <summary>
+/// Renders a list of expanded choice set items as an ItemsView
+/// </summary>
+public class ItemsViewChoiceSet : IAdaptiveElementRenderer
+{
+    /// <summary>
+    /// The DataTemplate to be used for the ItemsView. This will be used to allow consumers to specify how the items
+    /// look within the ItemsView.
+    /// </summary>
+    private readonly DataTemplate _itemsTemplate;
+
+    /// <summary>
+    /// The items to be displayed in the ItemsView. The items are objects which will be used as the Content
+    /// of the ListViewItems we add to the ItemsView. The datatemplate should use Binding to display values from
+    /// these objects.
+    /// </summary>
+    /// <remarks>
+    /// The order of the items in this list will should be the same order as the items in the choice set.
+    /// </remarks>
+    private readonly List<object> _originalItems;
+
+    public ItemsView ChoiceSetItemsView { get; private set; } = new();
+
+    public List<ListViewItem> ListViewItemsForItemsView { get; private set; } = new();
+
+    public ItemsViewChoiceSet(DataTemplate itemsTemplate, List<object> objectList)
+    {
+        _itemsTemplate = itemsTemplate;
+        _originalItems = objectList;
+
+        // set the template for the items view.
+        ChoiceSetItemsView.ItemTemplate = _itemsTemplate;
+    }
+
+    public UIElement Render(IAdaptiveCardElement element, AdaptiveRenderContext context, AdaptiveRenderArgs renderArgs)
+    {
+        var choiceSet = element as AdaptiveChoiceSetInput;
+
+        // We'll only allow this renderer to be used if the choice set is expanded.
+        // If it's not expanded, we'll just use the default renderer. (Note: expanded is visually a list of radio buttons)
+        // we'' use the custom renderer to show these as a list of items, instead of radio buttons.
+        if (choiceSet != null && choiceSet.ChoiceSetStyle == ChoiceSetStyle.Expanded)
+        {
+            if (choiceSet.Choices.Count != ListViewItemsForItemsView.Count)
+            {
+                throw new ArgumentException("The number of items in the choice set must match the number of items in the ItemsViewChoiceSet.");
+            }
+
+            // Check if the choice set is multi-select, and if it is make sure the ItemsView is set to allow multiple selection.
+            if (choiceSet.IsMultiSelect)
+            {
+                ChoiceSetItemsView.SelectionMode = ItemsViewSelectionMode.Multiple;
+            }
+
+            // Go through all the items in the choice set and make an item for each one.
+            for (var i = 0; i < choiceSet.Choices.Count; i++)
+            {
+                ListViewItemsForItemsView.Add(new ListViewItem() { Content = _originalItems[i] });
+            }
+
+            // Set upp the ItemsSource for the ItemsView and add the input value to the context.
+            // the input value is used to get the current index of the items view in relation
+            // to the item in the choice set.
+            ChoiceSetItemsView.ItemsSource = ListViewItemsForItemsView;
+            context.AddInputValue(new ItemsViewInputValue(choiceSet, ChoiceSetItemsView), renderArgs);
+
+            // Return the ItemsView.
+            return ChoiceSetItemsView;
+        }
+
+        // Use default render for all other cases.
+        var renderer = new AdaptiveChoiceSetInputRenderer();
+        return renderer.Render(element, context, renderArgs);
+    }
+}
