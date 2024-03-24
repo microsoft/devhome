26a743ba8dafd7525647a3f87d670bccf8000ea0
bbonaby-MSFT <bbonaby@microsoft.com>
2024-03-23 21:07:00 -0700

    add adaptive card

---
+++ common/DevHomeAdaptiveCards/CardModels/IAdaptiveSettingsCardAction.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdaptiveCards.ObjectModel.WinUI3;
+using AdaptiveCards.Rendering.WinUI3;
+
+namespace DevHome.Common.DevHomeAdaptiveCards.CardModels;
+
+public enum AdaptiveSettingsCardActionKind
+{
+    None,
+    LaunchContentDialog,
+}
+
+public interface IAdaptiveSettingsCardAction : IAdaptiveActionElement
+{
+    public string ActionButtonText { get; set; }
+
+    public AdaptiveSettingsCardActionKind ActionKind { get; set; }
+
+    public void LaunchContentDialog(AdaptiveCardRenderer adaptiveCardRenderer, );
+}
