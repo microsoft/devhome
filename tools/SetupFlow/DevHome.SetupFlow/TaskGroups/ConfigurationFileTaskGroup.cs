7b94d500739c6c660581424aa9032e587d99f059
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2024-03-28 11:10:01 -0700

    CE

--- tools/SetupFlow/DevHome.SetupFlow/TaskGroups/ConfigurationFileTaskGroup.cs
+++ tools/SetupFlow/DevHome.SetupFlow/TaskGroups/ConfigurationFileTaskGroup.cs
@@ -22,7 +22,7 @@ public class ConfigurationFileTaskGroup : ISetupTaskGroup
 
     public IEnumerable<ISetupTask> SetupTasks => _viewModel.TaskList;
 
-    public IEnumerable<ISetupTask> DSCTasks { get; } = [];
+    public IEnumerable<ISetupTask> DSCTasks => SetupTasks;
 
     /// <summary>
     /// Gets the task corresponding to the configuration file to apply
