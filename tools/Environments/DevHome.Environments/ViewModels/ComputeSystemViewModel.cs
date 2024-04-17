6df049af584a5aaf05e08efde68f6007457078e2
Branden Bonaby <105318831+bbonaby@users.noreply.github.com>
2024-04-16 21:18:46 -0700

    Add updates to allow for Dev Box creation in Dev Home (#2639)
    * Add changes to allow for DevBox creation from Dev Home
    
    * remove deleted systems from when done
    
    * update text
    
    * update based on comments
    
    * update comment based on PR comment

--- tools/Environments/DevHome.Environments/ViewModels/ComputeSystemViewModel.cs
+++ tools/Environments/DevHome.Environments/ViewModels/ComputeSystemViewModel.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.ObjectModel;
+using System.Numerics;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -10,6 +11,7 @@ using DevHome.Common.Environments.Helpers;
 using DevHome.Common.Environments.Models;
 using DevHome.Common.Environments.Services;
 using DevHome.Common.Extensions;
+using DevHome.Common.Services;
 using DevHome.Common.TelemetryEvents.SetupFlow.Environments;
 using DevHome.Environments.Helpers;
 using DevHome.Telemetry;
@@ -29,11 +31,11 @@ public partial class ComputeSystemViewModel : ComputeSystemCardBase
 {
     private readonly ILogger _log = Log.ForContext("SourceContext", nameof(ComputeSystemViewModel));
 
+    private readonly StringResource _stringResource;
+
     private readonly WindowEx _windowEx;
 
     private readonly IComputeSystemManager _computeSystemManager;
-
-    public bool IsOperationInProgress { get; set; }
 
     // Launch button operations
     public ObservableCollection<OperationsViewModel> LaunchOperations { get; set; }
@@ -42,10 +44,13 @@ public partial class ComputeSystemViewModel : ComputeSystemCardBase
 
     public string PackageFullName { get; set; }
 
+    private readonly Func<ComputeSystemCardBase, bool> _removalAction;
+
     public ComputeSystemViewModel(
         IComputeSystemManager manager,
         IComputeSystem system,
         ComputeSystemProvider provider,
+        Func<ComputeSystemCardBase, bool> removalAction,
         string packageFullName,
         WindowEx windowEx)
     {
@@ -56,6 +61,10 @@ public partial class ComputeSystemViewModel : ComputeSystemCardBase
         ProviderDisplayName = provider.DisplayName;
         PackageFullName = packageFullName;
         Name = ComputeSystem.DisplayName;
+        AssociatedProviderId = ComputeSystem.AssociatedProviderId!;
+        ComputeSystemId = ComputeSystem.Id!;
+        _removalAction = removalAction;
+        ShouldShowLaunchOperation = true;
 
         if (!string.IsNullOrEmpty(ComputeSystem.SupplementalDisplayName))
         {
@@ -67,6 +76,8 @@ public partial class ComputeSystemViewModel : ComputeSystemCardBase
         HeaderImage = CardProperty.ConvertMsResourceToIcon(provider.Icon, packageFullName);
         ComputeSystem.StateChanged += _computeSystemManager.OnComputeSystemStateChanged;
         _computeSystemManager.ComputeSystemStateChanged += OnComputeSystemStateChanged;
+
+        _stringResource = new StringResource("DevHome.Environments.pri", "DevHome.Environments/Resources");
     }
 
     public async Task InitializeCardDataAsync()
@@ -97,6 +108,8 @@ public partial class ComputeSystemViewModel : ComputeSystemCardBase
 
         State = result.State;
         StateColor = ComputeSystemHelpers.GetColorBasedOnState(State);
+
+        SetupOperationProgressBasedOnState();
     }
 
     private async Task SetBodyImageAsync()
@@ -112,14 +125,16 @@ public partial class ComputeSystemViewModel : ComputeSystemCardBase
         }
     }
 
-    public void OnComputeSystemStateChanged(ComputeSystem sender, ComputeSystemState state)
+    public void OnComputeSystemStateChanged(ComputeSystem sender, ComputeSystemState newState)
     {
         _windowEx.DispatcherQueue.TryEnqueue(() =>
         {
             if (sender.Id == ComputeSystem!.Id)
             {
-                State = state;
-                StateColor = ComputeSystemHelpers.GetColorBasedOnState(state);
+                UpdateOperationsPostCreation(State, newState);
+                State = newState;
+                StateColor = ComputeSystemHelpers.GetColorBasedOnState(newState);
+                SetupOperationProgressBasedOnState();
             }
         });
     }
@@ -138,7 +153,11 @@ public partial class ComputeSystemViewModel : ComputeSystemCardBase
         // We'll need to disable the card UI while the operation is in progress and handle failures.
         Task.Run(async () =>
         {
-            IsOperationInProgress = true;
+            _windowEx.DispatcherQueue.TryEnqueue(() =>
+            {
+                UiMessageToDisplay = _stringResource.GetLocalized("LaunchingEnvironmentText");
+                IsOperationInProgress = true;
+            });
 
             TelemetryFactory.Get<ITelemetry>().Log(
                 "Environment_Launch_Event",
@@ -148,11 +167,17 @@ public partial class ComputeSystemViewModel : ComputeSystemCardBase
             var operationResult = await ComputeSystem!.ConnectAsync(string.Empty);
 
             var completionStatus = EnvironmentsTelemetryStatus.Succeeded;
+            var completionMessage = _stringResource.GetLocalized("LaunchingEnvironmentSuccessText");
+            var operationFailed = (operationResult == null) || (operationResult.Result.Status == ProviderOperationStatus.Failure);
 
-            if ((operationResult == null) || (operationResult.Result.Status == ProviderOperationStatus.Failure))
+            if (operationFailed)
             {
                 completionStatus = EnvironmentsTelemetryStatus.Failed;
                 LogFailure(operationResult);
+
+                var messageWhenNull = _stringResource.GetLocalized("LaunchingEnvironmentFailedUnKnownReasonText");
+                completionMessage =
+                    (operationResult != null) ? _stringResource.GetLocalized("LaunchingEnvironmentFailedText", operationResult.Result.DisplayMessage) : messageWhenNull;
             }
 
             TelemetryFactory.Get<ITelemetry>().Log(
@@ -160,7 +185,22 @@ public partial class ComputeSystemViewModel : ComputeSystemCardBase
                 LogLevel.Critical,
                 new EnvironmentLaunchUserEvent(ComputeSystem!.AssociatedProviderId, completionStatus));
 
-            IsOperationInProgress = false;
+            _windowEx.DispatcherQueue.TryEnqueue(() =>
+            {
+                UiMessageToDisplay = completionMessage;
+                IsOperationInProgress = false;
+                UiMessageToDisplay = operationFailed ? UiMessageToDisplay : string.Empty;
+            });
+        });
+    }
+
+    private void RemoveComputeSystem()
+    {
+        _windowEx.DispatcherQueue.TryEnqueue(() =>
+        {
+            _log.Information($"Removing Compute system with Name: {ComputeSystem!.DisplayName} from UI");
+            _removalAction(this);
+            RemoveStateChangedHandler();
         });
     }
 
@@ -173,6 +213,65 @@ public partial class ComputeSystemViewModel : ComputeSystemCardBase
         else
         {
             _log.Error(computeSystemOperationResult.Result.ExtendedError, $"Launch operation failed for {ComputeSystem} error: {computeSystemOperationResult.Result.DiagnosticText}");
+        }
+    }
+
+    private bool IsComputeSystemStateTransitioning(ComputeSystemState state)
+    {
+        switch (state)
+        {
+            case ComputeSystemState.Starting:
+            case ComputeSystemState.Saving:
+            case ComputeSystemState.Stopping:
+            case ComputeSystemState.Pausing:
+            case ComputeSystemState.Restarting:
+            case ComputeSystemState.Creating:
+            case ComputeSystemState.Deleting:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void SetupOperationProgressBasedOnState()
+    {
+        if (IsComputeSystemStateTransitioning(State))
+        {
+            IsOperationInProgress = true;
+        }
+        else
+        {
+            IsOperationInProgress = false;
+        }
+
+        if ((State != ComputeSystemState.Creating) || (State != ComputeSystemState.Deleting))
+        {
+            ShouldShowLaunchOperation = true;
+        }
+
+        if (State == ComputeSystemState.Deleted)
+        {
+            RemoveComputeSystem();
+        }
+    }
+
+    private void UpdateOperationsPostCreation(ComputeSystemState previousState, ComputeSystemState newState)
+    {
+        // supported operations may have changed after creation, so we'll update them
+        if ((previousState == ComputeSystemState.Creating) && (previousState != newState))
+        {
+            LaunchOperations.Clear();
+            DotOperations!.Clear();
+
+            foreach (var buttonOperation in DataExtractor.FillLaunchButtonOperations(ComputeSystem!))
+            {
+                LaunchOperations.Add(buttonOperation);
+            }
+
+            foreach (var dotOperation in DataExtractor.FillDotButtonOperations(ComputeSystem!))
+            {
+                LaunchOperations.Add(dotOperation);
+            }
         }
     }
 }
