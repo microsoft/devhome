e228e5e8a69a8f0e05a3bdc1a20847de43e861b4
unknown <dhoehna@yahoo.com>
2024-05-09 13:05:58 -0700

    Adding it to the settings page

--- settings/DevHome.Settings/Views/SettingsPage.xaml.cs
+++ settings/DevHome.Settings/Views/SettingsPage.xaml.cs
@@ -1,8 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
 using DevHome.Settings.ViewModels;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 
 namespace DevHome.Settings.Views;
 
@@ -14,5 +17,15 @@ public sealed partial class SettingsPage : Page
     {
         ViewModel = new SettingsViewModel();
         this.InitializeComponent();
+    }
+
+    private async void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        // Focus on the first focusable element inside the shell content
+        var element = FocusManager.FindFirstFocusableElement(ParentContainer);
+        if (element != null)
+        {
+            await FocusManager.TryFocusAsync(element, FocusState.Programmatic).AsTask();
+        }
     }
 }
