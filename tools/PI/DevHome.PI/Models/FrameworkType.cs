f749ec24f8f704546f07ea239134ad317dd72baa
Andrew Whitechapel <andreww-msft@outlook.com>
2024-05-14 16:27:43 -0700

    Changed AppDetailsPage IsXXX to a collection of readonly checkboxes

---
+++ tools/PI/DevHome.PI/Models/FrameworkType.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DevHome.PI.Models;
+
+public partial class FrameworkType : ObservableObject
+{
+    internal bool IsExactMatch { get; private set; } = true;
+
+    internal string Determinator
+    {
+        get; set;
+    }
+
+    public string Name
+    {
+        get; set;
+    }
+
+    [ObservableProperty]
+    private bool status;
+
+    public FrameworkType(string determinator, string name, bool isExactMatch = true)
+    {
+        Determinator = determinator;
+        Name = name;
+        IsExactMatch = isExactMatch;
+    }
+}
