aab511f2b19421cbf1ce9aefe60123fbce8692b6
Manodasan Wignarajah <mawign@microsoft.com>
2024-05-13 18:17:30 -0700

    Add error UI

--- tools/SetupFlow/DevHome.SetupFlow/ViewModels/QuickstartPlaygroundViewModel.cs
+++ tools/SetupFlow/DevHome.SetupFlow/ViewModels/QuickstartPlaygroundViewModel.cs
@@ -127,6 +127,12 @@ public partial class QuickstartPlaygroundViewModel : SetupPageViewModelBase
     private bool _isPromptTextBoxReadOnly = false;
 
     [ObservableProperty]
+    private bool _isErrorViewVisible = false;
+
+    [ObservableProperty]
+    private string _errorMessage = string.Empty;
+
+    [ObservableProperty]
     [NotifyCanExecuteChangedFor(nameof(GenerateCodespaceCommand))]
     private bool _areDependenciesPresent = false;
 
@@ -408,10 +414,12 @@ public partial class QuickstartPlaygroundViewModel : SetupPageViewModelBase
 
             // Ensure file view isn't visible (in the case where the user has previously run a Generate command
             IsFileViewVisible = false;
+            IsErrorViewVisible = false;
 
             // Temporarily turn off the provider combobox and ensure user cannot edit the prompt for the moment
             EnableQuickstartProjectCombobox = false;
             IsPromptTextBoxReadOnly = true;
+            EnableProjectButtons = false;
 
             IProgress<QuickStartProjectProgress> progress = new Progress<QuickStartProjectProgress>(UpdateProgress);
 
@@ -430,7 +438,8 @@ public partial class QuickstartPlaygroundViewModel : SetupPageViewModelBase
             }
             else
             {
-                // TODO handle error scenario
+                IsErrorViewVisible = true;
+                ErrorMessage = StringResource.GetLocalized("QuickstartPlaygroundGenerationFailedDetails", _quickStartProject.Result.DisplayMessage);
                 TelemetryFactory.Get<ITelemetry>().Log("QuickstartPlaygroundGenerateFailed", LogLevel.Critical, new ProjectGenerationErrorInfo(_quickStartProject.Result.DisplayMessage, _quickStartProject.Result.ExtendedError, _quickStartProject.Result.DiagnosticText));
             }
         }
