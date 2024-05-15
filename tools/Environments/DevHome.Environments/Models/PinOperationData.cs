69d182a7d6be9727f2d1c9b66774a4bb1574f3a2
Branden Bonaby <105318831+bbonaby@users.noreply.github.com>
2024-05-14 18:43:36 -0700

    Update pinning operation and delete operation for HyperV (#2907)
    * add updates
    
    * Update text and notification size
    
    * update variable name
    
    * remove unused using statement per PR comment
    
    * update pinned methods to check for pinned status before invoking call
    
    * update Release to dispose

---
+++ tools/Environments/DevHome.Environments/Models/PinOperationData.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevHome.Environments.ViewModels;
+using Microsoft.Windows.DevHome.SDK;
+
+namespace DevHome.Environments.Models;
+
+public class PinOperationData
+{
+    public OperationsViewModel? ViewModel { get; }
+
+    public bool WasPinnedStatusSuccessful { get; }
+
+    public string? PinnedStatusDisplayMessage { get; }
+
+    public string? PinnedStatusDiagnosticText { get; }
+
+    public PinOperationData(OperationsViewModel? viewModel, ComputeSystemPinnedResult pinnedResult)
+    {
+        ViewModel = viewModel;
+        WasPinnedStatusSuccessful = pinnedResult.Result.Status == ProviderOperationStatus.Success;
+        PinnedStatusDisplayMessage = pinnedResult.Result.DisplayMessage;
+        PinnedStatusDiagnosticText = pinnedResult.Result.DiagnosticText;
+    }
+}
