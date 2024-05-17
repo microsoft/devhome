5001b9504a536b51e5f79d40405779ae738d26fc
Zarana Kiran Desai <78943958+zadesai@users.noreply.github.com>
2024-05-17 16:14:46 -0700

    close settings window when closing barwindow (#2957)

--- tools/PI/DevHome.PI/Controls/ExpandedViewControl.xaml.cs
+++ tools/PI/DevHome.PI/Controls/ExpandedViewControl.xaml.cs
@@ -8,13 +8,13 @@ using DevHome.PI.SettingsUi;
 using DevHome.PI.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using WinUIEx;
 
 namespace DevHome.PI.Controls;
 
 public sealed partial class ExpandedViewControl : UserControl
 {
     private readonly ExpandedViewControlViewModel viewModel = new();
+    private SettingsToolWindow? settingsTool;
 
     public ExpandedViewControl()
     {
@@ -34,7 +34,12 @@ public sealed partial class ExpandedViewControl : UserControl
 
     private void SettingsButton_Click(object sender, RoutedEventArgs e)
     {
-        SettingsToolWindow settingsTool = new(Settings.Default.SettingsToolPosition);
-        settingsTool.Show();
+        settingsTool = new(Settings.Default.SettingsToolPosition);
+        settingsTool.Activate();
+    }
+
+    public void CloseSettings()
+    {
+        settingsTool?.Close();
     }
 }
