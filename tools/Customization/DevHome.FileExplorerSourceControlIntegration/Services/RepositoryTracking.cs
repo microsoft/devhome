32e55b081aff22fc19d87c4b402b0b7040c622de
ssparach <128866445+ssparach@users.noreply.github.com>
2024-06-03 11:40:16 -0700

    Data storage (i.e. extension and registered root path)  for version control extensions inside Dev Home (#2984)
    * changes to write/read data (extension information + root path) from json
    
    * address PR comments
    
    * revert line endings
    
    * address PR comments

---
+++ tools/Customization/DevHome.FileExplorerSourceControlIntegration/Services/RepositoryTracking.cs
@@ -0,0 +1,149 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using DevHome.Common.Helpers;
+using DevHome.Common.Services;
+using Serilog;
+using Windows.Storage;
+
+namespace DevHome.FileExplorerSourceControlIntegration.Services;
+
+public class RepositoryTracking
+{
+    public RepoStoreOptions RepoStoreOptions
+    {
+        get; set;
+    }
+
+    public enum RepositoryChange
+    {
+        Added,
+        Removed,
+    }
+
+    private readonly FileService fileService;
+
+    private readonly object trackRepoLock = new();
+
+    private Dictionary<string, string> TrackedRepositories { get; set; } = new Dictionary<string, string>();
+
+    private readonly Serilog.ILogger log = Log.ForContext("SourceContext", nameof(RepositoryTracking));
+
+    public event Windows.Foundation.TypedEventHandler<string, RepositoryChange>? RepositoryChanged;
+
+    public RepositoryTracking(string? path)
+    {
+        if (RuntimeHelper.IsMSIX)
+        {
+            RepoStoreOptions = new RepoStoreOptions
+            {
+                RepoStoreFolderPath = ApplicationData.Current.LocalFolder.Path,
+            };
+            log.Debug("Repo Store for File Explorer Integration created under ApplicationData");
+        }
+        else
+        {
+            RepoStoreOptions = new RepoStoreOptions
+            {
+                RepoStoreFolderPath = path ?? string.Empty,
+            };
+        }
+
+        fileService = new FileService();
+        RestoreTrackedRepositoriesFomJson();
+    }
+
+    public void RestoreTrackedRepositoriesFomJson()
+    {
+        lock (trackRepoLock)
+        {
+            TrackedRepositories = fileService.Read<Dictionary<string, string>>(RepoStoreOptions.RepoStoreFolderPath, RepoStoreOptions.RepoStoreFileName);
+
+            // No repositories are currently being tracked. The file will be created on first add to repository tracking.
+            log.Debug("Repo store has just been created with the first registered repository root path");
+            TrackedRepositories ??= new Dictionary<string, string>();
+        }
+
+        log.Information($"Repositories retrieved from Repo Store, number of registered repositories: {TrackedRepositories.Count}");
+    }
+
+    // TO DO: Determine if the extension GUID should be stored instead of name as it is more identifiable and/or determine any other unique property to use for
+    // mapping here
+    public void AddRepositoryPath(string extension, string rootPath)
+    {
+        lock (trackRepoLock)
+        {
+            if (!TrackedRepositories.ContainsKey(rootPath))
+            {
+                TrackedRepositories[rootPath] = extension!;
+                fileService.Save(RepoStoreOptions.RepoStoreFolderPath, RepoStoreOptions.RepoStoreFileName, TrackedRepositories);
+                try
+                {
+                    RepositoryChanged?.Invoke(extension, RepositoryChange.Added);
+                }
+                catch (Exception ex)
+                {
+                    log.Error(ex, $"Added event signaling failed: ");
+                }
+
+                log.Information("Repository added to repo store");
+            }
+            else
+            {
+                log.Warning("Repository root path already registered in the repo store");
+            }
+        }
+    }
+
+    public void RemoveRepositoryPath(string rootPath)
+    {
+        lock (trackRepoLock)
+        {
+            TrackedRepositories.TryGetValue(rootPath, out var extension);
+            TrackedRepositories.Remove(rootPath);
+            fileService.Save(RepoStoreOptions.RepoStoreFolderPath, RepoStoreOptions.RepoStoreFileName, TrackedRepositories);
+            try
+            {
+                RepositoryChanged?.Invoke(extension ??= string.Empty, RepositoryChange.Removed);
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex, $"Removed event signaling failed: ");
+            }
+        }
+
+        log.Information("Repository removed from repo store");
+    }
+
+    public Dictionary<string, string> GetAllTrackedRepositories()
+    {
+        lock (trackRepoLock)
+        {
+            if (TrackedRepositories == null)
+            {
+                TrackedRepositories = fileService.Read<Dictionary<string, string>>(RepoStoreOptions.RepoStoreFolderPath, RepoStoreOptions.RepoStoreFileName);
+                TrackedRepositories ??= new Dictionary<string, string>();
+            }
+
+            log.Information("All repositories retrieved from repo store");
+            return TrackedRepositories;
+        }
+    }
+
+    public string GetSourceControlProviderForRootPath(string rootPath)
+    {
+        lock (trackRepoLock)
+        {
+            if (TrackedRepositories.TryGetValue(rootPath, out var value))
+            {
+                log.Information("Source Control Provider returned for root path");
+                return TrackedRepositories[rootPath];
+            }
+            else
+            {
+                log.Error("The root path is not registered for File Explorer Source Control Integration");
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ tools/Customization/DevHome.FileExplorerSourceControlIntegrationUnitTest/RepositoryTrackingServiceUnitTest.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Diagnostics;
+using DevHome.FileExplorerSourceControlIntegration.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevHome.FileExplorerSourceControlIntegrationUnitTest;
+
+[TestClass]
+public class RepositoryTrackingServiceUnitTest
+{
+    private RepositoryTracking RepoTracker { get; set; } = new(Path.Combine(Path.GetTempPath()));
+
+    private readonly string extension = "testExtension";
+
+    private readonly string rootPath = "c:\\test\\rootPath";
+
+    [TestMethod]
+    public void AddRepository()
+    {
+        RepoTracker.AddRepositoryPath(extension, rootPath);
+        var result = RepoTracker.GetAllTrackedRepositories();
+        Assert.IsNotNull(result);
+        Assert.AreEqual(1, result.Count);
+        Assert.IsTrue(result.ContainsKey(rootPath));
+        Assert.IsTrue(result.ContainsValue(extension));
+        RepoTracker.RemoveRepositoryPath(rootPath);
+    }
+
+    [TestMethod]
+    public void RemoveRepository()
+    {
+        RepoTracker.AddRepositoryPath(extension, rootPath);
+        var result = RepoTracker.GetAllTrackedRepositories();
+        Assert.AreEqual(1, result.Count);
+        RepoTracker.RemoveRepositoryPath(rootPath);
+        result = RepoTracker.GetAllTrackedRepositories();
+        Assert.AreEqual(0, result.Count);
+    }
+
+    [TestMethod]
+    public void GetAllRepositories()
+    {
+        for (var i = 0; i < 5; i++)
+        {
+            RepoTracker.AddRepositoryPath(extension, string.Concat(rootPath, i));
+        }
+
+        var result = RepoTracker.GetAllTrackedRepositories();
+        Assert.IsNotNull(result);
+        Assert.AreEqual(5, result.Count);
+        Assert.IsTrue(result.ContainsKey(string.Concat(rootPath, 0)));
+        Assert.IsTrue(result.ContainsValue(extension));
+
+        for (var i = 0; i < 5; i++)
+        {
+            RepoTracker.RemoveRepositoryPath(string.Concat(rootPath, i));
+        }
+    }
+
+    [TestMethod]
+    public void GetSourceControlProviderFromRepositoryPath()
+    {
+        RepoTracker.AddRepositoryPath(extension, rootPath);
+        var result = RepoTracker.GetSourceControlProviderForRootPath(rootPath);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(extension, result);
+        RepoTracker.RemoveRepositoryPath(rootPath);
+    }
+
+    [TestCleanup]
+    public void TestCleanup()
+    {
+        if (File.Exists(Path.Combine(Path.GetTempPath(), "TrackedRepositoryStore.json")))
+        {
+            File.Delete(Path.Combine(Path.GetTempPath(), "TrackedRepositoryStore.json"));
+        }
+    }
+}
