cac09c35b6cf3a3b6ca1088dcd80583ef80d5f61
Zarana Kiran Desai <zadesai@microsoft.com>
2024-06-03 15:14:58 -0700

    moving ResetBarWindowOnTop to viewmodel

--- tools/PI/DevHome.PI/ViewModels/BarWindowViewModel.cs
+++ tools/PI/DevHome.PI/ViewModels/BarWindowViewModel.cs
@@ -121,6 +121,18 @@ public partial class BarWindowViewModel : ObservableObject
         }
     }
 
+    public void ResetBarWindowOnTop()
+    {
+        // If we're snapped to a target window, and that window loses and then regains focus,
+        // we need to bring our window to the front also, to be in-sync. Otherwise, we can
+        // end up with the target in the foreground, but our window partially obscured.
+        // We set IsAlwaysOnTop to true to get it in foreground and then set to false,
+        // this ensures we don't steal focus from target window and at the same time
+        // bar window is not partially obscured.
+        IsAlwaysOnTop = true;
+        IsAlwaysOnTop = false;
+    }
+
     [RelayCommand]
     public void SwitchLayout()
     {
