44515609a7c1c8504a6cff77ea5f299d36ff443f
Zarana Kiran Desai <zadesai@microsoft.com>
2024-06-04 12:40:50 -0700

    remove unwanted using statement

--- tools/PI/DevHome.PI/PrimaryWindow.xaml.cs
+++ tools/PI/DevHome.PI/PrimaryWindow.xaml.cs
@@ -13,7 +13,6 @@ using Windows.System;
 using Windows.Win32;
 using Windows.Win32.Foundation;
 using Windows.Win32.UI.Input.KeyboardAndMouse;
-using WinRT.Interop;
 using WinUIEx;
 using static DevHome.PI.Helpers.WindowHelper;
 
