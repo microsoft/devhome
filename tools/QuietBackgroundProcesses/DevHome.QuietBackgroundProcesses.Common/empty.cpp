1ec083765d8382dc3f76658419a03e3cbc607b77
Jason Holmes <27746781+jaholme@users.noreply.github.com>
2024-06-04 12:28:25 -0700

    Remove empty.cpp as it is no longer necessary

--- tools/QuietBackgroundProcesses/DevHome.QuietBackgroundProcesses.Common/empty.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-// A cpp is required to produce a .lib, and a .lib is required to exist if this project is
-// referenced by another cpp project.
