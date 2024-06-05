c09d0a436d1739408637cac4794aca77aabe5491
Tim Kurtzman <49733346+timkur@users.noreply.github.com>
2024-06-05 16:13:21 -0700

    Initial change

---
+++ tools/PI/DevHome.PI/Controls/PISeperator.xaml.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+using Microsoft.UI.Xaml.Controls;
+
+namespace DevHome.PI.Controls;
+
+public sealed partial class PISeperator : UserControl
+{
+    public PISeperator()
+    {
+        this.InitializeComponent();
+    }
+}
