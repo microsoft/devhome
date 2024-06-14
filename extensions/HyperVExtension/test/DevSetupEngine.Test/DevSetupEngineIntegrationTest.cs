a2178ef7d4d7712501c4c36e6d3b9df29adcd225
Kristen Schau <47155823+krschau@users.noreply.github.com>
2024-06-14 11:09:36 -0400

    put back usings

--- extensions/HyperVExtension/test/DevSetupEngine.Test/DevSetupEngineIntegrationTest.cs
+++ extensions/HyperVExtension/test/DevSetupEngine.Test/DevSetupEngineIntegrationTest.cs
@@ -4,6 +4,8 @@
 using System.Runtime.InteropServices;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Windows.DevHome.DevSetupEngine;
+using Windows.Win32;
+using Windows.Win32.System.Com;
 using WinRT;
 
 namespace DevSetupEngine.Test;
