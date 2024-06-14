a68785f358f23c294a26e783eec011bef293d04a
Andrew Whitechapel <andreww-msft@outlook.com>
2024-06-14 16:13:03 -0700

    Update per feedback

--- tools/PI/DevHome.PI/Helpers/WindowHelper.cs
+++ tools/PI/DevHome.PI/Helpers/WindowHelper.cs
@@ -556,7 +556,7 @@ public class WindowHelper
     // Only one ContentDialog can be shown at a time, so we have to keep track of the current one.
     private static ContentDialog? ContentDialog { get; set; }
 
-    internal static void ShowTimedMessageDialog(FrameworkElement frameworkElement, string message, string closeButtonText)
+    internal static async void ShowTimedMessageDialog(FrameworkElement frameworkElement, string message, string closeButtonText)
     {
         if (ContentDialog is not null)
         {
@@ -590,13 +590,13 @@ public class WindowHelper
 
         try
         {
+            ContentDialog.Opened += (s, e) => timer.Start();
             ContentDialog.Closed += (s, e) =>
             {
                 timer.Stop();
                 ContentDialog = null;
             };
-            _ = ContentDialog.ShowAsync();
-            timer.Start();
+            await ContentDialog.ShowAsync();
         }
         catch (Exception ex)
         {
