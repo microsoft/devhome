8bbd0846c318d063750ebccef26afd6e7b4ef2aa
Andrew Whitechapel <andreww-msft@outlook.com>
2024-06-18 16:31:47 -0700

    Update registering external tools

--- tools/PI/DevHome.PI/Controls/AddToolControl.xaml.cs
+++ tools/PI/DevHome.PI/Controls/AddToolControl.xaml.cs
@@ -1,24 +1,87 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using CommunityToolkit.WinUI.UI.Controls;
 using DevHome.Common.Extensions;
 using DevHome.PI.Helpers;
+using DevHome.PI.Models;
+using IWshRuntimeLibrary;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+using Serilog;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Graphics.Imaging;
+using Windows.Management.Deployment;
+using Windows.Storage;
+using Windows.Storage.Streams;
 using Windows.Win32;
 using Windows.Win32.UI.Controls.Dialogs;
 
 namespace DevHome.PI.Controls;
 
-public sealed partial class AddToolControl : UserControl
+public sealed partial class AddToolControl : UserControl, INotifyPropertyChanged
 {
+    private static readonly ILogger _log = Log.ForContext("SourceContext", nameof(AddToolControl));
+
     private readonly string invalidToolInfo = CommonHelper.GetLocalizedString("InvalidToolInfoMessage");
     private readonly string messageCloseText = CommonHelper.GetLocalizedString("MessageCloseText");
+
+    // We have 3 sets of operations, and we arbitrarily divide the progress timing into 3 equal segments.
+    private const int ShortcutProcessingEndIndex = 33;
+    private const int PackageProcessingEndIndex = 67;
+
+    private InstalledAppInfo? selectedApp;
+    private List<string>? shortcuts;
+    private List<Package>? packages;
+    private List<InstalledAppInfo> allApps = [];
+    private int itemCount;
+
+    public ObservableCollection<InstalledAppInfo> SortedApps { get; set; } = [];
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private bool isLoading;
+
+    public bool IsLoading
+    {
+        get => isLoading;
+        set
+        {
+            isLoading = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsLoading)));
+        }
+    }
+
+    private int progressPercentage;
+
+    public int ProgressPercentage
+    {
+        get => progressPercentage;
+        set
+        {
+            progressPercentage = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ProgressPercentage)));
+        }
+    }
 
     public AddToolControl()
     {
         InitializeComponent();
+        LoadingProgressTextRing.DataContext = this;
     }
 
     private void ToolBrowseButton_Click(object sender, RoutedEventArgs e)
@@ -60,10 +123,7 @@ public sealed partial class AddToolControl : UserControl
         if (fileName != string.Empty)
         {
             ToolPathTextBox.Text = fileName;
-            if (ToolNameTextBox.Text == string.Empty)
-            {
-                ToolNameTextBox.Text = System.IO.Path.GetFileNameWithoutExtension(fileName);
-            }
+            ToolNameTextBox.Text = Path.GetFileNameWithoutExtension(fileName);
         }
 
         return;
@@ -87,10 +147,10 @@ public sealed partial class AddToolControl : UserControl
     {
         ToolNameTextBox.Text = string.Empty;
         ToolPathTextBox.Text = string.Empty;
-        NoneRadio.IsChecked = true;
-        PrefixTextBox.Text = string.Empty;
-        OtherArgsTextBox.Text = string.Empty;
+        LaunchRadio.IsChecked = true;
+        ArgumentsTextBox.Text = string.Empty;
         IsPinnedToggleSwitch.IsOn = true;
+        selectedApp = null;
     }
 
     private ExternalTool? GetCurrentToolDefinition()
@@ -101,55 +161,290 @@ public sealed partial class AddToolControl : UserControl
             return null;
         }
 
-        var argType = ExternalToolArgType.None;
-
-        if (HwndRadio.IsChecked ?? false)
+        var activationType = ToolActivationType.Launch;
+        if (ProtocolRadio.IsChecked ?? false)
         {
-            argType = ExternalToolArgType.Hwnd;
+            activationType = ToolActivationType.Protocol;
         }
-        else if (ProcessIdRadio.IsChecked ?? false)
+        else if (selectedApp is not null && selectedApp.IsMsix)
         {
-            argType = ExternalToolArgType.ProcessId;
+            activationType = ToolActivationType.Msix;
         }
 
         return new(
             ToolNameTextBox.Text,
             ToolPathTextBox.Text,
-            argType,
-            PrefixTextBox.Text ?? string.Empty,
-            OtherArgsTextBox.Text ?? string.Empty,
+            activationType,
+            ArgumentsTextBox.Text,
+            selectedApp?.AppUserModelId ?? string.Empty,
+            selectedApp?.IconFilePath ?? string.Empty,
             IsPinnedToggleSwitch.IsOn);
-    }
-
-    private void UpdateSampleCommandline(object sender, TextChangedEventArgs e)
-    {
-        UpdateSampleCommandline();
-    }
-
-    private void UpdateSampleCommandline(object sender, RoutedEventArgs e)
-    {
-        UpdateSampleCommandline();
-    }
-
-    private void UpdateSampleCommandline()
-    {
-        if (SampleCommandTextBox is null)
-        {
-            // The window is still initializing.
-            return;
-        }
-
-        var tool = GetCurrentToolDefinition();
-        if (tool is null)
-        {
-            return;
-        }
-
-        SampleCommandTextBox.Text = tool.CreateFullCommandLine(123, (Windows.Win32.Foundation.HWND)123);
     }
 
     private void CancelButton_Click(object sender, RoutedEventArgs e)
     {
         ClearValues();
+    }
+
+    private async void AppListButton_Click(object sender, RoutedEventArgs e)
+    {
+        allApps.Clear();
+        SortedApps.Clear();
+
+        itemCount = GetShortcuts();
+        if (itemCount == 0)
+        {
+            Log.Error("Error getting shortcuts");
+        }
+
+        var packageCount = GetPackages();
+        if (packageCount == 0)
+        {
+            Log.Error("Error getting packages");
+        }
+
+        itemCount += packageCount;
+        if (itemCount == 0)
+        {
+            Log.Error("Error getting list of installed apps");
+            return;
+        }
+
+        // We get most of the data on a background thread, which
+        // reports intermittent progress.
+        IsLoading = true;
+        var progress = new Progress<int>(percent =>
+        {
+            // Update the progress report.
+            LoadingProgressTextRing.Value = percent;
+        });
+
+        await ProcessItemsAsync(progress);
+
+        foreach (var app in allApps)
+        {
+            SortedApps.Add(app);
+        }
+
+        IsLoading = false;
+    }
+
+    private int GetShortcuts()
+    {
+        var count = 0;
+
+        // Search for .lnk files in the per-user and all-users Start Menu Programs directories.
+        // %APPDATA%\Microsoft\Windows\Start Menu\Programs
+        var startMenuProgramsPath =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), "Programs");
+
+        // %ProgramData%\Microsoft\Windows\Start Menu\Programs
+        var commonStartMenuProgramsPath =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonStartMenu), "Programs");
+
+        shortcuts = [];
+        if (Directory.Exists(startMenuProgramsPath))
+        {
+            shortcuts.AddRange(Directory.GetFiles(startMenuProgramsPath, "*.lnk", SearchOption.AllDirectories));
+        }
+
+        if (Directory.Exists(commonStartMenuProgramsPath))
+        {
+            shortcuts.AddRange(Directory.GetFiles(commonStartMenuProgramsPath, "*.lnk", SearchOption.AllDirectories));
+        }
+
+        if (shortcuts is not null)
+        {
+            count = shortcuts.Count;
+        }
+
+        return count;
+    }
+
+    private int GetPackages()
+    {
+        var count = 0;
+        var packageManager = new PackageManager();
+        packages = packageManager.FindPackagesForUserWithPackageTypes(string.Empty, PackageTypes.Main).ToList();
+        if (packages is not null)
+        {
+            count = packages.Count;
+        }
+
+        return count;
+    }
+
+    private async Task ProcessItemsAsync(IProgress<int> progress)
+    {
+        // Recursively search for .lnk files.
+        var currentCount = 0;
+        for (var i = 0; i < shortcuts?.Count; i++)
+        {
+            await Task.Run(() => ProcessShortcut(shortcuts[i]));
+
+            // Report progress.
+            currentCount++;
+            var percentComplete = (i + 1) * ShortcutProcessingEndIndex / itemCount;
+            progress.Report(percentComplete);
+        }
+
+        for (var j = 0; j < packages?.Count; j++)
+        {
+            await Task.Run(() => ProcessPackage(packages[j]));
+
+            // Report progress.
+            currentCount++;
+            var percentComplete = ShortcutProcessingEndIndex + ((j + 1) * ShortcutProcessingEndIndex / itemCount);
+            progress.Report(percentComplete);
+        }
+
+        allApps = allApps.OrderBy(app => app.Name).ToList();
+
+        // We get the icon data on the UI thread, because BitmapImages must be created on the UI thread.
+        for (var k = 0; k < allApps.Count; k++)
+        {
+            var app = allApps[k];
+            if (app.IsMsix)
+            {
+                if (app.AppPackage is not null)
+                {
+                    try
+                    {
+                        // The package might be in a bad state, and accessing its
+                        // properties might throw an exception.
+                        app.Icon = new BitmapImage(app.AppPackage.Logo);
+                        app.IconFilePath = app.AppPackage.Logo.LocalPath;
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(app.TargetPath))
+                {
+                    app.Icon = WindowHelper.GetBitmapImageFromFile(app.TargetPath);
+                }
+            }
+
+            currentCount++;
+            var percentComplete = PackageProcessingEndIndex + ((k + 1) * ShortcutProcessingEndIndex / itemCount);
+            progress.Report(percentComplete);
+
+            // Yield to make sure the UI thread can update the progress output.
+            await Task.Delay(1);
+        }
+    }
+
+    public void ProcessShortcut(string filePath)
+    {
+        var appName = Path.GetFileNameWithoutExtension(filePath);
+
+        // Exclude Microsoft Virtual Desktop and WSA shortcuts.
+        if (appName.Contains("Microsoft Virtual Desktop", StringComparison.OrdinalIgnoreCase))
+        {
+            return;
+        }
+
+        var wshShell = new WshShell();
+        var shortcut = (IWshShortcut)wshShell.CreateShortcut(filePath);
+        var targetPath = shortcut.TargetPath;
+
+        // Exclude shortcuts that point to empty targets, filesystem folders, or WSA apps.
+        if (string.IsNullOrEmpty(targetPath)
+            || Directory.Exists(targetPath)
+            || targetPath.Contains("WsaClient.exe", StringComparison.OrdinalIgnoreCase))
+        {
+            return;
+        }
+
+        // Exclude *.chm, *.url, *.html, *.ico targets.
+        var extension = Path.GetExtension(targetPath);
+        if (extension.Equals(".chm", StringComparison.OrdinalIgnoreCase)
+            || extension.Equals(".url", StringComparison.OrdinalIgnoreCase)
+            || extension.Equals(".html", StringComparison.OrdinalIgnoreCase)
+            || extension.Equals(".ico", StringComparison.OrdinalIgnoreCase))
+        {
+            return;
+        }
+
+        allApps.Add(new InstalledAppInfo
+        {
+            Name = appName,
+            ShortcutFilePath = filePath,
+            TargetPath = targetPath,
+        });
+    }
+
+    public void ProcessPackage(Package package)
+    {
+        var op = package.GetAppListEntriesAsync();
+        var task = op.AsTask();
+        task.Wait();
+        var entries = task.Result;
+
+        // We only get the icon for apps that have an AppListEntry,
+        // because the others are not likely to be activatable from the UI.
+        if (entries.Count > 0)
+        {
+            // We only get the icon for the first AppListEntry, ignoring MAPs.
+            // Note we use Package.Logo, not AppListEntry.DisplayInfo.GetLogo
+            // because the latter doesn't get consistently-sized icons.
+            var appListEntry = entries[0];
+            allApps.Add(new InstalledAppInfo
+            {
+                Name = package.DisplayName,
+                AppUserModelId = appListEntry.AppUserModelId,
+                AppPackage = package,
+            });
+        }
+    }
+
+    private void AppsDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (AppsDataGrid.SelectedItem is InstalledAppInfo app)
+        {
+            selectedApp = app;
+            if (!string.IsNullOrEmpty(app.TargetPath))
+            {
+                ToolPathTextBox.Text = app.TargetPath;
+            }
+            else if (!string.IsNullOrEmpty(app.AppUserModelId))
+            {
+                ToolPathTextBox.Text = app.AppUserModelId;
+            }
+
+            ToolNameTextBox.Text = app.Name;
+        }
+        else
+        {
+            selectedApp = null;
+            ToolPathTextBox.Text = string.Empty;
+            ToolNameTextBox.Text = string.Empty;
+        }
+    }
+
+    private void AppsDataGrid_Sorting(object sender, DataGridColumnEventArgs e)
+    {
+        var header = e.Column.Header.ToString();
+        switch (header)
+        {
+            case "Name":
+                if (e.Column.SortDirection == null || e.Column.SortDirection == DataGridSortDirection.Descending)
+                {
+                    AppsDataGrid.ItemsSource = new ObservableCollection<InstalledAppInfo>(
+                        from item in SortedApps orderby item.Name ascending select item);
+                    e.Column.SortDirection = DataGridSortDirection.Ascending;
+                }
+                else
+                {
+                    AppsDataGrid.ItemsSource = new ObservableCollection<InstalledAppInfo>(
+                        from item in SortedApps orderby item.Name descending select item);
+                    e.Column.SortDirection = DataGridSortDirection.Descending;
+                }
+
+                break;
+        }
     }
 }
---
+++ tools/PI/DevHome.PI/Helpers/ExternalTool_v1.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Text.Json.Serialization;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DevHome.PI.Helpers;
+
+public enum ExternalToolArgType
+{
+    None,
+    ProcessId,
+    Hwnd,
+}
+
+public partial class ExternalTool_v1 : ObservableObject
+{
+    public string ID { get; private set; }
+
+    public string Name { get; private set; }
+
+    public string Executable { get; private set; }
+
+    [JsonConverter(typeof(EnumStringConverter<ExternalToolArgType>))]
+    public ExternalToolArgType ArgType { get; private set; } = ExternalToolArgType.None;
+
+    public string ArgPrefix
+    {
+        get; private set;
+    }
+
+    public string OtherArgs
+    {
+        get; private set;
+    }
+
+    public bool IsPinned
+    {
+        get; private set;
+    }
+
+    public ExternalTool_v1(
+        string name,
+        string executable,
+        ExternalToolArgType argtype,
+        string argprefix = "",
+        string otherArgs = "",
+        bool isPinned = false)
+    {
+        Name = name;
+        Executable = executable;
+        ArgType = argtype;
+        ArgPrefix = argprefix;
+        OtherArgs = otherArgs;
+        IsPinned = isPinned;
+        ID = Guid.NewGuid().ToString();
+    }
+}
