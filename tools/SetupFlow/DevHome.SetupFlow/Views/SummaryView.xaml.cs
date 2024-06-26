72543735e080cb06255c0565fb14259d47b7edb6
dahoehna <dhoehna@yahoo.com>
2024-06-26 13:38:23 -0700

    Text Scaling Done

--- tools/SetupFlow/DevHome.SetupFlow/Views/SummaryView.xaml.cs
+++ tools/SetupFlow/DevHome.SetupFlow/Views/SummaryView.xaml.cs
@@ -1,22 +1,42 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using AdaptiveCards.Rendering.WinUI3;
-using CommunityToolkit.Mvvm.Messaging;
-using DevHome.SetupFlow.Models.Environments;
+using CommunityToolkit.WinUI;
+using DevHome.Common.Extensions;
 using DevHome.SetupFlow.ViewModels;
-using DevHome.SetupFlow.Windows;
+using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Windows.UI.ViewManagement;
 
 namespace DevHome.SetupFlow.Views;
 
 public sealed partial class SummaryView : UserControl
 {
+    private readonly UISettings _uiSettings = new();
+
+    private double _textScale = 1.0;
+
+    private const double BaseWidth = 550;
+
     public SummaryView()
     {
         this.InitializeComponent();
+
+        _textScale = _uiSettings.TextScaleFactor;
+        _uiSettings.TextScaleFactorChanged += HandleTextScaleFactorChanged;
+        ParentUniformGrid.MinWidth = BaseWidth * _textScale;
     }
 
     public SummaryViewModel ViewModel => (SummaryViewModel)this.DataContext;
+
+    private void HandleTextScaleFactorChanged(UISettings sender, object args)
+    {
+        Application.Current.GetService<DispatcherQueue>().EnqueueAsync(() =>
+        {
+            _textScale = sender.TextScaleFactor;
+            ParentUniformGrid.MinWidth = BaseWidth * _textScale;
+            InvalidateMeasure();
+        });
+    }
 }
