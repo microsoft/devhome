7359b2c134d47a5d593279819bab26e656409755
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2024-06-27 19:59:00 -0700

    Added behavior class

---
+++ common/Behaviors/AutoFocusBehavior.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using CommunityToolkit.WinUI.Behaviors;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace DevHome.Common.Behaviors;
+
+/// <summary>
+/// This behavior automatically sets the focus on the associated <see cref="Control"/> when it is loaded.
+/// </summary>
+/// <remarks>
+/// This implementation is based on the Uwp code from the Windows Community Toolkit.
+/// https://github.com/CommunityToolkit/WindowsCommunityToolkit/blob/main/Microsoft.Toolkit.Uwp.UI.Behaviors/Focus/AutoFocusBehavior.cs
+/// </remarks>
+public sealed class AutoFocusBehavior : BehaviorBase<Control>
+{
+    protected override void OnAssociatedObjectLoaded() => AssociatedObject.Focus(FocusState.Programmatic);
+}
