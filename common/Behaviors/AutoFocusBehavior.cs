493225cd1d9b86a89e4c8600c137f810b8bae871
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2024-06-27 21:37:29 -0700

    Updated comment

--- common/Behaviors/AutoFocusBehavior.cs
+++ common/Behaviors/AutoFocusBehavior.cs
@@ -11,8 +11,9 @@ namespace DevHome.Common.Behaviors;
 /// This behavior automatically sets the focus on the associated <see cref="Control"/> when it is loaded.
 /// </summary>
 /// <remarks>
-/// This implementation is based on the Uwp code from the Windows Community Toolkit.
-/// https://github.com/CommunityToolkit/WindowsCommunityToolkit/blob/main/Microsoft.Toolkit.Uwp.UI.Behaviors/Focus/AutoFocusBehavior.cs
+/// This implementation is based on the code from the Windows Community Toolkit.
+/// Reference: https://github.com/CommunityToolkit/WindowsCommunityToolkit/blob/winui/CommunityToolkit.WinUI.UI.Behaviors/Focus/AutoFocusBehavior.cs
+/// Issue: https://github.com/CommunityToolkit/Windows/issues/443
 /// </remarks>
 public sealed class AutoFocusBehavior : BehaviorBase<Control>
 {
