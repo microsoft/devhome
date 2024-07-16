62ae07bc5f0c1f1407ba53391926737d72dabf60
Matt Hyman <mathy@microsoft.com>
2024-07-16 10:47:10 -0700

    Fix program method logic. (#3445)

--- extensions/CoreWidgetProvider/Program.cs
+++ extensions/CoreWidgetProvider/Program.cs
@@ -11,7 +11,7 @@ namespace CoreWidgetProvider;
 public sealed class Program
 {
     [MTAThread]
-    public static void Main([System.Runtime.InteropServices.WindowsRuntime.ReadOnlyArray] string[] args)
+    public static async Task Main([System.Runtime.InteropServices.WindowsRuntime.ReadOnlyArray] string[] args)
     {
         // Set up Logging
         Environment.SetEnvironmentVariable("DEVHOME_LOGS_ROOT", Path.Join(Helpers.Logging.LogFolderRoot, "CoreWidgets"));
@@ -33,7 +33,7 @@ public sealed class Program
         if (!mainInstance.IsCurrent)
         {
             Log.Information($"Not main instance, redirecting.");
-            mainInstance.RedirectActivationToAsync(activationArgs).AsTask().Wait();
+            await mainInstance.RedirectActivationToAsync(activationArgs);
             Log.CloseAndFlush();
             return;
         }
@@ -64,7 +64,7 @@ public sealed class Program
             var d = activationArgs.Data as ILaunchActivatedEventArgs;
             var args = d?.Arguments.Split();
 
-            if (args?.Length > 0 && args[1] == "-RegisterProcessAsComServer")
+            if (args?.Length > 1 && args[1] == "-RegisterProcessAsComServer")
             {
                 Log.Information($"Activation COM Registration Redirect: {string.Join(' ', args.ToList())}");
                 HandleCOMServerActivation();
