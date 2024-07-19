35114b6f6709fe013f08d12eb9f5fc7084c1be17
dahoehna <dhoehna@yahoo.com>
2024-07-19 11:13:30 -0700

    Adding padding

--- tools/SetupFlow/DevHome.SetupFlow/ViewModels/Environments/SelectEnvironmentProviderViewModel.cs
+++ tools/SetupFlow/DevHome.SetupFlow/ViewModels/Environments/SelectEnvironmentProviderViewModel.cs
@@ -1,31 +1,43 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using AdaptiveCards.Rendering.WinUI3;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
+using CommunityToolkit.WinUI.Behaviors;
 using DevHome.Common.Contracts.Services;
+using DevHome.Common.Environments.Helpers;
 using DevHome.Common.Environments.Models;
 using DevHome.Common.Services;
 using DevHome.SetupFlow.Models.Environments;
 using DevHome.SetupFlow.Services;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.Windows.DevHome.SDK;
+using Serilog;
+using Windows.System;
 
 namespace DevHome.SetupFlow.ViewModels.Environments;
 
 public partial class SelectEnvironmentProviderViewModel : SetupPageViewModelBase
 {
-    private readonly Microsoft.UI.Dispatching.DispatcherQueue _dispatcher;
+    private readonly ILogger _log = Log.ForContext("SourceContext", nameof(SelectEnvironmentProviderViewModel));
 
     private readonly IComputeSystemService _computeSystemService;
 
     public ComputeSystemProviderDetails SelectedProvider { get; private set; }
+
+    private EnvironmentsNotificationHelper _notificationsHelper;
+
+    private bool _isFirstTimeLoading;
+
+    [ObservableProperty]
+    private string _callToActionText;
+
+    [ObservableProperty]
+    private string _callToActionHyperLinkButtonText;
 
     [ObservableProperty]
     private bool _areProvidersLoaded;
@@ -44,43 +56,38 @@ public partial class SelectEnvironmentProviderViewModel : SetupPageViewModelBase
     {
         PageTitle = stringResource.GetLocalized(StringResourceKey.SelectEnvironmentPageTitle);
         _computeSystemService = computeSystemService;
-        _dispatcher = Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread();
+        _isFirstTimeLoading = true;
     }
 
-    public void LoadProviders()
-    {
-        AreProvidersLoaded = false;
-        Orchestrator.NotifyNavigationCanExecuteChanged();
-
-        Task.Run(async () =>
-        {
-            var providerDetails = await _computeSystemService.GetComputeSystemProvidersAsync();
-
-            _dispatcher.TryEnqueue(() =>
-            {
-                ProvidersViewModels = new();
-
-                foreach (var providerDetail in providerDetails)
-                {
-                    ProvidersViewModels.Add(new ComputeSystemProviderViewModel(providerDetail));
-                }
-
-                AreProvidersLoaded = true;
-            });
-        });
-    }
-
-    protected async override Task OnFirstNavigateToAsync()
+    private async Task LoadProvidersAsync()
     {
         CanGoToNextPage = false;
-        LoadProviders();
+        AreProvidersLoaded = false;
+        Orchestrator.NotifyNavigationCanExecuteChanged();
+        CallToActionText = null;
+        CallToActionHyperLinkButtonText = null;
 
-        // Does nothing, but we need to override this as the base expects a task to be returned.
-        await Task.CompletedTask;
+        var providerDetails = await Task.Run(_computeSystemService.GetComputeSystemProvidersAsync);
+        ProvidersViewModels = new();
+        foreach (var providerDetail in providerDetails)
+        {
+            // Only list providers that support creation
+            if (providerDetail.ComputeSystemProvider.SupportedOperations.HasFlag(ComputeSystemProviderOperations.CreateComputeSystem))
+            {
+                _notificationsHelper?.DisplayComputeSystemProviderErrors(providerDetail);
+                ProvidersViewModels.Add(new ComputeSystemProviderViewModel(providerDetail));
+            }
+        }
+
+        AreProvidersLoaded = true;
+
+        var callToActionData = ComputeSystemHelpers.UpdateCallToActionText(ProvidersViewModels.Count, true);
+        CallToActionText = callToActionData.CallToActionText;
+        CallToActionHyperLinkButtonText = callToActionData.CallToActionHyperLinkText;
     }
 
     [RelayCommand]
-    public void ItemsViewSelectionChanged(ComputeSystemProviderViewModel sender)
+    private void ItemsViewSelectionChanged(ComputeSystemProviderViewModel sender)
     {
         if (sender != null)
         {
@@ -98,9 +105,31 @@ public partial class SelectEnvironmentProviderViewModel : SetupPageViewModelBase
             // Using the default channel to send the message to the recipient. In this case, the EnvironmentCreationOptionsViewModel.
             // In the future if we support a multi-instance setup flow, we can use a custom channel/a message broker to send messages.
             // For now, we are using the default channel.
-            WeakReferenceMessenger.Default.Send(new CreationProviderChangedMessage(new CreationProviderChangedData(SelectedProvider)));
+            WeakReferenceMessenger.Default.Send(new CreationProviderChangedMessage(SelectedProvider));
             CanGoToNextPage = true;
             Orchestrator.NotifyNavigationCanExecuteChanged();
         }
+    }
+
+    public async Task InitializeAsync(StackedNotificationsBehavior notificationQueue)
+    {
+        _notificationsHelper = new(notificationQueue);
+
+        if (_isFirstTimeLoading || !string.IsNullOrEmpty(CallToActionText))
+        {
+            _isFirstTimeLoading = false;
+            CanGoToNextPage = false;
+            await LoadProvidersAsync();
+        }
+    }
+
+    /// <summary>
+    /// Navigates the user to the extensions page library
+    /// process.
+    /// </summary>
+    [RelayCommand]
+    public void CallToActionButton()
+    {
+        Orchestrator.NavigateToOutsideFlow(KnownPageKeys.Extensions);
     }
 }
