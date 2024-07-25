1b1dc2b698da3dfb8adcfed1e4e4446f7780f607
ssparach <128866445+ssparach@users.noreply.github.com>
2024-07-25 14:04:42 -0700

    Detect/install command-line git and invoke (#3419)
    * detect git and invoke functionality
    
    * address PR comments
    
    * address PR feedback
    
    * address style comments

---
+++ extensions/GitExtension/FileExplorerGitIntegration/Models/GitConfiguration.cs
@@ -0,0 +1,96 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using DevHome.Common.Helpers;
+using DevHome.Common.Services;
+using Serilog;
+using Windows.Storage;
+
+namespace FileExplorerGitIntegration.Models;
+
+public class GitConfiguration : IDisposable
+{
+    public GitExecutableConfigOptions GitExecutableConfigOptions { get; set; }
+
+    private readonly FileService fileService;
+
+    private string GitExeInstallPath { get; set; } = string.Empty;
+
+    private readonly object fileLock = new();
+
+    private readonly ILogger log = Log.ForContext<GitDetect>();
+
+    private readonly string tempConfigurationFileName = "TemporaryGitConfiguration.json";
+
+    public GitConfiguration(string? path)
+    {
+        string folderPath;
+        if (RuntimeHelper.IsMSIX)
+        {
+            folderPath = ApplicationData.Current.LocalFolder.Path;
+        }
+        else
+        {
+            folderPath = path ?? string.Empty;
+        }
+
+        GitExecutableConfigOptions = new GitExecutableConfigOptions
+        {
+            GitExecutableConfigFolderPath = folderPath,
+        };
+
+        fileService = new FileService();
+        EnsureConfigFileCreation();
+    }
+
+    public string ReadInstallPath()
+    {
+        lock (fileLock)
+        {
+            GitExeInstallPath = fileService.Read<string>(GitExecutableConfigOptions.GitExecutableConfigFolderPath, GitExecutableConfigOptions.GitExecutableConfigFileName);
+            return GitExeInstallPath;
+        }
+    }
+
+    public void EnsureConfigFileCreation()
+    {
+        lock (fileLock)
+        {
+            if (!Directory.Exists(GitExecutableConfigOptions.GitExecutableConfigFolderPath))
+            {
+                Directory.CreateDirectory(GitExecutableConfigOptions.GitExecutableConfigFolderPath);
+            }
+
+            var configFileFullPath = Path.Combine(GitExecutableConfigOptions.GitExecutableConfigFolderPath, GitExecutableConfigOptions.GitExecutableConfigFileName);
+            if (!File.Exists(configFileFullPath))
+            {
+                fileService.Save(GitExecutableConfigOptions.GitExecutableConfigFolderPath, GitExecutableConfigOptions.GitExecutableConfigFileName, string.Empty);
+                log.Information("The git configuration file did not exists and has just been created");
+            }
+        }
+    }
+
+    public bool IsGitExeInstallPathSet()
+    {
+        return !string.IsNullOrEmpty(GitExeInstallPath);
+    }
+
+    public bool StoreGitExeInstallPath(string path)
+    {
+        lock (fileLock)
+        {
+            log.Information("Setting Git Exe Install Path");
+            GitExeInstallPath = path;
+
+            fileService.Save(GitExecutableConfigOptions.GitExecutableConfigFolderPath, tempConfigurationFileName, GitExeInstallPath);
+            File.Replace(Path.Combine(GitExecutableConfigOptions.GitExecutableConfigFolderPath, tempConfigurationFileName), Path.Combine(GitExecutableConfigOptions.GitExecutableConfigFolderPath, GitExecutableConfigOptions.GitExecutableConfigFileName), null);
+            log.Information("Git Exe Install Path stored successfully");
+            return true;
+        }
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ extensions/GitExtension/FileExplorerGitIntegration/Models/GitExeceutableConfigOptions.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExplorerGitIntegration.Models;
+
+public partial class GitExecutableConfigOptions
+{
+    private const string GitExecutableConfigFileNameDefault = "GitConfiguration.json";
+
+    public string GitExecutableConfigFileName { get; set; } = GitExecutableConfigFileNameDefault;
+
+    private readonly string gitExecutableConfigFolderPathDefault = Path.Combine(Path.GetTempPath(), "FileExplorerGitIntegration");
+
+    private string? gitExecutableConfigFolderPath;
+
+    public string GitExecutableConfigFolderPath
+    {
+        get => gitExecutableConfigFolderPath is null ? gitExecutableConfigFolderPathDefault : gitExecutableConfigFolderPath;
+        set => gitExecutableConfigFolderPath = string.IsNullOrEmpty(value) ? gitExecutableConfigFolderPathDefault : value;
+    }
+}
