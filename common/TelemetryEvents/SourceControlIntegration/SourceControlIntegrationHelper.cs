b956d2da3a59154018fc4760b1f03aa3584504a2
ssparach <128866445+ssparach@users.noreply.github.com>
2024-07-26 12:30:15 -0700

    Basic Telemetry for File Explorer Source Control Integration feature inside Dev Home (#3484)
    * proposed telemetry changes for add/remove repository
    
    * remove unrequired using statements
    
    * address PR feedback
    
    * address PR feedback
    
    * adjust comment
    
    * add count of repos tracked inside Dev Home to event

---
+++ common/TelemetryEvents/SourceControlIntegration/SourceControlIntegrationHelper.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using System.Linq;
+
+namespace DevHome.Common.TelemetryEvents.SourceControlIntegration;
+
+public static class SourceControlIntegrationHelper
+{
+    public static string GetSafeRootPath(string rootPath)
+    {
+        var parts = rootPath.Split(Path.DirectorySeparatorChar, StringSplitOptions.RemoveEmptyEntries);
+        return parts.LastOrDefault() ?? string.Empty;
+    }
+}
