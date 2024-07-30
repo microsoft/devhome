02b0e98ea4f8a1d9cb6b7b44a2b0ebff879eb94b
Kristen Schau <47155823+krschau@users.noreply.github.com>
2024-07-30 15:38:21 -0400

    slap a webview2 in there

--- tools/WebView2Tool/WebView2Tool/Views/WebView2ToolView.xaml.cs
+++ tools/WebView2Tool/WebView2Tool/Views/WebView2ToolView.xaml.cs
@@ -1,7 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
 using DevHome.Common.Views;
+using Microsoft.UI.Xaml.Input;
 
 namespace DevHome.WebView2Tool.Views;
 
@@ -10,5 +12,61 @@ public partial class WebView2ToolView : ToolPage
     public WebView2ToolView()
     {
         this.InitializeComponent();
+
+        addressBar.Text = "https://developer.microsoft.com/en-us/microsoft-edge/webview2/";
+        webView2.Source = new Uri(addressBar.Text);
+
+        InitializeWebView2Async();
+    }
+
+    private async void InitializeWebView2Async()
+    {
+        await webView2.EnsureCoreWebView2Async();
+    }
+
+    private void AddressBar_KeyDown(object sender, KeyRoutedEventArgs e)
+    {
+        if (e.Key == Windows.System.VirtualKey.Enter)
+        {
+            e.Handled = true;
+            TryNavigate();
+        }
+    }
+
+    private void TryNavigate()
+    {
+        Uri destinationUri;
+        if (TryCreateUri(addressBar.Text, out destinationUri))
+        {
+            webView2.Source = destinationUri;
+        }
+        else
+        {
+            var bingString = "https://www.bing.com/search?q=" + Uri.EscapeDataString(addressBar.Text);
+            if (TryCreateUri(bingString, out destinationUri))
+            {
+                addressBar.Text = destinationUri.AbsoluteUri;
+                webView2.Source = destinationUri;
+            }
+            else
+            {
+            }
+        }
+    }
+
+    private bool TryCreateUri(string potentialUri, out Uri result)
+    {
+        Uri uri;
+        if ((Uri.TryCreate(potentialUri, UriKind.Absolute, out uri) || Uri.TryCreate("http://" + potentialUri, UriKind.Absolute, out uri)) &&
+            (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps))
+        {
+            result = uri;
+            return true;
+        }
+        else
+        {
+            result = null;
+            return false;
+        }
     }
 }
