e22f9d33f2161ec6535b0846fd84a5cd0ca50ce8
Kristen Schau <47155823+krschau@users.noreply.github.com>
2024-07-31 15:34:24 -0400

    Add WidgetExtensionService

---
+++ tools/Dashboard/DevHome.Dashboard/Services/IWidgetExtensionService.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+
+namespace DevHome.Dashboard.Services;
+
+internal interface IWidgetExtensionService
+{
+    bool IsCoreWidgetExtension(string providerDefinitionId);
+
+    Task EnsureCoreWidgetExtensionStarted(string providerDefinitionId);
+}
---
+++ tools/Dashboard/DevHome.Dashboard/Services/WidgetExtensionService.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using DevHome.Common.Services;
+
+namespace DevHome.Dashboard.Services;
+
+internal sealed class WidgetExtensionService : IWidgetExtensionService
+{
+    private const string ExtensionUniqueIdStable = "Microsoft.Windows.DevHome_8wekyb3d8bbwe!App!PG-SP-ID1";
+    private const string ExtensionUniqueIdCanary = "Microsoft.Windows.DevHome.Canary_8wekyb3d8bbwe!App!PG-SP-ID1";
+    private const string ExtensionUniqueIdDev = "Microsoft.Windows.DevHome.Dev_8wekyb3d8bbwe!App!PG-SP-ID1";
+
+    private const string ProviderDefinitionStable = "Microsoft.Windows.DevHome_8wekyb3d8bbwe!App!!CoreWidgetProvider";
+    private const string ProviderDefinitionCanary = "Microsoft.Windows.DevHome.Canary_8wekyb3d8bbwe!App!!CoreWidgetProvider";
+    private const string ProviderDefinitionDev = "Microsoft.Windows.DevHome.Dev_8wekyb3d8bbwe!App!!CoreWidgetProvider";
+
+    private readonly IExtensionService _extensionService;
+
+    public WidgetExtensionService(IExtensionService extensionService)
+    {
+        _extensionService = extensionService;
+    }
+
+    public bool IsCoreWidgetExtension(string providerDefinitionId)
+    {
+        return providerDefinitionId.Equals(ProviderDefinitionStable, StringComparison.Ordinal) ||
+               providerDefinitionId.Equals(ProviderDefinitionCanary, StringComparison.Ordinal) ||
+               providerDefinitionId.Equals(ProviderDefinitionDev, StringComparison.Ordinal);
+    }
+
+    public async Task EnsureCoreWidgetExtensionStarted(string providerDefinitionId)
+    {
+        if (providerDefinitionId.StartsWith(ProviderDefinitionStable, StringComparison.Ordinal))
+        {
+            await EnsureExtensionStarted(ExtensionUniqueIdStable);
+        }
+        else if (providerDefinitionId.StartsWith(ProviderDefinitionCanary, StringComparison.Ordinal))
+        {
+            await EnsureExtensionStarted(ExtensionUniqueIdCanary);
+        }
+        else if (providerDefinitionId.StartsWith(ProviderDefinitionDev, StringComparison.Ordinal))
+        {
+            await EnsureExtensionStarted(ExtensionUniqueIdDev);
+        }
+    }
+
+    private async Task EnsureExtensionStarted(string extensionUniqueId)
+    {
+        var extensionWrapper = _extensionService.GetInstalledExtension(extensionUniqueId);
+        if (!extensionWrapper.IsRunning())
+        {
+            await extensionWrapper.StartExtensionAsync();
+        }
+    }
+}
