2826533e2d2770b7b4ddaf5a6b870f416cc95556
Kristen Schau <47155823+krschau@users.noreply.github.com>
2024-07-31 15:34:47 -0400

    Start core widget extension

--- tools/Dashboard/DevHome.Dashboard/Views/DashboardView.xaml.cs
+++ tools/Dashboard/DevHome.Dashboard/Views/DashboardView.xaml.cs
@@ -50,6 +50,7 @@ public partial class DashboardView : ToolPage, IDisposable
 
     private static DispatcherQueue _dispatcherQueue;
     private readonly ILocalSettingsService _localSettingsService;
+    private readonly IWidgetExtensionService _widgetExtensionService;
     private bool _disposedValue;
 
     private const string DraggedWidget = "DraggedWidget";
@@ -67,6 +68,7 @@ public partial class DashboardView : ToolPage, IDisposable
 
         _dispatcherQueue = Application.Current.GetService<DispatcherQueue>();
         _localSettingsService = Application.Current.GetService<ILocalSettingsService>();
+        _widgetExtensionService = Application.Current.GetService<IWidgetExtensionService>();
 
 #if DEBUG
         Loaded += AddResetButton;
@@ -590,6 +592,14 @@ public partial class DashboardView : ToolPage, IDisposable
                     _log.Error($"Error inserting widget in pinned widgets, id = {widgetId}, index = {index}");
                     await widget.DeleteAsync();
                     return;
+                }
+
+                // The WidgetService will start the widget provider, however Dev Home won't know about it and won't be
+                // able to send disposed events when Dev Home closes. Ensure the provider is started here so we can
+                // tell the extension to dispose later.
+                if (_widgetExtensionService.IsCoreWidgetExtension(comSafeWidgetDefinition.ProviderDefinitionId))
+                {
+                    await _widgetExtensionService.EnsureCoreWidgetExtensionStarted(comSafeWidgetDefinition.ProviderDefinitionId);
                 }
 
                 TelemetryFactory.Get<ITelemetry>().Log(
