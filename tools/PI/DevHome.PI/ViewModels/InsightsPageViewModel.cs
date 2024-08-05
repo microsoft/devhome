47e407b44f52ac0f98770a45d26de159defac969
Zarana Kiran Desai <78943958+zadesai@users.noreply.github.com>
2024-08-05 11:15:15 -0700

    Dev Home PI: Create a new BarWindowViewModel with a new BarWindow instance (#3527)
    * Create a new BarWindowViewModel, with a new BarWindow
    
    * update insights count
    
    * create a PIInsightsService
    
    * remove debug assert
    
    * PR feedback
    
    * PR suggestion

--- tools/PI/DevHome.PI/ViewModels/InsightsPageViewModel.cs
+++ tools/PI/DevHome.PI/ViewModels/InsightsPageViewModel.cs
@@ -1,77 +1,21 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
 using DevHome.Common.Extensions;
-using DevHome.PI.Models;
+using DevHome.PI.Services;
 using Microsoft.UI.Xaml;
 
 namespace DevHome.PI.ViewModels;
 
 public partial class InsightsPageViewModel : ObservableObject
 {
-    private readonly BarWindowViewModel _barWindowViewModel;
-    private Process? _targetProcess;
-
     [ObservableProperty]
-    private ObservableCollection<Insight> _insightsList;
-
-    [ObservableProperty]
-    private int _unreadCount;
+    private PIInsightsService _insightsService;
 
     public InsightsPageViewModel()
     {
-        _barWindowViewModel = Application.Current.GetService<BarWindowViewModel>();
-
-        TargetAppData.Instance.PropertyChanged += TargetApp_PropertyChanged;
-        _insightsList = [];
-
-        var process = TargetAppData.Instance.TargetProcess;
-        if (process is not null)
-        {
-            UpdateTargetProcess(process);
-        }
-    }
-
-    public void UpdateTargetProcess(Process process)
-    {
-        if (_targetProcess != process)
-        {
-            _targetProcess = process;
-            InsightsList.Clear();
-            UnreadCount = 0;
-            _barWindowViewModel.UpdateUnreadInsightsCount(0);
-        }
-    }
-
-    private void TargetApp_PropertyChanged(object? sender, PropertyChangedEventArgs e)
-    {
-        if (e.PropertyName == nameof(TargetAppData.TargetProcess))
-        {
-            if (TargetAppData.Instance.TargetProcess is not null)
-            {
-                UpdateTargetProcess(TargetAppData.Instance.TargetProcess);
-            }
-        }
-    }
-
-    internal void AddInsight(Insight insight)
-    {
-        insight.PropertyChanged += (sender, e) =>
-        {
-            if (e.PropertyName == nameof(Insight.HasBeenRead))
-            {
-                UnreadCount = InsightsList.Count(insight => !insight.HasBeenRead);
-                _barWindowViewModel.UpdateUnreadInsightsCount(UnreadCount);
-            }
-        };
-
-        UnreadCount++;
-        _barWindowViewModel.UpdateUnreadInsightsCount(UnreadCount);
-        InsightsList.Add(insight);
+        _insightsService = Application.Current.GetService<PIInsightsService>();
     }
 }
