5704c3d717d201301f4e67609e9b7996670bcdc2
Kristen Schau <47155823+krschau@users.noreply.github.com>
2024-08-05 16:02:12 -0400

    Add events for core telemetry

---
+++ common/TelemetryEvents/PageLoadedEvent.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Diagnostics.Tracing;
+using DevHome.Common.Helpers;
+using DevHome.Telemetry;
+using Microsoft.Diagnostics.Telemetry;
+using Microsoft.Diagnostics.Telemetry.Internal;
+
+namespace DevHome.Common.TelemetryEvents;
+
+[EventData]
+public class PageLoadedEvent : EventBase
+{
+    public override PartA_PrivTags PartA_PrivTags => PrivTags.ProductAndServicePerformance;
+
+    public Guid DeploymentIdentifier { get; }
+
+    public string PageName { get; }
+
+    public PageLoadedEvent(string pageName)
+    {
+        DeploymentIdentifier = Deployment.Identifier;
+        PageName = pageName;
+    }
+
+    public override void ReplaceSensitiveStrings(Func<string, string> replaceSensitiveStrings)
+    {
+        // No sensitive strings to replace.
+    }
+}
---
+++ src/TelemetryEvents/DevHomeStartedEvent.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Diagnostics.Tracing;
+using DevHome.Common.Helpers;
+using DevHome.Telemetry;
+using Microsoft.Diagnostics.Telemetry;
+using Microsoft.Diagnostics.Telemetry.Internal;
+using Serilog;
+
+namespace DevHome.TelemetryEvents;
+
+[EventData]
+public class DevHomeStartedEvent : EventBase
+{
+    public Guid DeploymentIdentifier { get; }
+
+    public override PartA_PrivTags PartA_PrivTags => PrivTags.ProductAndServicePerformance;
+
+    public DevHomeStartedEvent()
+    {
+        DeploymentIdentifier = Deployment.Identifier;
+        var log = Log.ForContext("SourceContext", nameof(DevHomeStartedEvent));
+        log.Debug($"DevHome Launched Event, Identifier: {DeploymentIdentifier}");
+    }
+
+    public override void ReplaceSensitiveStrings(Func<string, string> replaceSensitiveStrings)
+    {
+        // No sensitive strings to replace.
+    }
+}
