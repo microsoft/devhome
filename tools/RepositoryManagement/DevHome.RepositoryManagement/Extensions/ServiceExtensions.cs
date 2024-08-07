e4c82ba0d2861fb8299822fb7100f78ddf4f11b6
Darren Hoehna <dhoehna@yahoo.com>
2024-08-07 12:23:37 -0700

    Update tools/RepositoryManagement/DevHome.RepositoryManagement/Extensions/ServiceExtensions.cs
    Co-authored-by: Kristen Schau <47155823+krschau@users.noreply.github.com>

--- tools/RepositoryManagement/DevHome.RepositoryManagement/Extensions/ServiceExtensions.cs
+++ tools/RepositoryManagement/DevHome.RepositoryManagement/Extensions/ServiceExtensions.cs
@@ -1,11 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using DevHome.RepositoryManagement.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
