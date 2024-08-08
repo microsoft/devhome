07ba7d7511d97d4b6cf21a9918c0d67144c11fbe
ssparach <128866445+ssparach@users.noreply.github.com>
2024-08-08 12:58:38 -0700

    toggle settings view

---
+++ tools/Customization/DevHome.Customization/Views/VersionControlIntegrationSettingsView.xaml.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using DevHome.Common.Extensions;
+using DevHome.Common.Services;
+using DevHome.Customization.Models;
+using DevHome.Customization.ViewModels;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace DevHome.Customization.Views;
+
+public sealed partial class VersionControlIntegrationSettingsView : UserControl
+{
+    public VersionControlIntegrationSettingsView()
+    {
+        this.InitializeComponent();
+    }
+}
