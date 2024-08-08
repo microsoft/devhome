e61a843f597ae7ae5987c4f047edb1bd7993561c
Ryan Shepherd <ryansh@microsoft.com>
2024-08-07 18:42:28 -0700

    Trim down unused methods from LruCacheDictionary

--- extensions/GitExtension/FileExplorerGitIntegration/Models/LruCacheDictionary.cs
+++ extensions/GitExtension/FileExplorerGitIntegration/Models/LruCacheDictionary.cs
@@ -23,81 +23,6 @@ internal sealed class LruCacheDictionary<TKey, TValue>
         _dict = [];
     }
 
-    public LruCacheDictionary(IEqualityComparer<TKey> comparer, int capacity = DefaultCapacity)
-    {
-        _capacity = capacity;
-        _dict = new Dictionary<TKey, LinkedListNode<(TKey, TValue)>>(comparer);
-    }
-
-    public int Count => _dict.Count;
-
-    public bool IsEmpty => Count == 0;
-
-    public IEqualityComparer<TKey> Comparer => _dict.Comparer;
-
-    public void Clear()
-    {
-        lock (_lock)
-        {
-            _dict.Clear();
-            _lruList.Clear();
-        }
-    }
-
-    public TValue AddOrUpdate(TKey key, Func<TKey, TValue> addValueFactory, Func<TKey, TValue, TValue> updateValueFactory)
-    {
-        lock (_lock)
-        {
-            if (_dict.TryGetValue(key, out var node))
-            {
-                RenewExistingNodeNoLock(node);
-                return updateValueFactory(key, node.Value.Item2);
-            }
-
-            var value = addValueFactory(key);
-            AddAndTrimNoLock(key, value);
-            return value;
-        }
-    }
-
-    public TValue AddOrUpdate(TKey key, TValue addValue, Func<TKey, TValue, TValue> updateValueFactory)
-    {
-        lock (_lock)
-        {
-            if (_dict.TryGetValue(key, out var node))
-            {
-                RenewExistingNodeNoLock(node);
-                return updateValueFactory(key, node.Value.Item2);
-            }
-
-            AddAndTrimNoLock(key, addValue);
-            return addValue;
-        }
-    }
-
-    public bool ContainsKey(TKey key)
-    {
-        lock (_lock)
-        {
-            return _dict.ContainsKey(key);
-        }
-    }
-
-    public bool TryAdd(TKey key, TValue value)
-    {
-        lock (_lock)
-        {
-            if (_dict.TryGetValue(key, out var node))
-            {
-                RenewExistingNodeNoLock(node);
-                return false;
-            }
-
-            AddAndTrimNoLock(key, value);
-            return true;
-        }
-    }
-
     public bool TryGetValue(TKey key, out TValue value)
     {
         lock (_lock)
@@ -129,39 +54,6 @@ internal sealed class LruCacheDictionary<TKey, TValue>
         }
     }
 
-    public TValue GetOrAdd(TKey key, Func<TKey, TValue> valueFactory)
-    {
-        lock (_lock)
-        {
-            if (_dict.TryGetValue(key, out var node))
-            {
-                RenewExistingNodeNoLock(node);
-                return node.Value.Item2;
-            }
-
-            var value = valueFactory(key);
-            AddAndTrimNoLock(key, value);
-            return value;
-        }
-    }
-
-    public bool TryRemove(TKey key, out TValue value)
-    {
-        lock (_lock)
-        {
-            if (_dict.TryGetValue(key, out var node))
-            {
-                _lruList.Remove(node);
-                _dict.Remove(key);
-                value = node.Value.Item2;
-                return true;
-            }
-
-            value = default!;
-            return false;
-        }
-    }
-
     private void AddAndTrimNoLock(TKey key, TValue value)
     {
         var newNode = new LinkedListNode<(TKey, TValue)>((key, value));
@@ -187,17 +79,12 @@ internal sealed class LruCacheDictionary<TKey, TValue>
     {
         if (_lruList.Count > _capacity)
         {
-            RemoveFirstNoLock();
-        }
-    }
-
-    private void RemoveFirstNoLock()
-    {
-        var node = _lruList.First;
-        if (node != null)
-        {
-            _lruList.RemoveFirst();
-            _dict.Remove(node.Value.Item1);
+            var node = _lruList.First;
+            if (node != null)
+            {
+                _lruList.RemoveFirst();
+                _dict.Remove(node.Value.Item1);
+            }
         }
     }
 }
