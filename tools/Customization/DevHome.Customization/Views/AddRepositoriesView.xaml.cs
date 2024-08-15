267e7f8675fc34576913342b6e74b3021692606d
ssparach <128866445+ssparach@users.noreply.github.com>
2024-08-15 15:17:08 -0700

    add subheading, open folder in FE

--- tools/Customization/DevHome.Customization/Views/AddRepositoriesView.xaml.cs
+++ tools/Customization/DevHome.Customization/Views/AddRepositoriesView.xaml.cs
@@ -1,17 +1,21 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
 using DevHome.Common.Extensions;
-using DevHome.Common.Services;
 using DevHome.Customization.Models;
 using DevHome.Customization.ViewModels;
+using DevHome.FileExplorerSourceControlIntegration.Services;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Serilog;
 
 namespace DevHome.Customization.Views;
 
 public sealed partial class AddRepositoriesView : UserControl
 {
+    private readonly ILogger _log = Log.ForContext("SourceContext", nameof(AddRepositoriesView));
+
     public FileExplorerViewModel ViewModel
     {
         get;
@@ -40,6 +44,30 @@ public sealed partial class AddRepositoriesView : UserControl
         if (menuItem.DataContext is RepositoryInformation repoInfo)
         {
             ViewModel.AssignSourceControlProviderToRepository(menuItem.Text, repoInfo.RepositoryRootPath);
+        }
+    }
+
+    public void OpenFolderInFileExplorer_Click(object sender, RoutedEventArgs e)
+    {
+        // Extract relevant data from view and give to view model for assign
+        MenuFlyoutItem menuItem = (MenuFlyoutItem)sender;
+        if (menuItem.DataContext is RepositoryInformation repoInfo)
+        {
+            try
+            {
+                // Open folder in file explorer
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = "explorer.exe",
+                    Arguments = repoInfo.RepositoryRootPath,
+                };
+
+                System.Diagnostics.Process.Start(startInfo);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Failed to open folder in file explorer");
+            }
         }
     }
 }
