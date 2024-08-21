9229a7280b8405d616c54dd233cc2c6cfdfc38af
Tim Kurtzman <49733346+timkur@users.noreply.github.com>
2024-08-21 13:30:03 -0700

    enable fast COM rundown

--- DevHome.Service/WindowsBackgroundService.cs
+++ DevHome.Service/WindowsBackgroundService.cs
@@ -15,6 +15,7 @@ public sealed class WindowsBackgroundService(
     protected async override Task ExecuteAsync(CancellationToken stoppingToken)
     {
         RegisterClass(new Guid("1F98F450-C163-4A99-B257-E1E6CB3E1C57"));
+        EnableFastCOMRundown();
 
         try
         {
@@ -99,5 +100,74 @@ public sealed class WindowsBackgroundService(
         // https://docs.microsoft.com/windows/win32/api/combaseapi/nf-combaseapi-corevokeclassobject
         [DllImport(nameof(Ole32))]
         public static extern int CoRevokeClassObject(int register);
+    }
+
+    private void EnableFastCOMRundown()
+    {
+        CGlobalOptions options = new CGlobalOptions();
+
+        if (options is IGlobalOptions globalOptions)
+        {
+            globalOptions.SetItem(COMGlobalOptions.COMGLB_RO_SETTINGS, (uint)COM_RO_FLAGS.COMGLB_FAST_RUNDOWN);
+            globalOptions.SetItem(COMGlobalOptions.COMGLB_EXCEPTION_HANDLING, (uint)COMExeceptionHandling.COMGLB_EXCEPTION_DONOT_HANDLE_ANY);
+        }
+    }
+
+    [ComImport]
+    [Guid("0000034B-0000-0000-C000-000000000046")]
+    public class CGlobalOptions;
+
+    [ComImport]
+    [Guid("0000015B-0000-0000-C000-000000000046")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IGlobalOptions
+    {
+        void SetItem(COMGlobalOptions dwProperty, uint dwValue);
+
+        void Query(COMGlobalOptions dwProperty, out uint pdwValue);
+    }
+
+    public enum COMGlobalOptions
+    {
+        COMGLB_EXCEPTION_HANDLING = 1,
+        COMGLB_APPID = 2,
+        COMGLB_RPC_THREADPOOL_SETTING = 3,
+        COMGLB_RO_SETTINGS = 4,
+        COMGLB_UNMARSHALING_POLICY = 5,
+    }
+
+    public enum COMExeceptionHandling
+    {
+        COMGLB_EXCEPTION_HANDLE = 0,
+        COMGLB_EXCEPTION_DONOT_HANDLE_FATAL = 1,
+        COMGLB_EXCEPTION_DONOT_HANDLE = COMGLB_EXCEPTION_DONOT_HANDLE_FATAL, // Alias for compatibility
+        COMGLB_EXCEPTION_DONOT_HANDLE_ANY = 2,
+    }
+
+    public enum COM_RO_FLAGS
+    {
+        // Remove touch messages from the message queue in the STA modal loop.
+        COMGLB_STA_MODALLOOP_REMOVE_TOUCH_MESSAGES = 0x1,
+
+        // Flags that control the behavior of input message removal in
+        // the STA modal loop when the thread's message queue is attached.
+        COMGLB_STA_MODALLOOP_SHARED_QUEUE_REMOVE_INPUT_MESSAGES = 0x2,
+        COMGLB_STA_MODALLOOP_SHARED_QUEUE_DONOT_REMOVE_INPUT_MESSAGES = 0x4,
+
+        // Flag to opt-in to the fast rundown option.
+        COMGLB_FAST_RUNDOWN = 0x8,
+
+        // Reserved
+        COMGLB_RESERVED1 = 0x10,
+        COMGLB_RESERVED2 = 0x20,
+        COMGLB_RESERVED3 = 0x40,
+
+        // Flag to opt-in to pointer message re-ordering when
+        // queues are attached.
+        COMGLB_STA_MODALLOOP_SHARED_QUEUE_REORDER_POINTER_MESSAGES = 0x80,
+
+        COMGLB_RESERVED4 = 0x100,
+        COMGLB_RESERVED5 = 0x200,
+        COMGLB_RESERVED6 = 0x400,
     }
 }
