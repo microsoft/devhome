fb3fbce426e7f266e5a801700883ffe61fa189f8
Zarana Kiran Desai <zadesai@microsoft.com>
2024-08-22 12:13:00 -0700

    remove row coloring and add combobox header

--- tools/PI/DevHome.PI/Pages/WinLogsPage.xaml.cs
+++ tools/PI/DevHome.PI/Pages/WinLogsPage.xaml.cs
@@ -2,7 +2,10 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using DevHome.Common.Extensions;
+using DevHome.PI.Models;
 using DevHome.PI.Telemetry;
 using DevHome.PI.ViewModels;
 using Microsoft.UI.Xaml;
@@ -15,10 +18,14 @@ public partial class WinLogsPage : Page, IDisposable
 {
     private WinLogsPageViewModel ViewModel { get; }
 
+    public ObservableCollection<WinLogCategory> WinLogCategories { get; }
+
     public WinLogsPage()
     {
+        WinLogCategories = new ObservableCollection<WinLogCategory>(Enum.GetValues(typeof(WinLogCategory)).Cast<WinLogCategory>());
         ViewModel = Application.Current.GetService<WinLogsPageViewModel>();
         InitializeComponent();
+        DataContext = this;
     }
 
     protected override void OnNavigatedTo(NavigationEventArgs e)
