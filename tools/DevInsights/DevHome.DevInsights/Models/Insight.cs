23f42ed9264be12ac66ce13d4bb82be535d5ee3a
Jason Holmes <jaholme@users.noreply.github.com>
2024-08-22 10:23:01 -0700

    Switch Dev Insights name in code (#3666)
    * Rename Project Ironsides to Dev Insights in all code locations
    
    * Update launchsettings.json
    
    * Code review feedback
    
    * Fix crash on launch
    
    * Fix testing document
    
    ---------
    
    Co-authored-by: Jason Holmes <27746781+jaholme@users.noreply.github.com>

---
+++ tools/DevInsights/DevHome.DevInsights/Models/Insight.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Text.RegularExpressions;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DevHome.DevInsights.Models;
+
+internal enum InsightType
+{
+    Unknown,
+    LockedFile,
+    AccessDeniedFile,
+    AccessDeniedRegistry,
+    InvalidPath,
+    Security,
+    MemoryViolation,
+}
+
+public partial class Insight : ObservableObject
+{
+    internal string Title { get; set; } = string.Empty;
+
+    internal string Description { get; set; } = string.Empty;
+
+    internal InsightType InsightType { get; set; } = InsightType.Unknown;
+
+    [ObservableProperty]
+    private bool _hasBeenRead;
+
+    // We show the badge by default, as HasBeenRead is false by default.
+    [ObservableProperty]
+    private double _badgeOpacity = 1;
+}
+
+internal sealed class InsightRegex
+{
+    internal InsightType InsightType { get; set; }
+
+    internal Regex Regex { get; set; }
+
+    internal InsightRegex(InsightType type, Regex regex)
+    {
+        Regex = regex;
+        InsightType = type;
+    }
+}
