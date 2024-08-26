a14822ed16e9d78ec11d2a9124cfdc8c32a92058
dahoehna <dhoehna@yahoo.com>
2024-08-26 11:06:37 -0700

    More comments.

--- database/DevHome.Database/DatabaseModels/RepositoryManagement/Repository.cs
+++ database/DevHome.Database/DatabaseModels/RepositoryManagement/Repository.cs
@@ -5,6 +5,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DevHome.Database.DatabaseModels.RepositoryManagement;
 
+/// <summary>
+/// This represents the SQLite model EF has in the database.
+/// Any change here needs a corresponding migration.
+/// Use FluentAPI in the database context to further customize each column.
+/// </summary>
 [Index(nameof(RepositoryName), nameof(RepositoryClonePath), IsUnique = true)]
 public class Repository
 {
