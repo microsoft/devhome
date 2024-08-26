9299f5d21f89abf812f730d878e59d960547dea4
dahoehna <dhoehna@yahoo.com>
2024-08-26 13:38:22 -0700

    Removing unused equals code

--- database/DevHome.Database/DatabaseModels/RepositoryManagement/Repository.cs
+++ database/DevHome.Database/DatabaseModels/RepositoryManagement/Repository.cs
@@ -26,48 +26,4 @@ public class Repository
     // 1:1 relationship.  Repository is the parent and needs only
     // the object of the dependant.
     public RepositoryMetadata? RepositoryMetadata { get; set; }
-
-    public override bool Equals(object? obj)
-    {
-        if (obj == null)
-        {
-            return false;
-        }
-
-        if (obj is not Repository)
-        {
-            return false;
-        }
-
-        var objAsRepository = obj as Repository;
-        if (objAsRepository!.RepositoryName == null ||
-            objAsRepository.RepositoryClonePath == null)
-        {
-            return false;
-        }
-
-        // Compare names.  Comparing paths will add too much code in one place.
-        if (!objAsRepository.RepositoryName.Equals(RepositoryName, StringComparison.OrdinalIgnoreCase))
-        {
-            return false;
-        }
-
-        var leftPath = Path.GetFullPath(objAsRepository.RepositoryClonePath);
-
-        // Made sure RepositoryClonePath is not null earlier.
-        var rightPath = Path.GetFullPath(RepositoryClonePath!);
-
-        // DevHome is on windows.  If not on windows, this will change.
-        if (!string.Equals(leftPath, rightPath, StringComparison.OrdinalIgnoreCase))
-        {
-            return false;
-        }
-
-        return true;
-    }
-
-    public override int GetHashCode()
-    {
-        return (RepositoryName, RepositoryClonePath, RepositoryMetadata).GetHashCode();
-    }
 }
