bb4ba039aa53a22b0f5815430d6ff1367be962fa
Jason Holmes <27746781+jaholme@users.noreply.github.com>
2024-08-26 17:46:51 -0700

    Add narrator strings for several elements including the horizontal window

--- tools/DevInsights/DevHome.DevInsights/Models/NavLink.cs
+++ tools/DevInsights/DevHome.DevInsights/Models/NavLink.cs
@@ -7,12 +7,15 @@ namespace DevHome.DevInsights.Models;
 
 public class NavLink
 {
+    public string Name { get; internal set; }
+
     public string IconText { get; internal set; }
 
     public string ContentText { get; internal set; }
 
-    public NavLink(string icon, string title)
+    public NavLink(string name, string icon, string title)
     {
+        Name = name;
         IconText = icon;
         ContentText = title;
     }
@@ -22,8 +25,8 @@ public class PageNavLink : NavLink
 {
     public Type? PageViewModel { get; internal set; }
 
-    public PageNavLink(string icon, string title, Type? pageViewModel)
-        : base(icon, title)
+    public PageNavLink(string name, string icon, string title, Type? pageViewModel)
+        : base(name, icon, title)
     {
         PageViewModel = pageViewModel;
     }
