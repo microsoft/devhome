6ce61a09a04d0a7b4b022891a5c10d021d5e434f
Darren Hoehna <dhoehna@yahoo.com>
2024-09-03 14:09:40 -0700

    Moving Repository Management feature as a hidden experimental feature. (#3709)
    * Refining Repository Management's UI. (#3557)
    
    * Adding the Repository tool
    
    * WIP
    
    * View is set up.  Test ata as well.
    
    * Aligning columns.  Adding column spacing.
    
    * Reverting this change
    
    * Update tools/RepositoryManagement/DevHome.RepositoryManagement/Strings/en-us/Resources.resw
    
    Co-authored-by: Kristen Schau <47155823+krschau@users.noreply.github.com>
    
    * Adressing comments
    
    * Revert "Reverting this change"
    
    This reverts commit 711dd787a7edee4d38340b649b64a5599555294c.
    
    * Moving to experimental
    
    * Disabling by default
    
    * Removing from experimental.  Addressing comments.
    
    * Reverting to main
    
    * Update tools/RepositoryManagement/DevHome.RepositoryManagement/DevHome.RepositoryManagement.csproj
    
    Co-authored-by: Kristen Schau <47155823+krschau@users.noreply.github.com>
    
    * Update tools/RepositoryManagement/DevHome.RepositoryManagement/Extensions/ServiceExtensions.cs
    
    Co-authored-by: Kristen Schau <47155823+krschau@users.noreply.github.com>
    
    * Removing duplicate xaml code
    
    * Adding to the mermaid diagram
    
    * Got lost in the shuffle
    
    * Removing a refrence
    
    ---------
    
    Co-authored-by: Kristen Schau <47155823+krschau@users.noreply.github.com>
    
    * Adding EF core, database project, and some usage. (#3674)
    
    * WIP
    
    * EF works.  Can save data.
    
    * Can read and write. :)
    
    * Repos are added when cloned.
    
    * Putting save/load into a different class
    
    * Adding default values
    
    * Cleaning up names and using statements
    
    * More comments
    
    * Removing refrence to the public nuget
    
    * Restoring the nuget config
    
    * Adding a test.  Better defining dates
    
    * Adding some tests
    
    * More comments.
    
    * Better path comparison.
    
    * Removing unused equals code
    
    * Adding more comments.  Making new migrations
    
    * Moving this to experimental
    
    * Some comments and fixes
    
    * Changing to Windows.SDK and re-adding projects
    
    ---------
    
    Co-authored-by: Kristen Schau <47155823+krschau@users.noreply.github.com>

---
+++ tools/RepositoryManagement/DevHome.RepositoryManagement/ViewModels/RepositoryManagementMainPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
+using DevHome.RepositoryManagement.Services;
+using Serilog;
+
+namespace DevHome.RepositoryManagement.ViewModels;
+
+public partial class RepositoryManagementMainPageViewModel
+{
+    private readonly ILogger _log = Log.ForContext("SourceContext", nameof(RepositoryManagementMainPageViewModel));
+
+    private readonly RepositoryManagementDataAccessService _dataAccessService;
+
+    public ObservableCollection<RepositoryManagementItemViewModel> Items => new(_dataAccessService.GetRepositories(true));
+
+    [RelayCommand]
+    public void AddExistingRepository()
+    {
+        throw new NotImplementedException();
+    }
+
+    public RepositoryManagementMainPageViewModel(RepositoryManagementDataAccessService dataAccessService)
+    {
+        _dataAccessService = dataAccessService;
+    }
+}
