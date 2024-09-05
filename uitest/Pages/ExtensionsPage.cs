28a7e2aee8ffa7de1f9bb52b41463d2f6c14fbaf
Lauren Ciha <laurenciha@microsoft.com>
2024-09-04 17:21:44 -0700

    Start on Sample Extension unit tests

--- uitest/Pages/ExtensionsPage.cs
+++ uitest/Pages/ExtensionsPage.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Diagnostics;
 using OpenQA.Selenium.Appium.Windows;
 
 namespace DevHome.UITest.Pages;
@@ -13,8 +14,20 @@ public class ExtensionsPage : ApplicationPage
 
     public WindowsElement GitHubMoreOptionsButton => Driver.FindElementByAccessibilityId("MoreOptions_Microsoft.Windows.DevHomeGitHubExtension_8wekyb3d8bbwe");
 
+    public WindowsElement SampleExtensionCard => Driver.FindElementByAccessibilityId("Dev Home Sample Extension (Dev)");
+
+    public WindowsElement SampleExtensionMoreOptionsButton => Driver.FindElementByAccessibilityId("MoreOptions_Microsoft.Windows.DevHomeSampleExtension_8wekyb3d8bbwe");
+
     public ExtensionsPage(WindowsDriver<WindowsElement> driver)
         : base(driver)
     {
+    }
+
+    public SampleExtensionSettingsPage NavigateToSampleExtensionSettingsPage()
+    {
+        Trace.WriteLine("Navigating to SampleExtensionSettings");
+        SampleExtensionCard.Click();
+
+        return new(Driver);
     }
 }
---
+++ uitest/SampleExtensionTest.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using DevHome.UITest.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevHome.Tests.UITest;
+
+[TestClass]
+public class SampleExtensionTest : DevHomeTestBase
+{
+}
