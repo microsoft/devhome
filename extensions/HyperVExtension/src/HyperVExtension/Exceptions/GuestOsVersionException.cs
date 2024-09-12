ca1ca04bac3325bc68e29f8f478453d2b4f85971
Sergey Shilov <51001703+sshilov7@users.noreply.github.com>
2024-09-12 12:21:31 -0700

    Hyper-V extension: Check guest OS version before apply configuration. (#3831)

---
+++ extensions/HyperVExtension/src/HyperVExtension/Exceptions/GuestOsVersionException.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Globalization;
+using System.Management.Automation;
+using System.Text;
+using HyperVExtension.Common;
+using HyperVExtension.Helpers;
+
+namespace HyperVExtension.Exceptions;
+
+internal class GuestOsVersionException : Exception
+{
+    public GuestOsVersionException(IStringResource stringResource, Exception innerException, Dictionary<string, string>? guestOsProperties)
+        : base(stringResource.GetLocalized("FailedToDetermineGuestOsVersion", $"Windows {Constants.MinWindowsVersionForApplyConfiguration}"), innerException)
+    {
+        HResult = innerException.HResult;
+        GuestOsProperties = guestOsProperties;
+    }
+
+    protected GuestOsVersionException(string message, Dictionary<string, string>? guestOsProperties)
+        : base(message)
+    {
+        GuestOsProperties = guestOsProperties;
+    }
+
+    public Dictionary<string, string>? GuestOsProperties { get; }
+
+    public override string ToString()
+    {
+        StringBuilder message = new();
+        message.Append(CultureInfo.InvariantCulture, $"{Message}.");
+        if (InnerException != null)
+        {
+            message.Append(CultureInfo.InvariantCulture, $" {InnerException}.");
+        }
+
+        if (GuestOsProperties != null)
+        {
+            GuestOsProperties.TryGetValue(HyperVStrings.OSPlatformId, out var osPlatformId);
+            GuestOsProperties.TryGetValue(HyperVStrings.OSVersion, out var osVersion);
+            GuestOsProperties.TryGetValue(HyperVStrings.OSName, out var osName);
+            if ((osPlatformId != null) ||
+                (osVersion != null) ||
+                (osName != null))
+            {
+                message.Append(" (");
+                if (osPlatformId != null)
+                {
+                    message.Append(CultureInfo.InvariantCulture, $"{HyperVStrings.OSPlatformId} = {osPlatformId}");
+                }
+
+                if (osVersion != null)
+                {
+                    message.Append(CultureInfo.InvariantCulture, $", {HyperVStrings.OSVersion} = {osVersion}");
+                }
+
+                if (osName != null)
+                {
+                    message.Append(CultureInfo.InvariantCulture, $", {HyperVStrings.OSName} = {osName}");
+                }
+
+                message.Append(')');
+            }
+        }
+
+        if (InnerException != null)
+        {
+            message.Append(CultureInfo.InvariantCulture, $"{Environment.NewLine}{InnerException}.");
+        }
+
+        return message.ToString();
+    }
+}
