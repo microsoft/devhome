ccb31b4032dab4bc6208c0f12d18e08b0fb84bc2
Lauren Ciha <laurenciha@microsoft.com>
2024-09-30 17:31:09 -0700

    Webpage updates

--- extensions/SampleExtension/WebContent/jquerytest.js
+++ extensions/SampleExtension/WebContent/jquerytest.js
@@ -1,20 +1,33 @@
-﻿$(document).ready(function () {
-    $("h1").hover(
-        function () {
-            $(this).css(
-                "color",
-                "purple"
-            );
-        },
-        function () {
-            $(this).css(
-                "color",
-                "aliceblue"
-            );
-        }
-    );
-});
+﻿var lightMode = "rgb(243, 243, 243)";
+var darkMode = "rgb(32, 32, 32)";
 
-function navigateToPage(webPage) {
-    window.location.assign(webPage);
-};
+$(document).ready(function () {
+    $("body").css("font-family", "'Segoe UI', Tahoma, Geneva");
+    var isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    if (isDarkMode) {
+        $("html").attr('data-theme', 'dark');
+        $("h1").hover(
+            function () {
+                $(this).css("color", "yellow"); // High contrast with darkMode
+            },
+            function () {
+                $(this).css("color", "cyan"); // High contrast with darkMode
+            }
+        );
+    } else {
+        $("html").attr('data-theme', 'light');
+        $("h1").hover(
+            function () {
+                $(this).css("color", "blue"); // High contrast with lightMode
+            },
+            function () {
+                $(this).css("color", "red"); // High contrast with lightMode
+            }
+        );
+    }
+
+    $("input[name='button']").click(function () {
+        window.location.assign("ExtensionSettingsPage.html");
+    });
+});
