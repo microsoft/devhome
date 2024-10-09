a84277e3f7f7a143daf18f0d78f94d124864814a
Lauren Ciha <laurenciha@microsoft.com>
2024-10-09 16:06:55 -0700

    Remove using from PackageViewModel :

--- tools/SetupFlow/DevHome.SetupFlow/ViewModels/PackageViewModel.cs
+++ tools/SetupFlow/DevHome.SetupFlow/ViewModels/PackageViewModel.cs
@@ -11,7 +11,6 @@ using DevHome.Contracts.Services;
 using DevHome.Services.WindowsPackageManager.Contracts;
 using DevHome.SetupFlow.Models;
 using DevHome.SetupFlow.Services;
-using Microsoft.Internal.Windows.DevHome.Helpers.Restore;
 using Microsoft.UI.Xaml.Media.Imaging;
 using Windows.Storage.Streams;
 using Windows.System;
@@ -86,8 +85,8 @@ public partial class PackageViewModel : ObservableObject
         _orchestrator = orchestrator;
 
         // Lazy-initialize optional or expensive view model members
-        _packageDarkThemeIcon = new Lazy<BitmapImage>(() => GetIconByTheme(RestoreApplicationIconTheme.Dark));
-        _packageLightThemeIcon = new Lazy<BitmapImage>(() => GetIconByTheme(RestoreApplicationIconTheme.Light));
+        _packageDarkThemeIcon = new Lazy<BitmapImage>(() => IconByTheme);
+        _packageLightThemeIcon = new Lazy<BitmapImage>(() => IconByTheme);
 
         SelectedVersion = GetDefaultSelectedVersion();
         InstallPackageTask = CreateInstallTask();
@@ -203,22 +202,19 @@ public partial class PackageViewModel : ObservableObject
         _screenReaderService.Announce(announcementText);
     }
 
-    /// <summary>
-    /// Gets the package icon based on the provided theme
-    /// </summary>
-    /// <param name="theme">Package icon theme</param>
-    /// <returns>Package icon</returns>
-    private BitmapImage GetIconByTheme(RestoreApplicationIconTheme theme)
+    public BitmapImage IconByTheme
     {
-        return theme switch
+        get
         {
-            // Get default dark theme icon if corresponding package icon was not found
-            RestoreApplicationIconTheme.Dark =>
-                _package.DarkThemeIcon == null ? DefaultDarkPackageIconSource : CreateBitmapImage(_package.DarkThemeIcon),
-
-            // Get default light theme icon if corresponding package icon was not found
-            _ => _package.LightThemeIcon == null ? DefaultLightPackageIconSource : CreateBitmapImage(_package.LightThemeIcon),
-        };
+            if (_themeSelector.IsDarkTheme())
+            {
+                return CreateBitmapImage(_package.DarkThemeIcon);
+            }
+            else
+            {
+                return CreateBitmapImage(_package.LightThemeIcon);
+            }
+        }
     }
 
     private BitmapImage CreateBitmapImage(IRandomAccessStream stream)
